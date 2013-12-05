f71b240852c0e2805bad4169d85be811bf4de43c
luojing1211 <jingluo@Jings-MacBook-Air.local>
2013-12-05 15:26:14 -0500

    script for loading spice kernels.

---
+++ loadKernels.py
@@ -0,0 +1,7 @@
+import spice as spc
+
+
+spc.furnsh("/Users/jingluo/Research_codes/Data_simulator/simulate_toa_py/simulate_toa2/kernel/PCK/pck00008.tpc")
+spc.furnsh("/Users/jingluo/Research_codes/Data_simulator/simulate_toa_py/simulate_toa2/kernel/LPSK/naif0009.tls")
+spc.furnsh("/Users/jingluo/Research_codes/Data_simulator/simulate_toa_py/simulate_toa2/kernel/PCK/earth_000101_130422_130130.bpc")
+spc.furnsh("/Users/jingluo/Research_codes/Data_simulator/simulate_toa_py/simulate_toa2/kernel/Peph/de405.bsp")
