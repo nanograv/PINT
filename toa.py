0230b9bfd9a43e4d1226ee70118628404c1ff6ff
Scott Ransom <sransom@nrao.edu>
2013-12-06 11:47:18 -0500

    Lots of improvements.  Fixed INCLUDE bug also.

--- toa.py
+++ toa.py
@@ -68,7 +68,14 @@ def parse_TOA_line(line):
         # All the rest should be flags
         flags = fields[5:]
         for i in range(0, len(flags), 2):
-            d[flags[i].lstrip('-')] = flags[i+1]
+            k, v = flags[i].lstrip('-'), flags[i+1]
+            try:  # Convert what we can to floats and ints
+                d[k] = int(v)
+            except ValueError:
+                try:
+                    d[k] = float(v)
+                except ValueError:
+                    d[k] = v
     elif fmt=="Parkes" or fmt=="ITOA":
         raise RuntimeError, \
             "TOA format '%s' not implemented yet" % self.format
@@ -89,14 +96,14 @@ class toa(object):
         other keyword/value pairs can be specified as needed
 
     Example:
-        >>> a = toa("54567.876876876876876", 4.5, freq=1400.0,
+        >>> a = toa((54567, 0.876876876876876), 4.5, freq=1400.0,
                     obs="GBT", backend="GUPPI")
         >>> print a
         54567.876876876876876: 4.500 us at 'GBT' at 1400.0000 MHz
         {'backend': 'GUPPI'}
 
     """
-    __std_params = ['mjd', 'freq', 'error', 'obs', 'scale']
+    __std_params = ['mjd', 'freq', 'error', 'obs', 'scale', 'isTOA']
 
     def __init__(self, MJD, # required
                  error=0.0, obs='bary', freq=float("inf"), scale='utc', # with defaults
@@ -107,94 +114,128 @@ class toa(object):
             self.error = error
             self.obs = obs
             self.freq = freq
+            self.isTOA = True
         else:
             if MJD is "Command":
                 self.format = "Command"
-            elif MJD is None: # Blank line, comment, unknown
+            elif MJD is None: # Blank line, comment, unknown:  no
                 pass
+            self.isTOA = False
         # set any other optional params
         for key in kwargs:
             setattr(self, key, kwargs[key])
 
     def __str__(self):
-        if self.format in ("Tempo2", "Princeton", "Parkes", "ITOA"):
+        if self.isTOA:
             s = str(self.mjd) + ": %.3f us at '%s' at %.4f MHz" % \
                 (self.error, self.obs, self.freq)
         else:
-            s = self.format
+            s = ""
         other_keys = [k for k in self.__dict__.keys() if k not in self.__std_params]
         if len(other_keys):
-            s += "\n" + str(dict([(k,self.__dict__[k]) for k in other_keys]))
+            s += str(dict([(k,self.__dict__[k]) for k in other_keys]))
         return s
-
-    def is_toa(self):
-        if self.format in ("Tempo2", "Princeton", "Parkes", "ITOA"):
-            return True
-        else:
-            return False
 
 
 class TOAs(object):
 
-    def __init__(self, toafile):
-        if type(toafile) in [tuple, list]:
-            for infile in toafile:
-                self.read_toa_file(infile)
+    def __init__(self, toafile=None):
+        if toafile:
+            if type(toafile) in [tuple, list]:
+                for infile in toafile:
+                    self.read_toa_file(infile)
+            else:
+                self.read_toa_file(toafile)
         else:
-            self.read_toa_file(toafile)
+            self.toas = []
 
     def get_freqs(self):
-        return numpy.array([t.freq for t in self if t.is_toa()])
+        return numpy.array([t.freq for t in self.toas if t.isTOA])
 
     def get_mjds(self):
-        return numpy.array([t.mjd for t in self if t.is_toa()])
+        return numpy.array([t.mjd for t in self.toas if t.isTOA])
 
     def get_errs(self):
-        return numpy.array([t.error for t in self if t.is_toa()])
+        return numpy.array([t.error for t in self.toas if t.isTOA])
 
-    def get_flags(self,flag,f=lambda x: x):
-        return numpy.array([f(t.flags[flag]) for t in self if t.is_toa()])
+    def summary(self):
+        print "There are %d observatories:" % len(self.observatories), \
+              list(self.observatories)
+        print "There are %d TOAs" % len([x for x in self.toas if x.isTOA])
+        if self.emax_culled is not None:
+            print "There were %d TOAs ignored because of EMAX" % \
+                  self.emax_culled
+        print "There are %d commands" % \
+              len([x for x in self.toas if x.format=="Command"])
+        errs = self.get_errs()
+        print "Min / Max TOA errors:", min(errs), max(errs)
+        print "Mean / Median / StDev TOA error:", errs.mean(), \
+              numpy.median(errs), errs.std()
 
-    def read_toa_file(self, filename, process_includes=True,
-                      ignore_blanks=True, top=True):
+        
+    def read_toa_file(self, filename, process_includes=True, top=True):
         """
-        read_toa_file(filename, process_includes=True, ignore_blanks=True)
+        read_toa_file(filename, process_includes=True)
             Read the given filename and return a list of toa objects 
             parsed from it.  Will recurse to process INCLUDE-d files unless
             process_includes is set to False.
         """
         if top:
-            self.info = None
             self.toas = []
+            # The following are used while processing the TOAs
+            self.efac = 1.0
+            self.equad = 1.0
+            self.emax = None
+            self.info = None
+            self.observatories = set()
+            self.emax_culled = None
         with open(filename, "r") as f:
             skip = False
             for l in f.readlines():
                 MJD, d = parse_TOA_line(l)
                 newtoa = toa(MJD, **d)
                 if newtoa.format=="Command":
-                    if newtoa.command[0]=="SKIP":
+                    self.toas.append(newtoa) # add the command
+                    cmd = newtoa.command[0]
+                    if cmd=="SKIP":
                         skip = True
-                    elif newtoa.command[0]=="NOSKIP":
+                    elif cmd=="NOSKIP":
                         skip = False
                         continue
-                    if skip: continue
-                    if newtoa.command[0]=="INFO":
+                    if skip:
+                        continue
+                    if cmd=="END":
+                        if not top:
+                            return self.toas
+                        else:
+                            return
+                    elif cmd=="EMAX":
+                        self.emax = float(newtoa.command[1])
+                    elif cmd=="EFAC":
+                        self.efac = float(newtoa.command[1])
+                    elif cmd=="EQUAD":
+                        self.equad = float(newtoa.command[1])
+                    elif cmd=="INFO":
                         self.info = newtoa.command[1]
-                    if newtoa.command[0]=="INCLUDE" and process_includes:
-                        self.toas += self.read_toa_file(newtoa.command[1],
-                                                        ignore_blanks=ignore_blanks,
-                                                        top=False)
-                    else:
-                        self.toas.append(newtoa)
-                elif skip:
+                    elif cmd=="INCLUDE" and process_includes:
+                        self.read_toa_file(newtoa.command[1], top=False)
+                elif skip or newtoa.format in ("Blank", "Unknown", "Comment"):
                     continue
-                elif newtoa.format in ("Blank", "Unknown", "Comment"):
-                    if not ignore_blanks:
-                        self.toas.append(newtoa)
                 else:
-                    newtoa.info = self.info
-                    self.toas.append(newtoa)
-                # print newtoa
-            f.close()
-            if not top:
+                    if self.emax is None or newtoa.error < self.emax:
+                        newtoa.info = self.info
+                        newtoa.error *= self.efac
+                        newtoa.error = numpy.hypot(newtoa.error, self.equad)
+                        self.observatories.add(newtoa.obs)
+                        self.toas.append(newtoa)
+                    else:
+                        self.emax_culled = 1 if self.emax_culled is None \
+                                           else self.emax_culled + 1
+            if top:
+                # Clean up our temporaries used when reading TOAs
+                del(self.info)
+                del(self.emax)
+                del(self.equad)
+                del(self.efac)
+            else:
                 return self.toas
