212ff4a9f119c182b06e0ea470a8c1ac0ae634ce
Scott Ransom <sransom@nrao.edu>
2013-12-13 00:18:06 -0500

    Added test to ensure that JUMPs increment

--- tests/test_toa_reader.py
+++ tests/test_toa_reader.py
@@ -2,7 +2,7 @@ import toa
 
 x = toa.TOAs("tests/test1.tim")
 
-assert(len(x.commands)==14)
+assert(len(x.commands)==16)
 assert(len(x.toas)==9)
 assert(x.toas[0].flags["info"]=="test1")
 assert(x.toas[0].flags["jump"]==0)
@@ -10,3 +10,4 @@ assert(x.toas[3].flags["info"]=="test2")
 assert(x.toas[3].flags["time"]==1.0)
 assert("jump" not in x.toas[4].flags)
 assert("time" not in x.toas[4].flags)
+assert(x.toas[-1].flags["jump"]==1)
