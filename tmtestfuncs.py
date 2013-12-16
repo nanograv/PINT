023d8f1bc4f84422f52469dc7dc1a40ab2d707d5
csheehy <csheehy@uchicago.edu>
2013-12-16 02:12:44 -0600

    minor tweaks

--- tmtestfuncs.py
+++ tmtestfuncs.py
@@ -1,10 +1,15 @@
 # Test timing model functions to test out the residuals class
+from astropy.time import Time
+import numpy
 
 def F0(toa,model):
-
-    dt = (toa - model.PEPOCH.value)*24*3600
-    ph = dt*model.F0.value
+    
+    dt = toa.get_mjds() - Time(model.PEPOCH.value,format="mjd",scale="utc")
+    # Can use dt[n].jd1 and jd2 with mpmath here if necessary
+    ph = numpy.array([x.sec*model.F0.value for x in dt])
 
     return ph
+
+
 
 
