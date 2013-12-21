72119da1d1f96fad35b13f9f93b266994d91e087
Scott Ransom <sransom@nrao.edu>
2013-12-21 12:57:11 -0500

    renamed and cleaned up SPICE stuff.  DO NOT USE TABS!  Or camelCase! Yuck!

--- observatories.py
+++ observatories.py
@@ -1,8 +1,7 @@
 import os
 import numpy
-import spice
+import spiceutils
 import astropy.units as u
-from astropy.coordinates.angles import Angle
 
 class observatory:
     pass
@@ -17,8 +16,8 @@ def read_observatories():
                 obs = observatory()
                 obs.name = vals[0]
                 xyz_vals = (float(vals[1]), float(vals[2]), float(vals[3]))
-                obs.XYZ = [a * u.m for a in xyz_vals]
-                obs.geo = ITRF_to_GEO_WGS84(xyz_vals)
+                obs.xyz = [a * u.m for a in xyz_vals]
+                obs.geo = spiceutils.ITRF_to_GEO_WGS84(xyz_vals)
                 obs.aliases = [obs.name.upper()]+[x.upper() for x in vals[4:]]
                 observatories[obs.name] = obs
     return observatories
