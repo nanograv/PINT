a4dbd03eeac525f5163dcc51c948dfa2935a8d08
Scott Ransom <sransom@nrao.edu>
2013-12-29 03:34:43 -0500

    Added SPICE Solar System body masses file based on DE403

--- datafiles/download_data.sh
+++ datafiles/download_data.sh
@@ -4,3 +4,4 @@ wget -N ftp://ssd.jpl.nasa.gov/pub/eph/planets/bsp/de430t.bsp
 wget -N http://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/earth_latest_high_prec.bpc
 wget -N http://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/pck00010.tpc
 wget -N http://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/naif0010.tls
+wget -N http://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/de-403-masses.tpc
