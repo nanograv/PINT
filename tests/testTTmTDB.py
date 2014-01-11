73dd47dab1b8cb8c0b1d798136f92b39acde77ca
luojing1211 <jingluo@Jings-MacBook-Air.local>
2014-01-11 15:40:37 -0600

    generated 2 yrs data

--- tests/testTTmTDB.py
+++ tests/testTTmTDB.py
@@ -1,3 +1,16 @@
-import 
+import mpmath as mp
 
 spd = 86400.0    # seconds per day
+hpd = 24.0       # hours per day
+
+initT = 51544.5  # initial time in MJD (J2000 starting time)
+
+timeArray = []
+
+NumData = 20*365*24
+
+
+for i in range(NumData):
+    timeArray.append(initT + i*1.0/hpd)
+    print timeArray[i]
+
