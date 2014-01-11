7286983445a47f200715f6ac652539e5d78f92a4
Anne Archibald <peridot.faceted@gmail.com>
2014-01-11 18:21:01 -0500

    Raise exceptions if startup fails
    If the PINT variable is not set or the SPICE data files are not
    available, raise an exception so that the tests fail.

--- pint/loadKernels.py
+++ pint/loadKernels.py
@@ -6,18 +6,14 @@ import os
 # Load the PINT environment variable to get the top level directory
 pintdir = os.getenv("PINT")
 if pintdir is None:
-    print "Could not find the PINT environment variable. It should " \
-      "point to the PINT directory!"
-    os.sys.exit()
+    raise ValueError("Could not find the PINT environment variable. " \
+                     "It should point to the PINT directory!")
 
-try:
-    spice.furnsh(os.path.join(pintdir, "datafiles/pck00010.tpc"))
-    print "SPICE loaded planetary constants."
-    spice.furnsh(os.path.join(pintdir, "datafiles/naif0010.tls"))
-    print "SPICE loaded leap seconds."
-    spice.furnsh(os.path.join(pintdir, "datafiles/earth_latest_high_prec.bpc"))
-    print "SPICE loaded Earth rotation parameters."
-    spice.furnsh(os.path.join(pintdir, "datafiles/de405.bsp"))
-    print "SPICE loaded DE405 Planetary Ephemeris."
-except spice.SpiceException:
-    print "Could not load SPICE kernel!"
+spice.furnsh(os.path.join(pintdir, "datafiles/pck00010.tpc"))
+print "SPICE loaded planetary constants."
+spice.furnsh(os.path.join(pintdir, "datafiles/naif0010.tls"))
+print "SPICE loaded leap seconds."
+spice.furnsh(os.path.join(pintdir, "datafiles/earth_latest_high_prec.bpc"))
+print "SPICE loaded Earth rotation parameters."
+spice.furnsh(os.path.join(pintdir, "datafiles/de405.bsp"))
+print "SPICE loaded DE405 Planetary Ephemeris."
