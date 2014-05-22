666128b5b005b1d32293579af52ce37e8589c00d
luojing1211 <jingluo@Jings-MacBook-Air.local>
2014-05-22 17:07:43 -0500

    Add array topocenter posvel

--- pint/erfautils.py
+++ pint/erfautils.py
@@ -69,3 +69,67 @@ def topo_posvels(xyz, toa):
     vel = erfa.trxp(rc2i, vel) * u.m / u.s
 
     return utils.PosVel(pos, vel, obj=toa.obs, origin="EARTH")
+
+def topo_posvels_array(TOAs):
+    """
+    A array version of computing topocenter
+    """
+    # Two double point time 2-D array for tt
+    tt = TOAs.dataTable['tt']
+    # Float point time  1-D numpy array for mjd
+    mjd = TOAs.dataTable['utc']
+    # Two double point for ut1
+    ut1 = TOAs.dataTable['ut1']
+    # Get x,y coords of Celestial Intermediate Pole and CIO locator s
+        
+    # Interp the IERS table        
+    # Get dX and dY from IERS B
+    dX = numpy.interp(mjd, iers_tab['MJD'], iers_tab['dX_2000A']) * u.arcsec
+    dY = numpy.interp(mjd, iers_tab['MJD'], iers_tab['dY_2000A']) * u.arcsec
+    # Get the TIO locator s'
+    xp = numpy.interp(mjd, iers_tab['MJD'], iers_tab['PM_x']) * u.arcsec
+    yp = numpy.interp(mjd, iers_tab['MJD'], iers_tab['PM_y']) * u.arcsec
+        
+    posvel = numpy.zeros((TOAs.NumToa,6))
+    for ii in range(TOAs.NumToa):
+        obsname = TOAs.dataTable['obs'][ii]
+        # Get GCRS to CIRS matrix
+        X, Y, S = erfa.xys00a(*(tt[ii,0],tt[ii,1]))
+        rc2i = erfa.c2ixys(X+dX[ii].to(u.rad).value, Y+dY[ii].to(u.rad).value, S)    
+        
+        # Get the TIO locator s'
+        sp = erfa.sp00(*(tt[ii,0],tt[ii,1]))
+        #Get the polar motion matrix
+        rpm = erfa.pom00(xp[ii].to(u.rad).value, yp[ii].to(u.rad).value, sp)
+            
+        # Observatory XYZ coords in meters
+        xyz = getattr(TOAs,'xyz_'+obsname)
+        xyzm = [a.to(u.m).value for a in xyz]
+        x, y, z = erfa.trxp(rpm, xyzm)
+        
+        # Functions of earth Roatation Angle
+
+        theta = erfa.era00(*(ut1[ii,0],ut1[ii,1]))
+        s, c = math.sin(theta), math.cos(theta)
+            
+        # Position 
+
+        pos = numpy.asarray([c*x - s*y, s*x + c*y, z])
+        pos = erfa.trxp(rc2i,pos)*u.m
+
+        # Earth rotation rate in radians per UT1 second
+        OM = 1.00273781191135448 * 2 * math.pi / erfa.DAYSEC
+
+        # Velocity
+        vel = numpy.asarray([OM * (-s*x - c*y), OM * (c*x - s*y), 0.0])
+        vel = erfa.trxp(rc2i, vel)*u.m/u.s 
+        
+        posvel[ii] = numpy.hstack((pos,vel))
+    
+    return posvel  
+
+
+
+
+
+
