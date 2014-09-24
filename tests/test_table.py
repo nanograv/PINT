d2504d048ab543202b7ab91bc8145fc085f9cd23
Scott Ransom <sransom@nrao.edu>
2014-09-24 12:16:47 +0100

    Modified table test so that it will run

--- tests/test_table.py
+++ tests/test_table.py
@@ -5,10 +5,10 @@ import astropy.units as u
 import pint.models as tm
 from pint.phase import *
 
-tt = toaTable.TOAs("J1744-1134.Rcvr1_2.GASP.8y.x.tim")
+tt = toaTable.TOAs("tests/J1744-1134.Rcvr1_2.GASP.8y.x.tim")
 tt.compute_planet_posvel_c(planets=True)
 m = tm.StandardTimingModel()
-m.read_parfile('J1744-1134.basic.par')
+m.read_parfile('tests/J1744-1134.basic.par')
 p = m.phase_table(tt)
 t1 = time.time()
 
