cda31a9a994328ea3abc27b54b3c623729dce410
Scott Ransom <sransom@nrao.edu>
2014-11-28 15:40:49 +0000

    Converted so that it acts as a normal test now

--- tests/test_times.py
+++ tests/test_times.py
@@ -1,14 +1,26 @@
-from pint import toa, utils
+from pint import toa, utils, erfautils
+import pint.observatories as obsmod
 import math, shlex, subprocess, numpy
 import astropy.constants as const
 import astropy.units as u
 from pint.utils import PosVel
+from astropy import log
+
+log.setLevel('ERROR')
+# for nice output info, set the following instead
+#log.setLevel('INFO')
+
+observatories = obsmod.read_observatories()
 
 ls = u.def_unit('ls', const.c * 1.0 * u.s)
+
+log.info("Reading TOAs into PINT")
 ts = toa.get_TOAs("tests/testtimes.tim")
-ts.print_summary()
+if log.level < 25:
+    ts.print_summary()
 ts.table.sort('index')
 
+log.info("Calling TEMPO2")
 #cmd = 'tempo2 -output general2 -f tests/testtimes.par tests/testtimes.tim -s "XXX {clock0} {clock1} {clock2} {clock3} {tt} {t2tb} {telSSB} {telVel} {Ttt}\n"'
 cmd = 'tempo2 -output general2 -f tests/testtimes.par tests/testtimes.tim -s "XXX {clock0} {clock1} {clock2} {clock3} {tt} {t2tb} {earth_ssb1} {earth_ssb2} {earth_ssb3} {earth_ssb4} {earth_ssb5} {earth_ssb6} {telEpos} {telEVel} {Ttt}\n"'
 args = shlex.split(cmd)
@@ -30,32 +42,33 @@ for line, toa in zip(goodlines, ts.table):
                           numpy.asarray([ev0, ev1, ev2]) * ls/u.s)
     t2_opv = utils.PosVel(numpy.asarray([tp0, tp1, tp2]) * ls,
                           numpy.asarray([tv0, tv1, tv2]) * ls/u.s)
+
     t2_ssb2obs = t2_epv + t2_opv
     # print utils.time_toq_mjd_string(toa.mjd.tt), line.split()[-1]
     tempo_tt = utils.time_from_mjd_string(line.split()[-1], scale='tt')
     # Ensure that the clock corrections are accurate to better than 0.1 ns
     assert(math.fabs((oclk*u.s - toa['flags']["clkcorr"]).to(u.ns).value) < 0.1)
 
-    print "\nTOA in tt difference is:", \
-          ((toa['mjd'].tt - tempo_tt.tt).sec * u.s).to(u.ns)
+    log.info("TOA in tt difference is: %.2f ns" % \
+             ((toa['mjd'].tt - tempo_tt.tt).sec * u.s).to(u.ns).value)
 
-    #print "SSB-OBS:"
-    #print "   T2:", t2_epv.pos.to(ls), t2_epv.vel.to(ls/u.s)
-    #print " PINT:", toa['ssb_earth_pos'].to(ls), toa['ssb_earth_vel'].vel.to(ls/u.s)
-    #dssb = toa['earth_pvs'] - t2_epv
-    #print " diff:", dssb.pos.to(ls), dssb.vel.to(ls/u.s)
-
-    print "topocenter:"
-    print "   T2:", t2_ssb2obs.pos.to(u.m), t2_ssb2obs.vel.to(u.m/u.s)
-    print " PINT:", toa['ssb_obs_pos'], toa['ssb_obs_vel']
-    pint_ssb2obs = PosVel(toa['ssb_obs_pos']*u.km, toa['ssb_obs_vel']*u.km/u.s, origin='SSB', obj='OBS')
-    print pint_ssb2obs    
-    dgeo = pint_ssb2obs - t2_ssb2obs
-    xx = dgeo.pos / numpy.sqrt(numpy.dot(pint_ssb2obs.pos, pint_ssb2obs.pos))
-    xx = numpy.sqrt(numpy.dot(xx, xx))
-    print (xx * u.rad).to(u.arcsec)
-    print " diff:", dgeo.pos.to(u.m), dgeo.vel.to(u.m/u.s)
-
-    # Where in the heck is TEMPO2's ut1-utc correction?!?
-    #print toa.mjd.delta_ut1_utc[0], ut1_utc, ttcorr-tai_utc-tt_tai, \
-    #      toa.mjd.tdb.delta_tdb_tt[0], tt2tb-ttcorr
+    xyz = observatories[toa['obs']].loc.geocentric
+    pint_opv = erfautils.topo_posvels(xyz, toa)
+    #print " obs  T2:", t2_opv.pos.to(u.m).value, t2_opv.vel.to(u.m/u.s)
+    #print " obs PINT:", pint_opv.pos.to(u.m), pint_opv.vel.to(u.m/u.s)
+    dopv = pint_opv - t2_opv
+    dpos = numpy.sqrt(numpy.dot(dopv.pos.to(u.m), dopv.pos.to(u.m)))
+    dvel = numpy.sqrt(numpy.dot(dopv.vel.to(u.mm/u.s), dopv.vel.to(u.mm/u.s)))
+    log.info(" obs diff: %.2f m, %.3f mm/s" % (dpos, dvel))
+    assert(dpos < 2.0 and dvel < 0.02)
+    
+    pint_ssb2obs = PosVel(numpy.asarray(toa['ssb_obs_pos'])*u.km,
+                          numpy.asarray(toa['ssb_obs_vel'])*u.km/u.s,
+                          origin='SSB', obj='OBS')
+    #print " topo  T2:", t2_ssb2obs.pos.to(u.km), t2_ssb2obs.vel.to(u.km/u.s)
+    #print " topo PINT:", pint_ssb2obs.pos.to(u.km), pint_ssb2obs.vel.to(u.km/u.s)
+    dtopo = pint_ssb2obs - t2_ssb2obs
+    dpos = numpy.sqrt(numpy.dot(dtopo.pos.to(u.m), dtopo.pos.to(u.m)))
+    dvel = numpy.sqrt(numpy.dot(dtopo.vel.to(u.mm/u.s), dtopo.vel.to(u.mm/u.s)))
+    log.info(" topo diff: %.2f m, %.3f m/s" % (dpos, dvel))
+    assert(dpos < 2.0 and dvel < 0.02)
