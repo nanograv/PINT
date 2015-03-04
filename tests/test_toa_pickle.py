0f0210ea0a8d3e461c176b13f600601e9b7df624
Paul Ray <paul.ray@nrl.navy.mil>
2015-03-03 19:39:54 -0500

    Added test specifically for pickling/unpickling TOAs, which fails on Mac OS X for some reason.

---
+++ tests/test_toa_pickle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from pint import toa
+import os
+
+testdir=os.path.join(os.getenv('PINT'),'tests');
+os.chdir(testdir)
+
+class TestTOAReader:
+    def setUp(self):
+        # First, read the TOAs from the tim file.
+        # This should also create the pickle file.
+        try:
+            os.remove('test1.tim.pickle.gz')
+            os.remove('test1.tim.pickle')
+        except OSError:
+            pass
+        tt = toa.get_TOAs("test1.tim",usepickle=False)
+        self.numtoas = tt.ntoas
+        del tt
+        # Now read them from the pickle
+        self.t = toa.get_TOAs("test1.tim",usepickle=True)
+
+    def test_pickle(self):
+        # Initially this just checks that the same number
+        # of TOAs came out of the pickle as went in.
+        assert self.t.ntoas == self.numtoas
+
+if __name__ == '__main__':
+    t = TestTOAReader()
+    t.setUp()
+    print 'Tests are set up.'
+    
+    t.test_pickle()
+
