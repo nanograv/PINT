529197918f85fe7c2a909ac73e6738ccea3fdfe5
Paul Ray <paul.ray@nrl.navy.mil>
2015-03-03 19:53:20 -0500

    A couple of more fixes to test codes (removing pickle from test_toa_reader and fixing units in test_fitter)

--- tests/test_toa_reader.py
+++ tests/test_toa_reader.py
@@ -6,7 +6,7 @@ os.chdir(testdir)
 
 class TestTOAReader:
     def setUp(self):
-        self.x = toa.TOAs("test1.tim")
+        self.x = toa.TOAs("test1.tim",usepickle=False)
         self.x.apply_clock_corrections()
         self.x.compute_TDBs()
 
