9ab69db5b7ed50032a447e0aef59897acb1a91f1
luojing1211 <jingluo@Jings-MacBook-Air-2.local>
2015-07-14 00:02:40 -0500

    Add get_model function. optimize the model building path

--- pint/models/model_builder.py
+++ pint/models/model_builder.py
@@ -23,16 +23,20 @@ class model_builder(object):
         Examples:
         ---------
         Read model from parfile :
-        mc = model_constructor("PulsarJ1955", parfile ="J1955.par" )
-        psrJ1955 = mc.model_instance
+        >>>mb = model_builder("PulsarJ1955", parfile ="J1955.par" )
+        >>>psrJ1955 = mb.model_instance
 
-        Build model from sketch:
-        from .bt import BT 
-        mc = model_constructor("BT_model")
-        mc.add_components(BT)
-        model = mc.build_model()
-        MyModer = model()
-        MyModel.read_parfile('parfile')
+        Build model from sketch and read parfile:
+        >>>from .bt import BT 
+        >>>mb = model_builder("BT_model")
+        >>>mb.add_components(BT)
+        >>>psrJ1955 = mb.get_model_instance('J1955.par')
+
+        Build model instance without reading parfile:
+        >>>mb = model_builder("BT_model")
+        >>>mb.add_components(BT)
+        >>>myModel = mb.get_model_instance()
+
     """
     def __init__(self, name, parfile = None):
         self.name = name
@@ -42,10 +46,8 @@ class model_builder(object):
         self.select_comp = []
         if parfile is not None:
             self.parfile = parfile
-            self.get_model_from_parfile(parfile)
-            model = self.build_model()
-            self.model_instance = model()
-            self.model_instance.read_parfile(parfile)
+            self.get_comp_from_parfile(self.parfile)
+            self.get_model_instance(self.parfile)
 
     def __str__(self):
         result = 'Model name : ' + self.name + '\n'
@@ -80,7 +82,7 @@ class model_builder(object):
         return self.param_inparF
 
 
-    def get_model_from_parfile(self,parfile):
+    def get_comp_from_parfile(self,parfile):
         """Check all the components if it is needed from parfile
            Put the needed on in the selected components list
         """
@@ -109,3 +111,37 @@ class model_builder(object):
         for c in components:
     	    if c not in self.select_comp:
     	       self.select_comp.append(c)
+
+    def get_model_instance(self,parfile=None):
+        """Read parfile using the model_instance attribute.
+            Parameters
+            ---------
+            parfile : str optional
+                The parfile name
+        """
+        if self.model_instance is None:
+            model = self.build_model()
+
+        self.model_instance = model()
+        if parfile is not None:
+            self.model_instance.read_parfile(parfile)
+
+        return self.model_instance
+
+
+def get_model(name, parfile):
+    """A one step function to build model from a parfile
+        Parameters
+        ---------
+        name : str
+            Name for the model.
+        parfile : str
+            The parfile name
+
+        Return
+        ---------
+        Model instance get from parfile.  
+    """
+
+    mb = model_builder(name,parfile)
+    return mb.model_instance
