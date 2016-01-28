65b4ef364ca516ae633d826a6ce0fa8237096182
luojing1211 <jingluo@Jings-MacBook-Air-4.local>
2016-01-28 00:24:40 -0600

    update test file

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -13,19 +13,24 @@ class TestParameters(unittest.TestCase):
         self.m = mb.get_model('B1855+09_NANOGrav_dfg+12_modified.par')
     def test_RAJ(self):
         print "RAJ"
-        assert self.m.RAJ.base_unit == u.hourangle
-        assert self.m.RAJ.base_value == 18.960109246777776
+        assert self.m.RAJ.num_unit == u.hourangle
+        assert self.m.RAJ.num_value == 18.960109246777776
     def test_DECJ(self):
-        assert self.m.DECJ.base_unit == u.deg
-        assert self.m.DECJ.base_value == 9.72146998888889
-    def set_base_unit(self):
-        self.m.RAJ.base_unit = u.m
-    def test_base_unit(self):
-        self.assertRaises(AttributeError, self.set_base_unit)
-    def set_base_value(self):
-        self.m.RAJ.base_value = u.m
-    def test_base_value(self):
-        self.assertRaises(ValueError, self.set_base_value)
+        assert self.m.DECJ.num_unit == u.deg
+        assert self.m.DECJ.num_value == 9.72146998888889
+    def set_num_unit(self):
+        self.m.RAJ.num_unit = u.m
+    def test_num_unit(self):
+        self.assertRaises(AttributeError, self.set_num_unit)
+    def set_num_value(self):
+        self.m.RAJ.num_value = u.m
+        self.m.RAJ.num_value = 1*u.m
+        self.m.T0.num_value = 50044.3322
+        self.m.T0.num_value = None
+        self.m.T0.num_value = 'this is a string'
+    def test_num_value(self):
+        self.assertRaises(ValueError, self.set_num_value)
+
 
 if __name__ == '__main__':
     pass
