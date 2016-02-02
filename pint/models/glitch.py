520c7f8e4ba3ec3385cd8f26a0d8c85a67dc1699
luojing1211 <jingluo@Jings-MacBook-Air-4.local>
2016-02-02 00:11:55 -0600

    glicth parameter does not require to be continues

--- pint/models/glitch.py
+++ pint/models/glitch.py
@@ -50,29 +50,38 @@ class Glitch(TimingModel):
         super(Glitch, self).setup()
         # Check for required params, Check for Glitch numbers
         self.num_glitches = self.num_prefix_params['GLPH_']
-        # check if glitch phase is continuous
-        ps = ["GLPH_", "GLEP_", "GLF0_", "GLF1_"]
-        for ii in range(1,self.num_glitches+1):
-            glcname = 'GLPH_%d'%ii
-            if glcname not in self.params:
-                msg = 'Index of glitch prefix parameter should be continuous.'
-                raise MissingParameter("Glitch", glcname,msg)
-            # Check if the other glicth parameter exist
-            for p in ps:
-                glcparam = p+'%d'%ii
-                if glcparam not in self.params:
-                    raise MissingParameter("Glitch", glcparam)
+        glphparams = [x for x in self.params if x.startswith('GLPH_')]
+        # check if glitch phase matches GLEP, GLF0, GLF1
+        #ps = ["GLPH_", "GLEP_", "GLF0_", "GLF1_"]
+        for glphnm in glphparams:
+            glphpar = getattr(self,glphnm)
+            idx = glphpar.index
+            if not hasattr(self,'GLEP_%d'%idx): # Check if epoch is given.
+                msg = 'Glicth Epoch is needed for Glicth %d.'%idx
+                raise MissingParameter("Glitch", 'GLEP_%d'%idx, msg)
+            # Check if the other glicth F0 and F1 exists, if not add as zero parameter
+            for glf in ["GLF0_", "GLF1_"]:
+                if not hasattr(self,glf+'%d'%idx):
+                    glf0exp = getattr(self,glf+'1') # The first glf0 and glf1 should be there
+                    add_param(glf0exp.new_index_prefix_param(idx))
+                    getattr(self,plf+"%d"%idx).value = 0.0
 
 
-        # Check the Decay Term. If not match number of glitch. If not in parfile. add zeros. 
-        ps2 = [ "GLF0D_", "GLTD_"]
-        for p in ps2:
-            pname = [x for x in self.params if x.startswith(p) ]
-            for ii in range(1,self.num_glitches+1):
-                if p+"%d"%ii not in self.params:
-                    pfxpar = getattr(self,pname[0])
-                    self.add_param(pfxpar.new_index_prefix_param(ii))
-                    getattr(self,p+"%d"%ii).value = 0.0
+
+        # Check the Decay Term.
+        glf0dparams =  [x for x in self.params if x.startswith('GLF0D_')]
+        for glf0d in glf0dparams:
+            df0d = getattr(self,glf0d)
+            idx = df0d.index
+            if not hasattr(self,'GLPH_%d'%idx): # Check if the decay belongs to a glitch
+                msg = 'Glitch frequency decay index has to match one glicth phase'
+                msg += ' index'
+                raise MissingParameter("Glitch", 'GLPH_%d'%idx, msg)
+
+            if not hasattr(self,'GLTD_%d'%idx):
+                if df0d.value != 0.0:
+                    msg = 'None zero GLTD_%d parameter needs a GLTD_%d parameter'%(idx,idx)
+                    raise MissingParameter("Glitch", 'GLTD_%d'%idx, msg)
 
 
     def glitch_phase(self, toas, delay):
@@ -84,17 +93,23 @@ class Glitch(TimingModel):
         returns an array of phases in long double
         """
         phs = numpy.zeros_like(toas, dtype=numpy.longdouble)
-        for ii in range(1, self.num_glitches + 1):
-            dphs = getattr(self, "GLPH_%d"%ii).value
-            dF0 = getattr(self, "GLF0_%d"%ii).value
-            dF1 = getattr(self, "GLF1_%d"%ii).value
-            eph = time_to_longdouble(getattr(self, "GLEP_%d"%ii).value)
+        glphnames= [x for x in self.params if x.startswith('GLPH_')]
+        for glphnm in glphnames:
+            glph = getattr(self,glphnm)
+            dphs = glph.value
+            idx  = glph.index
+            dF0 = getattr(self, "GLF0_%d"%idx).value
+            dF1 = getattr(self, "GLF1_%d"%idx).value
+            eph = time_to_longdouble(getattr(self, "GLEP_%d"%idx).value)
             dt = (toas['tdbld'] - eph) * SECS_PER_DAY - delay
             affected = dt > 0.0 # TOAs affected by glitch
-            dF0D = getattr(self, "GLF0D_%d"%ii).value
-            if dF0D != 0.0:
-                tau = getattr(self, "GLTD_%d"%ii).value * SECS_PER_DAY
-                decayterm = dF0D * tau * (1.0 - numpy.exp(-dt[affected]/tau))
+            if hasattr(self,"GLF0D_%d"%idx):
+                dF0D = getattr(self, "GLF0D_%d"%idx).value
+                if dF0D != 0.0:
+                    tau = getattr(self, "GLTD_%d"%idx).value * SECS_PER_DAY
+                    decayterm = dF0D * tau * (1.0 - numpy.exp(-dt[affected]/tau))
+                else:
+                    decayterm = 0.0
             else:
                 decayterm = 0.0
             # print ii, dphs, dF0, dt[:5], len(dt[affected]), len(phs[affected])
