9927137912865eab9344e975e3f3a6aa516cc034
Paul Ray <paul.ray@nrl.navy.mil>
2016-02-16 18:04:29 -0500

    Fixed priors unit test and made a couple other small changes

--- tests/test_priors.py
+++ tests/test_priors.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python
 from __future__ import division, absolute_import, print_function
 
+import unittest
 import pint.models
 from pint.models.priors import *
+from scipy.stats import norm
+import os
 
-m = pint.models.get_model('examples/NGC6440E.par')
+testdir=os.path.join(os.getenv('PINT'),'tests');
+datadir = os.path.join(testdir,'datafile')
+os.chdir(datadir)
 
-print("F0 is {0}".format(m.F0.num_value))
-# Check with uniform prior
-print("Uniform Prior on F0 {0}".format(m.F0.prior_probability()))
+class TestPriors(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.m = pint.models.get_model('B1855+09_NANOGrav_dfg+12_modified.par')
 
-# Now add a gaussian prior with mean 61.0 and sigma 2.0
-m.F0.prior = GaussianPrior(61.0,2.0)
-print("Gaussian Prior on F0 {0}".format(m.F0.prior_probability()))
 
-# Now test a value before setting
-print("Test Gaussian Prior of F0 {0}".format(m.F0.prior_probability(value=63.0)))
+    def test_uniform(self):
+        print("test_uniform")
+        assert self.m.F0.prior_pdf() == 1.0
+        assert self.m.F0.prior_pdf(logpdf=True) == 0.0
 
+#    def test_uniform_bounded(self):
+#        print("test_uniform_bounded")    
+
+if __name__ == '__main__':
+    unittest.main()
