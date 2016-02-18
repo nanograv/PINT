ae2e310edb4e242f1a143ec2f8a276c4ff6b284a
luojing1211 <jingluo@Jings-MacBook-Air-4.local>
2016-02-18 10:57:10 -0600

    add docstring

--- pint/models/pint_pulsar_binary.py
+++ pint/models/pint_pulsar_binary.py
@@ -100,7 +100,10 @@ class PSRbinaryWapper(TimingModel):
     def setup(self):
         super(PSRbinaryWapper, self).setup()
 
+    # With new parameter class set up, do we need this?
     def apply_units(self):
+        """Apply units to parameter value.
+        """
         for bpar in self.binary_params:
             bparObj = getattr(self,bpar)
             if bparObj.num_value is None or bparObj.num_unit is None:
@@ -122,7 +125,7 @@ class PSRbinaryWapper(TimingModel):
             if par in binobj.param_aliases.keys():
                 aliase = binobj.param_aliases[par]
             if hasattr(self, par) or \
-                list(set(aliase).intersection(self.params)!=[]) :
+                list(set(aliase).intersection(self.params))!=[] :
                 binObjpar = getattr(self, par)
                 if binObjpar.num_value is None:
                     continue
@@ -138,20 +141,20 @@ class PSRbinaryWapper(TimingModel):
 
         return bmob.binary_delay()
 
-
-    def binary_delay(self,toas):
-        """Returns total pulsar binary delay.
-           Parameters
-           ----------
-           toas : PINT toas table
-           Return
-           ----------
-           Pulsar binary delay in the units of second
-        """
-        bdelay = np.longdouble(np.zeros(len(toas)))*u.s
-        for bdf in self.binary_delay_funcs:
-            bdelay+= bdf(toas)
-        return bdelay
+    #
+    # def binary_delay(self,toas):
+    #     """Returns total pulsar binary delay.
+    #        Parameters
+    #        ----------
+    #        toas : PINT toas table
+    #        Return
+    #        ----------
+    #        Pulsar binary delay in the units of second
+    #     """
+    #     bdelay = np.longdouble(np.zeros(len(toas)))*u.s
+    #     for bdf in self.binary_delay_funcs:
+    #         bdelay+= bdf(toas)
+    #     return bdelay
 
     @Cache.use_cache
     def d_delay_d_xxxx(self,param,toas):
