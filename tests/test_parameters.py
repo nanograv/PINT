b2d4a9ce2b25a9451dbe79c5838257ff55de2bc5
Rutger van Haasteren <vhaasteren@gmail.com>
2016-02-23 10:55:07 -0800

    Clarified some parameter tests

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -1,5 +1,6 @@
 from pint.models import parameter as p
 from pint.models import model_builder as mb
+from pint.utils import str2longdouble
 from astropy.coordinates.angles import Angle
 import astropy.time as time
 import astropy.units as u
@@ -15,57 +16,101 @@ class TestParameters(unittest.TestCase):
         self.m = mb.get_model('B1855+09_NANOGrav_dfg+12_modified.par')
         self.mp = mb.get_model('prefixtest.par')
     def test_RAJ(self):
-        print "RAJ"
-        assert self.m.RAJ.num_unit == u.hourangle
-        assert self.m.RAJ.num_value == 18.960109246777776
+        """Check whether the value and units of RAJ parameter are ok"""
+        num_unit = u.hourangle
+        num_value = 18.960109246777776
+
+        self.assertEqual(self.m.RAJ.num_unit, num_unit)
+        self.assertEqual(self.m.RAJ.num_value, num_value)
+        self.assertEqual(self.m.RAJ.value, num_value * num_unit)
 
     def test_DECJ(self):
-        assert self.m.DECJ.num_unit == u.deg
-        assert self.m.DECJ.num_value == 9.72146998888889
+        """Check whether the value and units of DECJ parameter are ok"""
+        num_unit = u.deg
+        num_value = 9.72146998888889
+        self.assertEqual(self.m.DECJ.num_unit, num_unit)
+        self.assertEqual(self.m.DECJ.num_value, num_value)
+        self.assertEqual(self.m.DECJ.value, num_value*num_unit)
 
-    def set_num_unit(self):
+    def test_F0(self):
+        """Check whether the value and units of F0 parameter are ok"""
+        num_unit = u.Hz
+        num_value = str2longdouble('186.49408156698235146')
+
+        self.assertEqual(self.m.F0.num_unit, num_unit)
+        self.assertEqual(self.m.F0.num_value, num_value)
+        self.assertEqual(self.m.F0.value, num_value*num_unit)
+
+    def set_num_unit_fail(self):
+        """Setting the unit to a non-compatible unit should fail """
         self.m.RAJ.num_unit = u.m
+
+    def set_num_unit_pass_identical(self):
+        """Setting the unit to the same unit as the base unit should pass """
+        #self.m.RAJ.num_unit = u.hourangle
+        pass
+
+    def set_num_unit_pass_compatible(self):
+        """Setting the unit to a compatible unit should pass """
+        #self.m.RAJ.num_unit = u.deg
+        pass
+
     def test_num_unit(self):
-        self.assertRaises(AttributeError, self.set_num_unit)
+        """Test setting the units """
+        self.assertRaises(AttributeError, self.set_num_unit_fail)
+        self.set_num_unit_pass_identical()
+        self.set_num_unit_pass_compatible()
 
-    def set_num_value1(self):
+    def set_num_to_unit(self):
+        """Try to set the numerical value to a unit """
         self.m.RAJ.num_value = u.m
-    def test_num_value1(self):
-        self.assertRaises(ValueError, self.set_num_value1)
 
-    def set_num_value2(self):
-        self.m.RAJ.num_value = 1*u.m
-    def test_num_value2(self):
-        self.assertRaises(ValueError, self.set_num_value2)
+    def set_num_to_quantity(self):
+        """Try to set the numerical value to a quantity """
+        self.m.RAJ.num_value = 1.0*u.m
 
-    def test_num_value3(self):
+    def test_set_num_value(self):
+        """Try to set the numerical value of a parameter to various things"""
+        self.assertRaises(ValueError, self.set_num_to_unit)
+        self.assertRaises(ValueError, self.set_num_to_quantity)
+
+    def test_T0(self):
+        """Test setting T0 to a test value"""
         self.m.T0.num_value = 50044.3322
         # I don't understand why this is failing...  something about float128
-        assert numpy.isclose(self.m.T0.num_value, 50044.3322)
+        # Does not fail for me (both lines)  -- RvH 02/22/2015
+        self.assertTrue(numpy.isclose(self.m.T0.num_value, 50044.3322))
+        self.assertEqual(self.m.T0.num_value, 50044.3322)
 
-    def set_num_value4(self):
+    def set_num_to_none(self):
+        """Set T0 to None"""
         self.m.T0.num_value = None
-    def test_num_value4(self):
-        self.assertRaises(ValueError, self.set_num_value4)
 
-    def set_num_value5(self):
+    def set_num_to_string(self):
+        """Set T0 to a string"""
         self.m.T0.num_value = 'this is a string'
-    def test_num_value5(self):
-        self.assertRaises(ValueError, self.set_num_value5)
 
-    def test_value1(self):
-        self.m.OM.value = 10.0*u.deg
-        assert self.m.OM.value == 10.0 * u.deg
+    def test_num_to_other(self):
+        """Test setting the T0 numerical value to a not-number"""
+        self.assertRaises(ValueError, self.set_num_to_none)
+        self.assertRaises(ValueError, self.set_num_to_string)
 
-    def set_value2(self):
+    def set_OM_to_none(self):
+        """Set OM to None"""
         self.m.OM.value = None
-    def test_value2(self):
-        self.assertRaises(ValueError, self.set_value2)
 
-    def set_value3(self):
+    def set_OM_to_time(self):
+        """Set OM to a time"""
         self.m.OM.value = time.Time(54000,format = 'mjd')
-    def test_value3(self):
-        self.assertRaises(ValueError, self.set_value3)
+
+    def test_OM(self):
+        """Test doing stuff to OM"""
+        value = 10.0 * u.deg
+        self.m.OM.value = value
+
+        self.assertEqual(self.OM.value, value)
+        self.assertRaises(ValueError, self.set_OM_to_none)
+        self.assertRaises(ValueError, self.set_OM_to_time)
 
     def test_prefix_value1(self):
         self.mp.GLF0_2.value = 50
