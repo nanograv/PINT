ca2afac9163dca1c0291ed229757f5edd25b5234
luojing1211 <jingluo@Jings-MacBook-Air-4.local>
2016-02-24 10:33:02 -0600

    add test file

--- tests/test_d_phase_d_toa.py
+++ tests/test_d_phase_d_toa.py
@@ -1,7 +1,10 @@
 from pint.models import model_builder as mb
 import pint.toa as toa
 import matplotlib.pyplot as plt
+import astropy.units as u
+import astropy.time as time
 import unittest
+import copy
 import os
 
 datapath = os.path.join(os.environ['PINT'], 'tests', 'datafile')
@@ -16,20 +19,56 @@ class TestD_phase_D_toa(unittest.TestCase):
         self.toas = toa.get_TOAs(self.timf)
 
     def test_d_phase_d_toa(self):
-        print 'test_d_phase_d_toa at observatory.'
-        dpdtoa = self.model.d_phase_d_toa(self.toas)
-        plt.plot(self.toas.get_mjds(),dpdtoa)
-        plt.show()
+        dpdtoa_model = self.model.d_phase_d_toa(self.toas)
+        dpdtoa_full = self.d_phase_d_toa_full()
+        dpdtoa_basic = self.d_phase_d_toa_basic()
+        diff_m_f = dpdtoa_model - dpdtoa_full
+        diff_m_b = dpdtoa_model - dpdtoa_basic
+        max_diff_m_f = max(diff_m_f)
+        max_diff_m_b = max(diff_m_b)
+        emsg = str(max_diff_m_f) + "is bigger then 1e-8"
+        assert max_diff_m_f < 1e-8, emsg
+    def d_phase_d_toa_full(self):
+        print "Test with full ssb_obs_pos calculation."
+        copy_toas = copy.deepcopy(self.toas)
+        pulse_period = 1.0/self.model.F0.value
+        sample_step = pulse_period/10.0
+        sample_dt = [-sample_step,2 * sample_step]
 
-        print 'test_d_phase_d_toa at SSB.'
-        dpdtoa = self.model.d_phase_d_toa(self.toas, obs='SSB')
-        plt.plot(self.toas.get_mjds(),dpdtoa)
-        plt.show()
+        sample_phase = []
+        for dt in sample_dt:
+            dt_array = ([dt] * copy_toas.ntoas) * u.s
+            deltaT = time.TimeDelta(dt_array)
+            copy_toas.adjust_TOAs(deltaT, get_posvel=True)
+            phase = self.model.phase(copy_toas.table)
+            sample_phase.append(phase)
+        # Use finite difference method.
+        # phase'(t) = (phase(t+h)-phase(t-h))/2+ 1/6*F2*h^2 + ..
+        # The error should be near 1/6*F2*h^2
+        dp = (sample_phase[1]-sample_phase[0])
+        d_phase_d_toa = dp.int/(2*sample_step)+dp.frac/(2*sample_step)
+        return d_phase_d_toa
 
-        print 'test d_phase_d_toa as geocenter'
-        dpdtoa = self.model.d_phase_d_toa(self.toas, obs='GEO')
-        plt.plot(self.toas.get_mjds(),dpdtoa)
-        plt.show()
+    def d_phase_d_toa_basic(self):
+        print "Test with without ssb_obs_pos calculation."
+        copy_toas = copy.deepcopy(self.toas)
+        pulse_period = 1.0/self.model.F0.value
+        sample_step = pulse_period/10.0
+        sample_dt = [-sample_step,2 * sample_step]
+
+        sample_phase = []
+        for dt in sample_dt:
+            dt_array = ([dt] * copy_toas.ntoas) * u.s
+            deltaT = time.TimeDelta(dt_array)
+            copy_toas.adjust_TOAs(deltaT)
+            phase = self.model.phase(copy_toas.table)
+            sample_phase.append(phase)
+        # Use finite difference method.
+        # phase'(t) = (phase(t+h)-phase(t-h))/2+ 1/6*F2*h^2 + ..
+        # The error should be near 1/6*F2*h^2
+        dp = (sample_phase[1]-sample_phase[0])
+        d_phase_d_toa = dp.int/(2*sample_step)+dp.frac/(2*sample_step)
+        return d_phase_d_toa
 
 if __name__ == '__main__':
     pass
