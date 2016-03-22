1f547404d1fec78edd5d9f45f19f121bd18d47c5
luojing1211 <jingluo@Jings-MacBook-Air-4.local>
2016-03-22 11:39:16 -0500

    add tempo test data file

--- tests/test_dd.py
+++ tests/test_dd.py
@@ -2,7 +2,6 @@
 import pint.models.model_builder as mb
 import pint.toa as toa
 import matplotlib.pyplot as plt
-import tempo2_utils
 import astropy.units as u
 from pint.residuals import resids
 from pint.models.pint_dd_model import DDwrapper
@@ -16,7 +15,7 @@ class TestDD(unittest.TestCase):
     """Compare delays from the dd model with libstempo and PINT"""
     @classmethod
     def setUpClass(self):
-        self.parfileB1855 = 'B1855+09_NANOGrav_dfg+12_modified.par'
+        self.parfileB1855 = 'B1855+09_NANOGrav_dfg+12_modified_DD.par'
         self.timB1855 = 'B1855+09_NANOGrav_dfg+12.tim'
         self.toasB1855 = toa.get_TOAs(self.timB1855, ephem="DE405", planets=False)
         self.modelB1855 = mb.get_model(self.parfileB1855)
@@ -25,7 +24,7 @@ class TestDD(unittest.TestCase):
     def test_J1855_binary_delay(self):
         # Calculate delays with PINT
         pint_binary_delay = self.modelB1855.binary_delay(self.toasB1855.table)
-        assert np.all(np.abs(pint_binary_delay.value + self.ltbindelay) < 1e-11), 'DD J1955 TEST FAILED'
+        assert np.all(np.abs(pint_binary_delay.value + self.ltbindelay) < 1e-11), 'DD B1855 TEST FAILED'
     # TODO: PINT can still incresase the precision by adding more components
     def test_B1855(self):
         pint_resids_us = resids(self.toasB1855, self.modelB1855).time_resids.to(u.s)
