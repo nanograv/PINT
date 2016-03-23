0d91f19a52a8aa05416a9e688e31070f68d8cb97
luojing1211 <jingluo@Jings-MacBook-Air-4.local>
2016-03-23 11:01:04 -0500

    FD delay test independent from tempo

--- tests/test_fd.py
+++ tests/test_fd.py
@@ -1,28 +1,31 @@
 """Various tests to assess the performance of the FD model."""
 import pint.models.model_builder as mb
 import pint.toa as toa
-import libstempo as lt
-import matplotlib.pyplot as plt
 import astropy.units as u
 import pint.residuals
 import numpy as np
 import os, unittest
-datapath = os.path.join(os.environ['PINT'], 'tests', 'datafile')
+from pinttestdata import testdir, datadir
+os.chdir(datadir)
 
 class TestFD(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.parf = os.path.join(datapath, 'test_FD.par')
-        self.timf = os.path.join(datapath, 'test_FD.simulate.pint_corrected')
+        self.parf = 'test_FD.par'
+        self.timf = 'test_FD.simulate.pint_corrected'
         self.FDm = mb.get_model(self.parf)
         self.toas = toa.get_TOAs(self.timf)
-
+        # libstempo result
+        self.ltres, self.ltbindelay = np.genfromtxt(self.parf + '.tempo_test', unpack=True)
     def test_DMX(self):
         print "Testing FD module."
         rs = pint.residuals.resids(self.toas, self.FDm).time_resids.to(u.s).value
-        psr = lt.tempopulsar(self.parf, self.timf)
-        resDiff = rs-psr.residuals()
-        #TODO : this percision should be increased.
+        resDiff = rs - self.ltres
+        #NOTE : This prescision is a lower then 1e-7 seconds level, due to some
+        # early parks clock corrections are treated differently.
+        # TEMPO2: Clock correction = clock0 + clock1 (in the format of general2)
+        # PINT : Clock correction = toas.table['flags']['clkcorr']
+        # Those two clock correction difference are causing the trouble. 
         assert np.all(resDiff< 5e-6) , \
             "PINT and tempo Residual difference is too big. "
 
