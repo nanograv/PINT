8f9e60b35cabc704b11d7c5698cde4a88c96f0f3
luojing1211 <jingluo@Jings-MacBook-Air-4.local>
2016-03-29 13:53:33 -0500

    Change BT model to generic form

---
+++ pint/models/pulsar_binaries/BT_model.py
@@ -0,0 +1,10 @@
+from .pulsar_binary import PSR_BINARY
+from pint.models.timing_model import Cache
+import numpy as np
+import astropy.units as u
+import astropy.constants as c
+from pint import ls,GMsun,Tsun
+
+
+class DDmodel(PSR_BINARY):
+    pass 
