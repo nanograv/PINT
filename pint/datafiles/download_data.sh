21e8cfa75a2f4f664947b8b9f3a9988fd04427fd
luojing1211 <jingluo@Jings-MacBook-Air-4.local>
2016-08-08 13:40:34 -0500

    update leapsecond

--- pint/datafiles/download_data.sh
+++ pint/datafiles/download_data.sh
@@ -4,4 +4,5 @@ wget -N -c ftp://ssd.jpl.nasa.gov/pub/eph/planets/bsp/de430t.bsp
 wget -N -c http://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/earth_latest_high_prec.bpc
 wget -N -c http://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/pck00010.tpc
 wget -N -c http://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/naif0011.tls
+wget -N  -c http://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/naif0012.tls
 wget -N -c http://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/de-403-masses.tpc
