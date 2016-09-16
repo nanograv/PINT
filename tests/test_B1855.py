a8f8ec05d31bfb7a5b24e10f0d5eab5d8e461181
luojing1211 <jingluo@Jings-MacBook-Air-4.local>
2016-09-16 10:35:13 -0600

    remove test dependency and add test J0613

--- tests/test_B1855.py
+++ tests/test_B1855.py
@@ -11,23 +11,25 @@ from pinttestdata import testdir, datadir
 os.chdir(datadir)
 
 class TestB1855(unittest.TestCase):
-    """Compare delays from the dd model with libstempo and PINT"""
+    """Compare delays from the dd model with tempo and PINT"""
     @classmethod
     def setUpClass(self):
         self.parfileB1855 = 'B1855+09_NANOGrav_dfg+12_TAI.par'
         self.timB1855 = 'B1855+09_NANOGrav_dfg+12.tim'
         self.toasB1855 = toa.get_TOAs(self.timB1855, ephem="DE405", planets=False)
         self.modelB1855 = mb.get_model(self.parfileB1855)
-        # libstempo result
-        self.ltres, self.ltbindelay = np.genfromtxt(self.parfileB1855 + '.tempo_test', unpack=True)
-    def test_J1855_binary_delay(self):
+        # tempo result
+        self.ltres, self.ltbindelay = np.genfromtxt(self.parfileB1855 + \
+                                     '.tempo_test',skip_header=1, unpack=True)
+        print self.ltres
+    def test_B1855_binary_delay(self):
         # Calculate delays with PINT
         pint_binary_delay = self.modelB1855.binarymodel_delay(self.toasB1855.table)
-        assert np.all(np.abs(pint_binary_delay.value + self.ltbindelay) < 1e-11), 'B1855 binary delay test failed.'
+        assert np.all(np.abs(pint_binary_delay.value + self.ltbindelay) < 1e-8), 'B1855 binary delay test failed.'
 
     def test_B1855(self):
         pint_resids_us = resids(self.toasB1855, self.modelB1855).time_resids.to(u.s)
-        assert np.all(np.abs(pint_resids_us.value - self.ltres) < 4e-8), 'B1855 residuals test failed.'
+        assert np.all(np.abs(pint_resids_us.value - self.ltres) < 3e-8), 'B1855 residuals test failed.'
 
 
 if __name__ == '__main__':
---
+++ tests/test_J0613.py
@@ -0,0 +1,35 @@
+"""Various tests to assess the performance of the J0623-0200."""
+import pint.models.model_builder as mb
+import pint.toa as toa
+import libstempo as lt
+import matplotlib.pyplot as plt
+import tempo2_utils
+import astropy.units as u
+from pint.residuals import resids
+import numpy as np
+import os, unittest
+
+from pinttestdata import testdir, datadir
+
+# Using Nanograv data J0623-0200
+parfile = os.path.join(datadir, 'J0613-0200_NANOGrav_dfg+12_TAI.par')
+timfile = os.path.join(datadir, 'J0613-0200_NANOGrav_dfg+12.tim')
+
+# libstempo calculation
+print "libstempo calculation"
+psr = lt.tempopulsar(parfile, timfile)
+# Build PINT model
+print "PINT calculation"
+mdd = mb.get_model(parfile)
+# Get toas to pint
+toas = toa.get_TOAs(timfile, planets=False, ephem='DE405')
+tt = toas.table
+
+t2_resids = psr.residuals()
+presids_us = resids(toas, mdd).time_resids
+diff = (presids_us - t2_resids * u.second).to(u.us)
+plt.plot(toas.get_mjds(high_precision=False), diff, '+')
+plt.xlabel('Mjd (DAY)')
+plt.ylabel('residule difference (us)')
+plt.title('Residule difference between PINT and tempo2')
+plt.show()
