de173169b4f43ad38345d7a27dba113dc9cf173d
luojing1211 <jingluo@Jings-MacBook-Air-4.local>
2016-09-16 14:13:37 -0600

    Remove test dependency of libstempo and matplotlib

--- tests/test_B1855.py
+++ tests/test_B1855.py
@@ -14,13 +14,13 @@ class TestB1855(unittest.TestCase):
     """Compare delays from the dd model with tempo and PINT"""
     @classmethod
     def setUpClass(self):
-        self.parfileB1855 = 'B1855+09_NANOGrav_dfg+12_TAI.par'
+        self.parfileB1855 = 'B1855+09_NANOGrav_dfg+12_TAI_FB90.par'
         self.timB1855 = 'B1855+09_NANOGrav_dfg+12.tim'
         self.toasB1855 = toa.get_TOAs(self.timB1855, ephem="DE405", planets=False)
         self.modelB1855 = mb.get_model(self.parfileB1855)
         # tempo result
         self.ltres, self.ltbindelay = np.genfromtxt(self.parfileB1855 + \
-                                     '.tempo_test',skip_header=1, unpack=True)
+                                     '.tempo2_test',skip_header=1, unpack=True)
         print self.ltres
     def test_B1855_binary_delay(self):
         # Calculate delays with PINT
@@ -29,6 +29,7 @@ class TestB1855(unittest.TestCase):
 
     def test_B1855(self):
         pint_resids_us = resids(self.toasB1855, self.modelB1855).time_resids.to(u.s)
+        # Due to the gps2utc clock correction. We are at 3e-8 seconds level. 
         assert np.all(np.abs(pint_resids_us.value - self.ltres) < 3e-8), 'B1855 residuals test failed.'
 
 
