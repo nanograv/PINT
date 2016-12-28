aaad1c8fdc2b8862a898a0f2bbadd0bf472e066f
luojing1211 <luojing1211@gmail.com>
2016-12-28 12:21:43 -0600

    remove check sum for *.bsp files since those files ar no long in our datafile. Astropy will handle it

--- setup.py
+++ setup.py
@@ -53,9 +53,6 @@ fnames = [os.path.split(x)[1] for x in data_urls]
 # Following md5sums created using:
 # >  dict([(fname, hashlib.md5(open(fname, 'rb').read()).digest()) for fname in fnames])
 md5sums = {'de-403-masses.tpc': '\x00\x13{\xda\x957\xbbG\xcb\xd0v\xb6qoBg',
-           'de405.bsp': '\xc5\xe5\x9d\x92\xfcO\x84`\xaa\xa1\xeeV\x94i0x',
-           'de421.bsp': "a\x10\xd8\xd2\xb65`h\x0f\xf8\x0b'\xe3\x00e\xb2",
-           'de430t.bsp': '\xe2p\xe5\xb9\xcc\x8c(\xa2\xd8#\x06\xbeLQU\xb6',
            'earth_latest_high_prec.bpc': '\xd3\xefHO%\x1dc\xb4\x91\x06\xc1\x93\xf6\x01\xbb\x08',
            'naif0012.tls': '%\xa2\xff\xf3\x0b\r\xed\xb4\xd7l\x06r{\x18\x95\xb1',
            'pck00010.tpc': '\xda\x156A\xf74k\xd5\xb6\xa1"gx\xe0\xd5\x1b'}
