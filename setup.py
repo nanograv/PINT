7556af23844b28ff50683bf813acdab09bfb7182
Paul Ray <paul.ray@nrl.navy.mil>
2017-01-20 07:15:09 -0500

    Added scripts to setup.py to be installed

--- setup.py
+++ setup.py
@@ -101,7 +101,8 @@ setup(
     install_requires = ['astropy>=1.2'],
 
     # These scripts will be installed in your PATH
-    scripts = ['scripts/pintempo.py'],
+    scripts = ['scripts/pintempo.py', 'scripts/zima.py', 'scripts/pintbary.py', 'scripts/fermiphaseogram.py', 
+               'scripts/event_optimize.py', 'scripts/htest_optimize.py' ],
 
     packages=['pint',
         'pint.extern',
