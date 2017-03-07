c3be8b5f22207b17cefa9f284d13b50a7e8d16a0
Paul Ray <paul.ray@nrl.navy.mil>
2017-03-07 15:36:36 -0500

    Added comment

--- tests/test_event_optimize-DISABLED.py
+++ tests/test_event_optimize-DISABLED.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# This test is DISABLED because event_optimize requires PRESTO to be installed
+# to get the fftfit module.  It can be run manually by people who have PRESTO
 from __future__ import division, print_function
 import sys, os
 from StringIO import StringIO
