7bb7aafd01e96139f48fc8865c97e5e04f58dea2
Paul Ray <paul.ray@nrl.navy.mil>
2017-03-27 17:39:54 -0400

    Fix for __file__ being unicode string when run from ipython

--- examples/fit_NGC6440E.py
+++ examples/fit_NGC6440E.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import astropy.units as u
 import os, sys
 
-datadir = os.path.dirname(os.path.abspath(__file__))
+datadir = os.path.dirname(os.path.abspath(str(__file__)))
 parfile = os.path.join(datadir, 'NGC6440E.par')
 timfile = os.path.join(datadir, 'NGC6440E.tim')
 
