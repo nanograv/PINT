399d40cf6d08cd65a33059652e00fda8c14df135
luojing1211 <luojing1211@gmail.com>
2017-04-03 15:37:32 -0500

    Change file name to test_glitch

--- tests/test_glitch.py
+++ tests/test_glitch.py
@@ -14,21 +14,21 @@ parfile = os.path.join(datadir, 'prefixtest.par')
 timfile = os.path.join(datadir, 'prefixtest.tim')
 
 
-class TestPrefix(unittest.TestCase):
+class TestGlitch(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.m = pint.models.get_model(parfile)
         self.t = pint.toa.get_TOAs(timfile,ephem="DE405", include_bipm=False)
+        self.f = pint.fitter.PowellFitter(self.t, self.m)
     def test_prefix(self):
         print("Test prefix parameter via a glitch model")
         rs = pint.residuals.resids(self.t, self.m).phase_resids
         # Now do the fit
         print("Fitting...")
-        f = pint.fitter.PowellFitter(self.t, self.m)
-        f.fit_toas()
+        self.f.fit_toas()
         emsg = "RMS of " + self.m.PSR.value + " is too big."
         assert f.resids.time_resids.std().to(u.us).value < 950.0, emsg
-
+        
 
 if __name__ == '__main__':
     pass
