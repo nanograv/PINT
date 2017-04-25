f3d66b8980437096ad2c114284ebc3fa9f84b80b
luojing1211 <luojing1211@gmail.com>
2017-04-25 16:08:27 -0500

    make change allow the composition style work

--- pint/models/__init__.py
+++ pint/models/__init__.py
@@ -5,21 +5,21 @@
 from .timing_model import TimingModel, generate_timing_model
 
 # Import all standard model components here
-from .astrometry import AstrometryEquatorial, AstrometryEcliptic
-from .binary_bt import BinaryBT
-from .binary_dd import BinaryDD
-from .binary_ell1 import BinaryELL1
-from .dispersion_model import Dispersion, DispersionDMX
-from .spindown import Spindown
-from .frequency_dependent import FD
-from .glitch import Glitch
-from .jump import JumpDelay
-from .solar_system_shapiro import SolarSystemShapiro
-from .model_builder import get_model
+# from .astrometry import AstrometryEquatorial, AstrometryEcliptic
+# from .binary_bt import BinaryBT
+# from .binary_dd import BinaryDD
+# from .binary_ell1 import BinaryELL1
+# from .dispersion_model import Dispersion, DispersionDMX
+# from .spindown import Spindown
+# from .frequency_dependent import FD
+# from .glitch import Glitch
+# from .jump import JumpDelay
+# from .solar_system_shapiro import SolarSystemShapiro
+# from .model_builder import get_model
 
 # Define a standard basic model
-StandardTimingModel = generate_timing_model("StandardTimingModel",
-        (AstrometryEquatorial, Spindown, Dispersion, SolarSystemShapiro))
+# StandardTimingModel = generate_timing_model("StandardTimingModel",
+#         (AstrometryEquatorial, Spindown, Dispersion, SolarSystemShapiro))
 # BTTimingModel = generate_timing_model("BTTimingModel",
 #         (Astrometry, Spindown, Dispersion, SolarSystemShapiro, BT))
 # DDTimingModel = generate_timing_model("DDTimingModel",
