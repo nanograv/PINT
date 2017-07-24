This is fix to get all the delays and phases with units (#322)
* add unit to the delays and phases

* change stepsize of numerical derivatives

* Change phase unit from dimensionless to cycle

pint/models/timing_model.py
@@ -435,7 +435,7 @@ class TimingModel(object):
         Return the total delay which will be subtracted from the given
         TOA to get time of emission at the pulsar.
         """
-        delay = np.zeros(len(toas))
+        delay = np.zeros(len(toas)) * u.second
         if cutoff_component == '':
             idx = len(self.DelayComponent_list)
         else:
@@ -455,7 +455,7 @@ class TimingModel(object):
         """Return the model-predicted pulse phase for the given TOAs."""
         # First compute the delays to "pulsar time"
         delay = self.delay(toas)
-        phase = Phase(np.zeros(len(toas)), np.zeros(len(toas)))
+        phase = Phase(np.zeros(len(toas)) , np.zeros(len(toas)))
         # Then compute the relevant pulse phases
         for pf in self.phase_funcs:
             phase += Phase(pf(toas, delay))
@@ -481,7 +481,7 @@ class TimingModel(object):
                 if cp.category == 'pulsar_system':
                     cutoff_component = cp.__class__.__name__
         corr = self.delay(toas, cutoff_component, False)
-        return toas['tdbld'] * u.day - corr * u.second
+        return toas['tdbld'] * u.day - corr
 
     def d_phase_d_toa(self, toas, sample_step=None):
         """Return the derivative of phase wrt TOA
@@ -528,7 +528,7 @@ class TimingModel(object):
         # Is it safe to assume that any param affecting delay only affects
         # phase indirectly (and vice-versa)??
         par = getattr(self, param)
-        result = np.longdouble(np.zeros(len(toas))) * u.Unit('')/par.units
+        result = np.longdouble(np.zeros(len(toas))) * u.cycle/par.units
         param_phase_derivs = []
         phase_derivs = self.phase_deriv_funcs
         delay_derivs = self.delay_deriv_funcs
@@ -545,7 +545,7 @@ class TimingModel(object):
             #                         d_delay_d_param
 
             d_delay_d_p = self.d_delay_d_param(toas, param)
-            dpdd_result = np.longdouble(np.zeros(len(toas))) * u.Unit('')/u.second
+            dpdd_result = np.longdouble(np.zeros(len(toas))) * u.cycle/u.second
             for dpddf in self.d_phase_d_delay_funcs:
                 dpdd_result += dpddf(toas, delay)
             result = dpdd_result * d_delay_d_p
@@ -577,10 +577,10 @@ class TimingModel(object):
             h = 1.0 * step
         else:
             h = ori_value * step
-        parv = [par.value-h, par.value+h]
+        parv = [par.value - h, par.value + h]
 
-        phaseI = np.zeros((len(toas),2))
-        phaseF = np.zeros((len(toas),2))
+        phaseI = np.zeros((len(toas),2), dtype=np.longdouble) * u.cycle
+        phaseF = np.zeros((len(toas),2), dtype=np.longdouble) * u.cycle
         for ii, val in enumerate(parv):
             par.value = val
             ph = self.phase(toas)
@@ -588,10 +588,10 @@ class TimingModel(object):
             phaseF[:,ii] = ph.frac
         resI = (- phaseI[:,0] + phaseI[:,1])
         resF = (- phaseF[:,0] + phaseF[:,1])
-        result = (resI + resF)/(2.0 * h)
+        result = (resI + resF)/(2.0 * h * unit)
         # shift value back to the original value
-        par.value = ori_value
-        return result * u.Unit("")/unit
+        par.quantity = ori_value
+        return result
 
     def d_delay_d_param_num(self, toas, param, step=1e-2):
         """ Return the derivative of phase with respect to the parameter.

pint/scripts/fermiphase.py
@@ -81,7 +81,7 @@ def main(argv=None):
     # Compute model phase for each TOA
     phss = modelin.phase(ts.table)[1]
     # ensure all postive
-    phases = np.where(phss < 0.0, phss + 1.0, phss)
+    phases = np.where(phss < 0.0 * u.cycle, phss + 1.0 * u.cycle, phss)
     mjds = ts.get_mjds()
     weights = np.array([w['weight'] for w in ts.table['flags']])
     h = float(hmw(phases,weights))

pint/scripts/photonphase.py
@@ -96,7 +96,7 @@ def main(argv=None):
     # Compute model phase for each TOA
     phss = modelin.phase(ts.table)[1]
     # ensure all postive
-    phases = np.where(phss < 0.0, phss + 1.0, phss)
+    phases = np.where(phss < 0.0 * u.cycle, phss + 1.0 * u.cycle, phss)
     mjds = ts.get_mjds()
     h = float(hm(phases))
     print("Htest : {0:.2f} ({1:.2f} sigma)".format(h,h2sig(h)))

tests/test_d_phase_d_toa.py
@@ -13,7 +13,7 @@ class TestD_phase_d_toa(unittest.TestCase):
     def setUpClass(self):
         self.parfileB1855 = 'B1855+09_polycos.par'
         self.timB1855 = 'B1855_polyco.tim'
-        self.toasB1855 = toa.get_TOAs(self.timB1855, ephem="DE405", 
+        self.toasB1855 = toa.get_TOAs(self.timB1855, ephem="DE405",
                                       planets=False, include_bipm=False)
         self.modelB1855 = mb.get_model(self.parfileB1855)
         # Read tempo style polycos.
@@ -23,7 +23,7 @@ class TestD_phase_d_toa(unittest.TestCase):
         pint_d_phase_d_toa = self.modelB1855.d_phase_d_toa(self.toasB1855)
         mjd = np.array([np.longdouble(t.jd1 - ut.DJM0)+np.longdouble(t.jd2) for t in self.toasB1855.table['mjd']])
         tempo_d_phase_d_toa = self.plc.eval_spin_freq(mjd)
-        diff = pint_d_phase_d_toa - tempo_d_phase_d_toa
+        diff = pint_d_phase_d_toa.value - tempo_d_phase_d_toa
         relative_diff = diff/tempo_d_phase_d_toa
         assert np.all(relative_diff < 1e-8), 'd_phae_d_toa test filed.'
 if __name__ == '__main__':