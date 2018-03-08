fde3623c6707e8f3f5967cd6d0e58126a023765c
Paul Ray <paul.ray@nrl.navy.mil>
2018-03-08 14:55:04 -0500

    Added Hobart and trying to debug travis-ci

--- pint/solar_system_ephemerides.py
+++ pint/solar_system_ephemerides.py
@@ -46,11 +46,13 @@ def _load_kernel_link(ephem, link=''):
         if load_kernel:
             break
         try:
+            log.info('Trying to set astropy ephemeris to {0}'.format(ephem_link))
             coor.solar_system_ephemeris.set(ephem_link)
             load_kernel = True
         except:
+            raise
             try:
-                log.info('Trying to set astropy ephemeris to {0}'.format(ephem_link))
+                log.info('Trying to download and set astropy ephemeris to {0}'.format(ephem_link))
                 aut.data.download_file(ephem_link, timeout=50, cache=True)
                 coor.solar_system_ephemeris.set(ephem_link)
                 load_kernel = True
