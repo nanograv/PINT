8ae249a63fc01b1ddff27691cbaa74c02b373f59
Paul Ray <paul.ray@nrl.navy.mil>
2018-07-06 18:18:49 -0400

    Added absolute_import

--- pint/observatory/__init__.py
+++ pint/observatory/__init__.py
@@ -1,3 +1,4 @@
+from __future__ import print_function, division, absolute_import
 from .observatory import Observatory, get_observatory
 
 # Include any files that define observatories here.  This will start
