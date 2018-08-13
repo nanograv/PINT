940b5735a26d237c8349b26886c42e8a1b6c01a9
Paul Ray <paul.ray@nrl.navy.mil>
2018-08-13 17:48:18 -0400

    Increase download timeout to 300s

--- pint/solar_system_ephemerides.py
+++ pint/solar_system_ephemerides.py
@@ -57,7 +57,7 @@ def _load_kernel_link(ephem, link=''):
             #log.info('Exception! {0} {1} {2}'.format(type(ex), ex.args, ex))
             try:
                 log.info('Trying to download and set astropy ephemeris to {0}'.format(ephem_link))
-                aut.data.download_file(ephem_link, timeout=60, cache=True)
+                aut.data.download_file(ephem_link, timeout=300, cache=True)
                 coor.solar_system_ephemeris.set(ephem_link)
                 load_kernel = True
             except Exception as ex2:
