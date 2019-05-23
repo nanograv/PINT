2c4af0e1d063debaadf63006815bc0415496d9d1
Jing Luo <luojing1211@gmail.com>
2019-05-23 13:52:03 -0400

    Fix scipy factorial import

--- pint/utils.py
+++ pint/utils.py
@@ -1,7 +1,7 @@
 """Miscellaneous potentially-helpful functions."""
 from __future__ import absolute_import, print_function, division
 import numpy as np
-from scipy.misc import factorial
+from scipy.special import factorial
 import string
 import astropy.time
 try:
