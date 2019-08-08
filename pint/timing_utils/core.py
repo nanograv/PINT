74f191c944591feb0b75494704d6959ac0eb42fb
Jing Luo <jingluo@jings-air.lan>
2019-08-07 22:23:35 -0400

    add docstring

--- pint/timing_utils/core.py
+++ pint/timing_utils/core.py
@@ -4,10 +4,24 @@
 from ..toa import get_TOAs
 from ..models import get_model
 
-__all__ = ['add_param_from_str']
+
+__all__ = ['add_param_via_line', 'del_param']
 
 
 def add_param_via_line(model, par_lines):
+    """Add parameters from parameter lines.
+
+       Parameter
+       ---------
+       model: `TimingModel` object.
+           Input model.
+       par_lines: str or list.
+           Paremeter lines for adding to the input model.
+
+       Return
+       ------
+       New `TimingModel` object with added parameters.
+    """
     if isinstance(par_lines, str):
         par_lines = [par_lines,]
     par_strings = model.as_parfile().split('\n')
@@ -18,6 +32,15 @@ def add_param_via_line(model, par_lines):
 
 
 def del_param(model, params):
+    """Delete parameters from the input model.
+
+       Parameter
+       ---------
+       model: `TimingModel` object.
+           Input model.
+       params: str or list
+           Parameter names.
+    """
     new_lines = []
     if isinstance(params, str):
         params = [params,]
