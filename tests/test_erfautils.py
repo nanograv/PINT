f2695f697ad4c1669f4a4daca9f80b385208dde2
Anne Archibald <archibald@astron.nl>
2019-09-05 22:45:20 +0200

    Added tests to characterize the IERS behaviour
    We currently work around the IERS B support in astropy's
    IERS_Auto. These tests demonstrate why.

--- tests/test_erfautils.py
+++ tests/test_erfautils.py
@@ -3,58 +3,126 @@ import unittest
 
 from nose.tools import raises
 import numpy as np
+from numpy.testing import assert_equal, assert_allclose
 from astropy import log
 import astropy.table as table
 from astropy.time import Time
 import astropy.units as u
-from astropy.utils.iers import IERS_Auto, IERS_B
+from astropy.utils.iers import IERS_Auto, IERS_B, IERS_B_URL, IERS_B_FILE
+from astropy.utils.data import download_file
 
 from pint.observatory import Observatory
 from pint import toa, utils, erfautils
 from pinttestdata import testdir, datadir
 
+class TestERFAUtils(unittest.TestCase):
+    def test_simpler_erfa_import(self):
+        import astropy._erfa as erfa
 
-def test_simpler_erfa_import():
-    import astropy._erfa as erfa
+    @unittest.expectedFailure
+    def test_compare_erfautils_astropy(self):
+        o = "Arecibo"
+        loc = Observatory.get(o).earth_location_itrf()
+        mjds = np.linspace(50000,58000,512)
+        t = Time(mjds, scale="tdb", format="mjd")
+        posvel = erfautils.gcrs_posvel_from_itrf(loc, t, obsname=o)
+        astropy_posvel = erfautils.astropy_gcrs_posvel_from_itrf(
+            loc, t, obsname=o)
+        dopv = astropy_posvel - posvel
+        dpos = np.sqrt((dopv.pos**2).sum(axis=0))
+        dvel = np.sqrt((dopv.vel**2).sum(axis=0))
+        assert len(dpos)==len(mjds)
+        # This is just above the level of observed difference
+        assert dpos.max()<0.05*u.m, "position difference of %s" % dpos.max().to(u.m)
+        # This level is what is permitted as a velocity difference from tempo2 in test_times.py
+        assert dvel.max()<0.02*u.mm/u.s, "velocity difference of %s" % dvel.max().to(u.mm/u.s)
 
-@unittest.skip
-def test_compare_erfautils_astropy():
-    o = "Arecibo"
-    loc = Observatory.get(o).earth_location_itrf()
-    mjds = np.linspace(50000,58000,512)
-    t = Time(mjds, scale="tdb", format="mjd")
-    posvel = erfautils.gcrs_posvel_from_itrf(loc, t, obsname=o)
-    astropy_posvel = erfautils.astropy_gcrs_posvel_from_itrf(
-        loc, t, obsname=o)
-    dopv = astropy_posvel - posvel
-    dpos = np.sqrt((dopv.pos**2).sum(axis=0))
-    dvel = np.sqrt((dopv.vel**2).sum(axis=0))
-    assert len(dpos)==len(mjds)
-    # This is just above the level of observed difference
-    assert dpos.max()<0.05*u.m, "position difference of %s" % dpos.max().to(u.m)
-    # This level is what is permitted as a velocity difference from tempo2 in test_times.py
-    assert dvel.max()<0.02*u.mm/u.s, "velocity difference of %s" % dvel.max().to(u.mm/u.s)
+    def test_iers_discrepancies(self):
+        iers_auto = IERS_Auto.open()
+        iers_b = IERS_B.open()
+        for mjd in [56000,56500,57000]:
+            t = Time(mjd, scale="tdb", format="mjd")
+            b_x, b_y = iers_b.pm_xy(t)
+            a_x, a_y = iers_auto.pm_xy(t)
+            assert abs(a_x-b_x)<1*u.marcsec
+            assert abs(a_y-b_y)<1*u.marcsec
 
-def test_iers_discrepancies():
-    iers_auto = IERS_Auto.open()
-    iers_b = IERS_B.open()
-    for mjd in [56000,56500,57000]:
-        t = Time(mjd, scale="tdb", format="mjd")
-        b_x, b_y = iers_b.pm_xy(t)
-        a_x, a_y = iers_auto.pm_xy(t)
-        assert abs(a_x-b_x)<1*u.marcsec
-        assert abs(a_y-b_y)<1*u.marcsec
+    def test_scalar(self):
+        o = "Arecibo"
+        loc = Observatory.get(o).earth_location_itrf()
+        t = Time(56000, scale="tdb", format="mjd")
+        posvel = erfautils.gcrs_posvel_from_itrf(loc, t, obsname=o)
+        assert posvel.pos.shape == (3,)
 
-def test_scalar():
-    o = "Arecibo"
-    loc = Observatory.get(o).earth_location_itrf()
-    t = Time(56000, scale="tdb", format="mjd")
-    posvel = erfautils.gcrs_posvel_from_itrf(loc, t, obsname=o)
-    assert posvel.pos.shape == (3,)
+    @raises(ValueError)
+    def test_matrix(self):
+        o = "Arecibo"
+        loc = Observatory.get(o).earth_location_itrf()
+        t = Time(56000*np.ones((4,5)), scale="tdb", format="mjd")
+        posvel = erfautils.gcrs_posvel_from_itrf(loc, t, obsname=o)
 
-@raises(ValueError)
-def test_matrix():
-    o = "Arecibo"
-    loc = Observatory.get(o).earth_location_itrf()
-    t = Time(56000*np.ones((4,5)), scale="tdb", format="mjd")
-    posvel = erfautils.gcrs_posvel_from_itrf(loc, t, obsname=o)
+    @unittest.expectedFailure # Not true in current astropy; wish we had knownfail!
+    def test_IERS_B_all_in_IERS_Auto(self):
+        B = IERS_B.open(download_file(IERS_B_URL, cache=True))
+        mjd = B['MJD'].to(u.day).value
+        A = IERS_Auto.open()
+        A.pm_xy(mjd) # ensure that data is available for this date
+        i_A = np.searchsorted(A['MJD'].to(u.day).value, mjd)
+        assert_equal(A['dX_2000A_B'][i_A], B['dX_2000A'])
+
+    @unittest.expectedFailure # Disagreement in current astropy
+    def test_IERS_B_agree_with_IERS_Auto(self):
+        A = IERS_Auto.open()
+        B = IERS_B.open(download_file(IERS_B_URL, cache=True))
+        mjd = B['MJD'].to(u.day).value
+        A.pm_xy(mjd) # ensure that data is available for this date
+
+        # Let's get rid of some trouble values and see if they agree on a restricted subset
+        # IERS Auto ends with a bunch of 1e20 values meant as sentinels (?)
+        ok_A = abs(A['dX_2000A_B']) < 1e6*u.marcsec
+        # For some reason IERS B starts with zeros up to MJD 45700? IERS Auto doesn't match this
+        #ok_A &= A['MJD'] > 45700*u.d
+        # Maybe the old values are bogus?
+        ok_A &= A['MJD'] > 50000*u.d
+
+        mjds_A = A['MJD'][ok_A].to(u.day).value
+        i_B = np.searchsorted(B['MJD'].to(u.day).value, mjds_A)
+        assert_equal(A['MJD'][ok_A], B['MJD'][i_B], "MJDs don't make sense")
+        assert_allclose(
+                A['dX_2000A_B'][ok_A].to(u.marcsec).value,
+                B['dX_2000A'][i_B].to(u.marcsec).value,
+                atol=1e-5, rtol=1e-3,
+                err_msg="Inserted IERS B values don't match current IERS B values")
+
+    def test_astropy_IERS_B_vs_downloaded(self):
+        P = IERS_B.open(IERS_B_FILE)
+        B = IERS_B.open(download_file(IERS_B_URL, cache=True))
+        assert B['MJD'][-1]>P['MJD'][-1]
+
+    @unittest.expectedFailure # Disagreement in current astropy (!)
+    def test_IERS_B_builtin_agree_with_IERS_Auto(self):
+        A = IERS_Auto.open()
+        B = IERS_B.open(IERS_B_FILE)
+        mjd = B['MJD'].to(u.day).value
+        A.pm_xy(mjd) # ensure that data is available for these dates
+
+        # We're going to look up the OK auto values in the B table
+        ok_A = A['MJD'] < B['MJD'][-1]
+        # Let's get rid of some trouble values and see if they agree on a restricted subset
+        # IERS Auto ends with a bunch of 1e20 values meant as sentinels (?)
+        ok_A &= abs(A['dX_2000A_B']) < 1e6*u.marcsec
+        # For some reason IERS B starts with zeros up to MJD 45700? IERS Auto doesn't match this
+        ok_A &= A['MJD'] > 45700*u.d
+        # Maybe the old values are bogus?
+        ok_A &= A['MJD'] > 50000*u.d
+
+        mjds_A = A['MJD'][ok_A].to(u.day).value
+        i_B = np.searchsorted(B['MJD'].to(u.day).value, mjds_A)
+
+        assert np.all(np.diff(i_B)==1), "Valid region not contiguous"
+        assert_equal(A['MJD'][ok_A], B['MJD'][i_B], "MJDs don't make sense")
+        assert_allclose(
+                A['dX_2000A_B'][ok_A].to(u.marcsec).value,
+                B['dX_2000A'][i_B].to(u.marcsec).value,
+                atol=1e-5, rtol=1e-3,
+                err_msg="Inserted IERS B values don't match IERS_B_FILE values")
