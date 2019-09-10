01e6259550b6ce975c2838c06bcff6e1b06f46d0
Anne Archibald <archibald@astron.nl>
2019-09-10 15:59:19 +0200

    Try to ensure readthedocs uses the correct version of sphinx

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ setup(
 
     install_requires = ['astropy>=2.0'],
     setup_requires = ['pytest-runner>=2.0,<3dev'],
-    tests_require = ['pytest'],
+    tests_require = ['pytest', 'sphinx>=2.2.0'],
 
     entry_points={
         'console_scripts': console_scripts,
