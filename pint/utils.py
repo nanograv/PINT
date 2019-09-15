c921b0e428455eac1172fee29cd06b4114cd7f2f
Anne Archibald <archibald@astron.nl>
2019-09-15 20:55:45 +0200

    Raise an error on import if long double isn't useful

--- pint/utils.py
+++ pint/utils.py
@@ -19,6 +19,8 @@ except AttributeError:
     # fallback for Python 2
     from string import maketrans
 
+if np.finfo(np.longdouble).eps > 2e-19 or not hasattr(np, "float128"):
+    raise ValueError("This platform does not support extended precision floating-point, and PINT cannot run without this.")
 
 class PosVel(object):
     """Position/Velocity class.
