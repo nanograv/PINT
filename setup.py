c0ac248c37074139cca96517643c7e042f6be9ea
Anne Archibald <archibald@astron.nl>
2019-09-19 20:33:13 +0200

    Explain setup.py emptiness [skip ci]

--- setup.py
+++ setup.py
@@ -2,4 +2,7 @@ from setuptools import setup
 
 import versioneer
 
+# If you are surprised by how empty this file is, that is because almost all
+# of the information that was here has been moved to the file setup.cfg
+
 setup(version=versioneer.get_version(), cmdclass=versioneer.get_cmdclass())
