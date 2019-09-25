Precision testing reveals a bug

src/pint/utils.py
@@ -128,10 +128,10 @@ class PosVel(object):
 
     def __str__(self):
         if self._has_labels():
-            return (str(self.pos)+", "+str(self.vel)
-                    + " " + self.origin + "->" + self.obj)
+            return ("PosVel(" + str(self.pos)+", "+str(self.vel)
+                    + " " + self.origin + "->" + self.obj + ")")
         else:
-            return str(self.pos)+", "+str(self.vel)
+            return "PosVel(" + str(self.pos)+", "+str(self.vel) + ")"
     def __getitem__(self, k):
         """Allow extraction of slices of the contained arrays"""
         colon = slice(None,None,None)
@@ -172,11 +172,13 @@ def time_from_mjd_string(s, scale='utc'):
                         "time_from_mjd_string ('%s')!" % s)
             # This could cause a loss of precision...
             # maybe throw an exception instead?
-            imjd, fmjd = 0, float(ss)
+            imjd, fmjd = 0, np.longdouble(ss)
         else:
             imjd_s, fmjd_s = num.split('.')
             imjd = int(imjd_s + fmjd_s[:expon])
             fmjd = float("0."+fmjd_s[expon:])
+            if imjd<0:
+                fmjd = -fmjd
     else:
         mjd_s = ss.split('.')
         # If input was given as an integer, add floating "0"
@@ -185,6 +187,8 @@ def time_from_mjd_string(s, scale='utc'):
         imjd_s, fmjd_s = mjd_s
         imjd = int(imjd_s)
         fmjd = float("0." + fmjd_s)
+        if imjd<0:
+            fmjd = -fmjd
     return astropy.time.Time(imjd, fmjd, scale=scale, format='pulsar_mjd',
                              precision=9)
 

tests/test_precision.py
@@ -18,6 +18,7 @@ from pint.utils import (
     str2longdouble,
     time_to_longdouble,
     time_to_mjd_string,
+    time_from_mjd_string,
 )
 
 
@@ -192,7 +193,6 @@ def test_pulsar_mjd_never_differs_too_much_from_mjd(i, f):
 @given(integers(40000, 70000), floats(-2.0, 2.0, allow_nan=False))
 @example(i=40000, f=-8.881784197001254e-16)
 @example(i=43510, f=-1.0000000000000002)
-@settings(max_examples=5000)
 def test_pulsar_mjd_never_differs_too_much_from_mjd_utc(i, f):
     t = Time(val=i, val2=f, format="mjd", scale="utc")
     t2 = Time(val=i, val2=f, format="pulsar_mjd", scale="utc")
@@ -207,3 +207,34 @@ def test_posvel_respects_longdouble():
     pv = PosVel(pos,vel)
     assert_array_equal(pv.pos, pos)
     assert_array_equal(pv.vel, vel)
+
+
+@given(
+    one_of(integers(40000, 60000), integers(-3000000, 3000000), just(0)),
+    floats(-2, 2, allow_nan=False),
+)
+@example(i=-1, f=0.9)
+@example(i=0, f=-0.00010000000000021106)
+def test_time_from_mjd_string_accuracy_vs_longdouble(i, f):
+    mjd = np.longdouble(i)+np.longdouble(f)
+    s = str(mjd)
+    t = Time(val=i, val2=f, format='mjd', scale='utc')
+    assert abs(time_from_mjd_string(s)-t).to(u.us) < 1*u.us
+    if 40000 <= i <= 60000:
+        assert abs(time_from_mjd_string(s)-t).to(u.us) < 1*u.ns
+
+@given(
+    one_of(integers(40000, 60000), integers(-3000000, 3000000), just(0)),
+    floats(-2, 2, allow_nan=False),
+)
+@example(i=-1, f=0.9)
+@example(i=0, f=-0.00010000000000021106)
+def test_time_from_longdouble_accuracy_vs_longdouble(i, f):
+    mjd = np.longdouble(i)+np.longdouble(f)
+    s = str(mjd)
+    t = Time(mjd, format='mjd', scale='utc')
+    assert abs(time_from_mjd_string(s)-t).to(u.us) < 1*u.us
+    if 40000 <= i <= 60000:
+        assert abs(time_from_mjd_string(s)-t).to(u.us) < 1*u.ns
+
+# should be exact on 50000+np.finfo(float).eps