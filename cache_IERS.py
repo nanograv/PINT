9db6c60f8da2c45e344e64584f6653019c2e8c4c
Paul Ray <paul.ray@nrl.navy.mil>
2019-10-29 07:44:08 -0400

    Added exception trap

--- cache_IERS.py
+++ cache_IERS.py
@@ -2,9 +2,9 @@
 from astropy.utils.iers import IERS_A, IERS_A_URL
 from astropy.utils.data import download_file
 from astropy.utils.iers import IERS_A_URL
+from urllib.error import HTTPError, URLError
 
 try:
-    from urllib.error import HTTPError
 
     iers_a = IERS_A.open(IERS_A_URL)
     URL = IERS_A_URL
@@ -14,7 +14,7 @@ except HTTPError:
 
         iers_a = IERS_A.open(IERS_A_URL_MIRROR)
         URL = IERS_A_URL_MIRROR
-    except ImportError:
+    except (ImportError, URLError):
         URL = "NO_IERS_URL_A_OR_MIRROR_FOUND"
 
 download_file(IERS_A_URL, cache=True)
