f20802ede7f44b8f13577e6af70a604c9571ac30
Paul Ray <paul.ray@nrl.navy.mil>
2019-10-29 14:11:29 -0400

    flake8 fixes

--- cache_IERS.py
+++ cache_IERS.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
+"Download the IERS_A data file so it will be cached."
 from __future__ import print_function, division
 
 import sys
 
 from astropy.utils.iers import IERS_A, IERS_A_URL
 from astropy.utils.data import download_file
-from astropy.utils.iers import IERS_A_URL
 
 from astropy import log
 
+# Different code required for Python 2, since it has no HTTPError
+# Remove this once Python 2 support is ended
 if sys.version_info.major < 3:
     try:
         iers_a = IERS_A.open(IERS_A_URL)
         URL = IERS_A_URL
     except:
-        URL = "NO_IERS_URL_A_OR_MIRROR_FOUND"
+        URL = "NO_IERS_A_URL_FOUND"
     try:
         download_file(URL, cache=True)
     except:
@@ -33,7 +35,7 @@ else:
             iers_a = IERS_A.open(IERS_A_URL_MIRROR)
             URL = IERS_A_URL_MIRROR
         except (ImportError, URLError):
-            URL = "NO_IERS_URL_A_OR_MIRROR_FOUND"
+            URL = "NO_IERS_A_URL_OR_MIRROR_FOUND"
 
     try:
         download_file(URL, cache=True)
