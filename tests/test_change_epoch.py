e110735606011fab493ca27ea7f8babb3def6d52
Ross Jennings <rossjjennings@gmail.com>
2020-02-12 18:44:49 -0500

    Add unit test for models.Spindown.change_pepoch()

---
+++ tests/test_change_epoch.py
@@ -0,0 +1,14 @@
+import pint
+from pint import models
+from astropy.time import Time
+import astropy.units as u
+from pinttestdata import datadir
+import os.path
+
+def test_change_pepoch():
+    model = models.get_model(os.path.join(datadir, 'J1713+0747_NANOGrav_11yv0.gls.par'))
+    t0 = Time(56000, scale='tdb', format='mjd')
+    epoch_diff = (t0.mjd_long - model.PEPOCH.quantity.mjd_long)*u.day
+    F0_at_t0 = model.F0.quantity + model.F1.quantity*epoch_diff.to(u.s)
+    model.change_pepoch(t0)
+    assert model.F0.quantity == F0_at_t0
