c66fe47cf1e40db7694ff82d4dfcaadc84dce878
Ross Jennings <rossjjennings@gmail.com>
2020-02-19 21:15:03 -0500

    Make model a fixture in test_change_pepoch()

--- tests/test_change_epoch.py
+++ tests/test_change_epoch.py
@@ -1,6 +1,7 @@
 import os.path
 
 import astropy.units as u
+import pytest
 from astropy.time import Time
 
 import pint
@@ -8,8 +9,13 @@ from pint import models
 from pinttestdata import datadir
 
 
-def test_change_pepoch():
-    model = models.get_model(os.path.join(datadir, "J1713+0747_NANOGrav_11yv0.gls.par"))
+@pytest.fixture
+def model():
+    parfile = os.path.join(datadir, "J1600-3053_test.par")
+    return models.get_model(parfile)
+
+
+def test_change_pepoch(model):
     t0 = Time(56000, scale="tdb", format="mjd")
     epoch_diff = (t0.mjd_long - model.PEPOCH.quantity.mjd_long) * u.day
     F0_at_t0 = model.F0.quantity + model.F1.quantity * epoch_diff.to(u.s)
