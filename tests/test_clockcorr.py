5bedcbde2bfcc526359684bdc67ef3ea87f1fceb
Paul Ray <paul.ray@nrl.navy.mil>
2020-02-28 11:50:28 -0500

    blacken

--- tests/test_clockcorr.py
+++ tests/test_clockcorr.py
@@ -32,9 +32,10 @@ class TestClockcorrection(unittest.TestCase):
 
     def test_wsrt(self):
         # Issue here is the wsrt clock file has text columns, which used to cause np.loadtxt to crash
-        cf = ClockFile.read(path.join(datadir,"wsrt2gps.clk"),format="tempo2",obscode="i")
-        
-        t = Time(57109.5, scale='utc', format='mjd')
-        e = cf.evaluate(t)
-        assert numpy.isclose(e.to(u.us).value,7.907)
+        cf = ClockFile.read(
+            path.join(datadir, "wsrt2gps.clk"), format="tempo2", obscode="i"
+        )
 
+        t = Time(57109.5, scale="utc", format="mjd")
+        e = cf.evaluate(t)
+        assert numpy.isclose(e.to(u.us).value, 7.907)
