Use correct terms for correlation, covariance matrices

src/pint/fitter.py
@@ -222,10 +222,10 @@ class WLSFitter(Fitter):
             sigma_var = (Sigma / fac).T / fac
             errors = np.sqrt(np.diag(sigma_var))
             sigma_cov = (sigma_var / errors).T / errors
-            # correlation matrix = variances in diagonal, used for gaussian random models
-            self.correlation_matrix = sigma_var
-            # covariance matrix = 1s in diagonal, use for comparison to tempo/tempo2 cov matrix
-            self.covariance_matrix = sigma_cov
+            # covariance matrix = variances in diagonal, used for gaussian random models
+            self.covariance_matrix = sigma_var
+            # correlation matrix = 1s in diagonal, use for comparison to tempo/tempo2 cov matrix
+            self.correlation_matrix = sigma_cov
             self.fac = fac
             self.errors = errors
 

src/pint/random_models.py
@@ -45,16 +45,16 @@ def random_models(
     params = fitter.get_fitparams_num()
     mean_vector = params.values()
     # remove the first column and row (absolute phase)
-    cor_matrix = (((fitter.correlation_matrix[1:]).T)[1:]).T
+    cov_matrix = (((fitter.covariance_matrix[1:]).T)[1:]).T
     fac = fitter.fac[1:]
     f_rand = deepcopy(fitter)
     mrand = f_rand.model
 
     # scale by fac
-    log.debug("errors", np.sqrt(np.diag(cor_matrix)))
+    log.debug("errors", np.sqrt(np.diag(cov_matrix)))
     log.debug("mean vector", mean_vector)
     mean_vector = np.array(list(mean_vector)) * fac
-    cov_matrix = ((cor_matrix * fac).T * fac).T
+    cov_matrix = ((cov_matrix * fac).T * fac).T
 
     toa_mjds = fitter.toas.get_mjds()
     minMJD, maxMJD = toa_mjds.min(), toa_mjds.max()