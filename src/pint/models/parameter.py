Fix PBDOT scaling

src/pint/models/parameter.py
@@ -590,14 +590,14 @@ class floatParameter(Parameter):
             # This will happen if the input value did not have units
             num_value = setfunc_no_unit(val)
             if self.unit_scale:
+                # For some parameters, if the value is above a threshold, it is assumed to be in units of scale_factor
+                # e.g. "PBDOT 7.2" is interpreted as "PBDOT 7.2E-12", since the scale_factor is 1E-12 and the scale_threshold is 1E-7
                 if np.abs(num_value) > np.abs(self.scale_threshold):
                     log.info(
-                        "Parameter %s's unit will be scaled to %s %s"
+                        "Parameter %s's value will be scaled to %s %s"
                         % (self.name, str(self.scale_factor), str(self._original_units))
                     )
-                    self.units = self.scale_factor * self._original_units
-                else:
-                    self.units = self._original_units
+                    num_value *= self.scale_factor
             result = (num_value) * self.units
 
         return result