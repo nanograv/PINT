fa7fd32f1ce7bd7a5eac0103a4f20a82c9ea8e65
Jing Luo <luojing1211@gmail.com>
2020-05-09 16:54:42 -0400

    add general data fitter

---
+++ src/pint/general_data_fitter.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import collections
+import copy
+
+import astropy.units as u
+import numpy as np
+import scipy.linalg as sl
+import scipy.optimize as opt
+from astropy import log
+import astropy.constants as const
+import pint.utils
+from pint.fitter import Fitter
+
+
+class GRSFitter(Fitter):
+    """ Generalized residual fitter.
+
+    The generalized residual fitter uses more than TOAs as data input, it also
+    uses the independent measured data (e.g., Wideband's DM valus) as part of
+    the fit data.
+
+    Note
+    ----
+    This fitter assume the independent measured data are stored in the toas as
+    default.
+    """
+
+    def __init__(self, toas, model, residuals=None, ):
+        super(GRSFitter, self).__init__(toas=toas, model=model, residuals=residuals)
+        self.method = "g"
