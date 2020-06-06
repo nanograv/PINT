847fca3d8f9430841ee886a8f531d46eaf917dc7
Jing Luo <jingluo@Jings-MacBook-Air.local>
2020-06-06 17:01:50 -0400

    add test

---
+++ tests/test_model_sector.py
@@ -0,0 +1,22 @@
+""" Various tests for ModelSector and its interaction with TimingModel
+"""
+from __future__ import absolute_import, division, print_function
+
+import pytest
+import os
+import numpy as np
+import astropy.units as u
+
+from pint.models import (
+    TimingModel,
+    ModelSector,
+    Component
+)
+
+
+class test_sector:
+    def setup(self):
+        self.all_components = Component.component_types
+
+    def test_init(self):
+        sector = ModelSector(self.all_components['AstrometryEquatorial'])
