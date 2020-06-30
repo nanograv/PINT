58a880c48093b792930ab7d9583cec380c91533b
Jing Luo <luojing1211@gmail.com>
2020-06-30 15:30:36 -0400

    Add more functions

--- src/pint/models/pint_matrix.py
+++ src/pint/models/pint_matrix.py
@@ -20,6 +20,8 @@ class PintMatrix:
         The labels of the axises. Each list element contains the names and
         indices of the labels for the dimension.
         [{dim0_label0: (start, end), dim0_label1:(start, end)}, {dim1_lable0:.}]
+        The start and end follows the python slice convention (i.e.,
+        end = size + 1).
     """
     def __init__(self, data, axis_labels):
         self.data = data
@@ -55,13 +57,16 @@ class PintMatrix:
                     raise ValueError("Label index in dim {} has"
                                      " overlap".format(ii))
 
-    def _get_label(self, lable):
+    def get_axis_labels(self, axis):
+        return self.axis_labels[axis]
+
+    def get_label(self, label):
         """ Get the label entry and its dimension. We assume the labels are
         unique in the matrix.
         """
         for ii, dim in enumerate(self.axis_labels):
             if label in dim.keys():
-                return(ii, label, dim[label]))
+                return(label, ii, dim[label]))
         raise KeyError("Label {} is not in the matrix".format(label))
 
     def get_label_slice(self, labels):
@@ -70,19 +75,19 @@ class PintMatrix:
         dim_slices = dict([(d, slice(None) for d in range(self.ndim))])
         new_labels =  dict([(d, {} for d in range(self.ndim))])
         for lb in labels:
-            lable_info = self._get_label(lb)
-            label_size = label_info[2][1] - label_info[2][0] + 1
+            lable_info = self.get_label(lb)
+            label_size = label_info[2][1] - label_info[2][0]
             # if slice is a list, there is a label already added.
             if isinstance(dim_slices[label_info[0]], list):
                 # The start of the new matrix.
                 start = len(dim_slices[label_info[0]]) + 1
                 dim_slices[label_info[0]] += range(label_info[2][0],
-                                                   label_info[2][1] + 1)
+                                                   label_info[2][1])
 
             else:
                 start = 0
                 dim_slices[label_info[0]] = range(label_info[2][0],
-                                                  label_info[2][1] + 1)
+                                                  label_info[2][1])
 
             new_labels[label_info[0]].update({lb: (start, start + label_size)})
         return list(dim_slices.values()), list(new_labels.values())
@@ -93,9 +98,10 @@ class PintMatrix:
         slice, new_labels = self.get_label_slice(labels)
         return PintMatrix(self.data[slice], new_labels)
 
-    def match_labels(self, pint_matrix):
-        """ Match the label index between the current matrix and input pint
-        matrix. The labels will be matched along axises, not cross the axises.
+    def match_labels_along_axis(self, pint_matrix, axis):
+        """ Match one axis' labels index between the current matrix and input
+        pint matrix. The labels will be matched along axises, not cross the
+        axises.
 
         Parametere
         ----------
@@ -104,14 +110,24 @@ class PintMatrix:
 
         Return
         ------
-            Index map between the current labels and input matrix labels.
+            Index map between the current labels and input matrix labels along
+            axis.
         """
-        pass
+        current_lalels = self.get_axis_labels(axis)
+        input_labels = pint_matrix.get_axis_labels(axis)
+        matched = list(set(current_lables).instersection(set(input_labels)))
+        match_index = {}
+        for lb in matched:
+            l1, ax1, idx1 = self.get_label(lb)
+            l2, ax2, idx2 = pint_matrix.get_label(lb)
+            match_index[lb] = [idx1, idx2]
+        return match_index
 
     def append_along_axis(self, pint_matrix, axis):
         """ Append one pint matrix on x axis.
         """
         pass
+
 
 class DesignMatrix(PintMatrix):
     """ Design matrix for least square fitting.
@@ -130,20 +146,59 @@ class DesignMatrix(PintMatrix):
         The parameter list for the derivatives 'd_quantity_d_param'
     scale_by_F0: bool, optional
         If it is a phase derivative, if it is scaled by spin period. Default is
-        False.
+        True.
     incoffset: bool, optional
-        If add the absolute phase offset. Default is False
+        If add the absolute phase offset. Default is True.
 
     Note:
     -----
         The incoffset maybe replaced by the absolute phase fitting in the future.
     """
     def __init__(self, data, model, derivative_quantity, derivative_param,
-                 scaled_by_F0=False, incoffset=False):
+                 scaled_by_F0=True, incoffset=True):
         # Check if the derivate quantity a phase derivative
+        if derivative_param = 'phase':
+            params = ["Offset"] if incoffset else []
+
+        param += derivative_param
+        self.data = data
+        self.model = model
+        self.derivative_quantity = derivative_quantity
+        self.deriv_func = getattr(self.model,
+            'd_{}_d_param'.format(derivative_quantity))
+
 
 
         super(DesignMatrix, self).__init__(toa, model, derivative_quantity)
+
+    def make_design_matrix(self):
+        """ Create the design matrix from the derivatives.
+        """
+        M = np.zeros((ntoas, nparams))
+        for ii, param in enumerate(params):
+            if param == "Offset":
+                M[:, ii] = 1.0
+                units.append(u.s / u.s)
+            else:
+                # NOTE Here we have negative sign here. Since in pulsar timing
+                # the residuals are calculated as (Phase - int(Phase)), which is different
+                # from the conventional definition of least square definition (Data - model)
+                # We decide to add minus sign here in the design matrix, so the fitter
+                # keeps the conventional way.
+                q = -self.d_phase_d_param(toas, delay, param)
+                M[:, ii] = q
+                units.append(u.Unit("") / getattr(self, param).units)
+
+        if self.derivative_quantity == 'phase':
+            if scale_by_F0:
+                mask = []
+                for ii, un in enumerate(units):
+                    if params[ii] == "Offset":
+                        continue
+                    units[ii] = un * u.second
+                    mask.append(ii)
+                M[:, mask] /= F0.value
+        return M, params, units, scale_by_F0
 
 
 class CovarianceMatrix(PintMatrix):
