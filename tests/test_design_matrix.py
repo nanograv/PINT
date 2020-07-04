1513452434d20729de35dbd6727ce0f38c00fc15
Jing Luo <luojing1211@gmail.com>
2020-07-04 12:24:30 -0400

    add test

--- tests/test_design_matrix.py
+++ tests/test_design_matrix.py
@@ -5,6 +5,7 @@ import pytest
 from pint.models import get_model
 from pint.toa import get_TOAs
 from pint.pint_matrix import  DesignMatrix
+import astropy.units as u
 
 
 class TestDesignMatrix:
@@ -14,6 +15,17 @@ class TestDesignMatrix:
         self.tim_file = "J1614-2230_NANOGrav_12yv3.wb.tim"
         self.model = get_model(self.par_file)
         self.toas = get_TOAs(self.tim_file)
+        self.default_test_param = []
+        for p in self.model.params:
+            if not getattr(self.model, p).frozen:
+                self.default_test_param.append(p)
 
-    def test_make_design_matrix(self):
-        pass
+    def test_make_phase_designmatrix(self):
+        quantity = 'phase'
+        phase_designmatrix = DesignMatrix(self.toas, self.model, quantity,
+                                          u.Unit(''), self.default_test_param)
+
+    def test_make_dm_designmatrix(self):
+        quantity = 'dm'
+        phase_designmatrix = DesignMatrix(self.toas, self.model, quantity,
+                                          u.Unit(''), self.default_test_param)
