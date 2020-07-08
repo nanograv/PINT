dbbc42857b93aecf6f5fdb236efdad0aabd87874
Josef Zimmerman <jmz5fx@virginia.edu>
2020-07-08 06:56:48 -0400

    Added more tests

--- tests/test_troposphere_model.py
+++ tests/test_troposphere_model.py
@@ -18,14 +18,26 @@ class TestTroposphereDelay(unittest.TestCase):
 
     MIN_ALT = 5  # the minimum altitude in degrees for testing the delay model
 
+    FLOAT_THRESHOLD = 1e-12  #
+
     def setUp(self):
+        ngc = "datafile/NGC6440E"
+
         self.toas = toa.get_TOAs(ngc + ".tim")
         self.model = pint.models.get_model(ngc + ".par")
+        self.modelWithTD = pint.models.get_model(ngc + ".par")
+        self.modelWithTD.CORRECT_TROPOSPHERE.value = True
+
+        self.toasInvalid = toa.get_TOAs(ngc + ".tim")
+
+        for i in range(len(self.toasInvalid.table)):
+            # adjust the timing by half a day to make them invalid
+            self.toasInvalid.table["mjd"][i] += 0.5
 
         self.testAltitudes = np.arange(self.MIN_ALT, 90, 100) * u.deg
         self.testHeights = np.array([10, 100, 1000, 5000]) * u.m
 
-        self.td = TroposphereDelay()
+        self.td = self.modelWithTD.components["TroposphereDelay"]
 
     def test_altitudes(self):
         # the troposphere delay should strictly decrease with increasing altitude above horizon
@@ -39,6 +51,21 @@ class TestTroposphereDelay(unittest.TestCase):
         heightDelays = []  # list of the delays at each height
         for h in self.testHeights:
             heightDelays.append(self.td.delay_model(self.testAltitudes, 0, h, 0))
-
-        for i in range(1, len(testHeights)):
+        for i in range(1, len(self.testHeights)):
+            print(heightDelays[i], heightDelays[i - 1])
             assert np.all(np.less(heightDelays[i], heightDelays[i - 1]))
+
+    def test_model_access(self):
+        # make sure that the model components are linked correctly to the troposphere delay
+        assert hasattr(self.model, "CORRECT_TROPOSPHERE")
+
+        # the model should have the sky coordinates defined
+        assert self.td._get_target_skycoord() is not None
+
+    def test_invalid_altitudes(self):
+        assert np.all(
+            np.less_equal(
+                np.abs(self.td.troposphere_delay(self.toasInvalid)),
+                self.FLOAT_THRESHOLD * u.s,
+            )
+        )
