86b8e58626fb4656cc748adc9d12933f37d5ebed
Jing Luo <luojing1211@gmail.com>
2020-08-04 21:37:52 -0400

    black the code

--- tests/test_copy.py
+++ tests/test_copy.py
@@ -22,16 +22,16 @@ class TestObjectCopy:
         toa_copy = copy.deepcopy(toa)
 
         assert sys.getsizeof(toa) == sys.getsizeof(toa_copy)
-        assert id(toa)!= id(toa_copy)
+        assert id(toa) != id(toa_copy)
         assert toa.ntoas == toa_copy.ntoas
-        assert all(toa.table['mjd'] == toa_copy.table['mjd'])
+        assert all(toa.table["mjd"] == toa_copy.table["mjd"])
 
     def test_copy_model_object(self):
         model = get_model("J1614-2230_NANOGrav_12yv3.wb.gls.par")
         model_copy = copy.deepcopy(model)
 
         assert sys.getsizeof(model) == sys.getsizeof(model_copy)
-        assert id(model)!= id(model_copy)
+        assert id(model) != id(model_copy)
         assert len(model.params) == len(model_copy.params)
         assert list(model.components.keys()) == list(model_copy.components.keys())
 
@@ -42,8 +42,14 @@ class TestObjectCopy:
         fitter_copy = copy.deepcopy(fitter)
 
         assert sys.getsizeof(fitter) == sys.getsizeof(fitter_copy)
-        assert id(fitter)!= id(fitter_copy)
+        assert id(fitter) != id(fitter_copy)
         assert len(fitter.designmatrix_makers) == len(fitter_copy.designmatrix_makers)
         for ii in range(len(fitter.designmatrix_makers)):
-            assert fitter.designmatrix_makers[ii].derivative_quantity == fitter_copy.designmatrix_makers[ii].derivative_quantity
-            assert fitter.designmatrix_makers[ii].quantity_unit == fitter_copy.designmatrix_makers[ii].quantity_unit
+            assert (
+                fitter.designmatrix_makers[ii].derivative_quantity
+                == fitter_copy.designmatrix_makers[ii].derivative_quantity
+            )
+            assert (
+                fitter.designmatrix_makers[ii].quantity_unit
+                == fitter_copy.designmatrix_makers[ii].quantity_unit
+            )
