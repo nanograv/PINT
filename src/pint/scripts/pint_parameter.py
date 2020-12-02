ba09736add6fef6ae4bce0ca2f1c9b8f9f802872
Jing Luo <luojing1211@gmail.com>
2020-12-02 12:10:43 -0500

    get the all parameter loaded.

--- src/pint/scripts/pint_parameter.py
+++ src/pint/scripts/pint_parameter.py
@@ -1,7 +1,7 @@
 """ A script that quarry the PINT built-in parameters.
 """
 
-from pint.models.timing_model import TimingModel
+from pint.models.timing_model import TimingModel, Component
 import argparse
 
 
@@ -23,53 +23,86 @@ if __name__ == "__main__":
                              "'stdout': Stand out.")
 
     args = parser.parse_args()
-    # Get the all the components
+    # Get the all the components map from all lower case name
+    #  to real component name.
     all_components = Component.component_types
     component_name_map = {}
     for c in all_components.keys():
         component_name_map[c.lower()] = c
-    result = {}
-    required_info = ['name', 'description', 'unit', 'value', 'aliases']
+
+    # Set up the parameter name space including the aliases
+    all_params = []
+    param_name_map = {}
+    for cp_name, cp_cls in all_components.items():
+        cp = cp_cls()
+        for param in cp.params:
+            all_params.append(param)
+            par = getattr(cp, param)
+            # If one parameter does not have aliases,
+            # Take the parameter name as key.
+            param_name_map[param] = (param, cp_name, par)
+            for ali in par.aliases:
+                param_name_map[ali] = (param, cp_name, par)
+
+    # set up the required info.
+    all_info = {}
+    required_info = ['name', 'description', 'units', 'value', 'aliases']
+
+    # Get information for all buiting parameters. The result will be a subset
+    # of the this result.
+    for param in all_params:
+        param_entry = param_name_map[param]
+        pint_param = param_entry[0]
+        host_cp = param_entry[1]
+        info_entry = {}
+        for ri in required_info:
+            info_entry[ri] = getattr(param_entry[2], ri)
+        if host_cp in all_info.keys():
+            all_info[host_cp].update({pint_param: info_entry})
+        else:
+            all_info[host_cp] = {pint_param: info_entry}
+
+    print(all_info)
 
     # Check the component input.
-    quarry_components = args.c
-    if 'all' in quarry_components:
+    if 'all' in args.component:
         quarry_components = list(all_components.keys())
     else:
-        built_in_cps = list(component_name_map.keys())
-        for quarry_com in quarry_components:
-            if quarry_com.lower() in built_in_cps:
-                cp_name = component_name_map[quarry_com]
-                result[cp_name] = {'exists': True}
-            else:
-                result[quarry_com] = {'exists': False}
+        quarry_components = args.component
 
     # Check the parameter input.
-    quarry_params = args.p
-    if 'all' in quarry_params:
-        all_param = True
+    if 'all' in args.parameter:
+        quarr_params = all_params
     else:
-        all_param = False
+        quarry_params = args.parameter
 
-    # Get parameter mapping.
-    aliases = {} 
-    for cp_name, cp in all_components.items():
-        for param in cp.params:
-            par = getattr(param)
-            for ali in par.aliases:
-                aliases[ali] = (param, cp)
+    # Construct the output dictionary
+    #
 
-    # Start quarrying the parameter information.
-    parameters = {}
-    for cp_name in result.keys():
-        cp_obj = all_components[cp_name]()
-        if all_param:
-            quarry_params = cp_obj.params
-        
+    # Get quarry parameters.
+    for q_param in quarry_params:
+        # First check if the quarry parameter is in the
+        # PINT parameter name space
+        if q_param in param_names.keys():
+            param_entry = param_names[q_param]
+            pint_param = param_entry[0]
+            host_cp = param_entry[1]
+            info_entry = {'quarry_name': q_param}
+            for ri in required_info:
+                info_entry[ri] = getattr(param_entry[2], ri)
 
-        for ri in required_info:
+            if host_cp in result.keys():
+               result[host_cp].update({pint_param: info_entry})
+            else:
+               result[host_cp] = {'exists': True, pint_param: info_entry}
 
 
+    print(result)
 
-    # Identify the format
+    # TODO Fix the out put of prefix and mask parameter.
+    # TODO add parameter type to info
+    # TODO get all parameters first??
+
+    # Identify thi e format
     if args.format == 'rst':
+        pass
