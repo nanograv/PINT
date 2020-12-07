b28ec257691007b7b6f8bae85bbfb619398b0e07
Matthew Kerr <matthew.kerr@gmail.com>
2020-12-07 18:17:56 -0500

    Add test for 2mus issue; add min/maxMJD to fermiphase.

--- tests/test_fermiphase.py
+++ tests/test_fermiphase.py
@@ -8,6 +8,8 @@ import unittest
 import pytest
 from six import StringIO
 
+from astropy.io import fits
+
 import pint.models
 import pint.scripts.fermiphase as fermiphase
 import pint.toa as toa
@@ -30,28 +32,42 @@ ft2file = os.path.join(datadir, "lat_spacecraft_weekly_w323_p202_v001.fits")
 class TestFermiPhase(unittest.TestCase):
     def test_GEO_Htest_result(self):
         saved_stdout, sys.stdout = sys.stdout, StringIO("_")
-        cmd = "--plot --plotfile fermitest.png --outfile fermitest.fits {0} {1} CALC".format(
+        cmd = "--maxMJD 55000 --plot --plotfile fermitest.png --outfile fermitest.fits {0} {1} CALC".format(
             eventfile, parfile
         )
         fermiphase.main(cmd.split())
         lines = sys.stdout.getvalue()
-        v = 999.0
+        v = 0
         for l in lines.split("\n"):
             if l.startswith("Htest"):
                 v = float(l.split()[2])
-        # Check that H-test is greater than 1380
-        self.assertTrue(v > 1380)
+        # Check that H-test falls in right range for this portion of data
+        self.assertTrue((v > 550) and (v < 600))
         sys.stdout = saved_stdout
 
-    def test_process_raw_LAT_times(self):
-        # This just checks that the process of reading the FT2 file and
-        # computing positions doesn't crash. It doesn't validate any results.
-        # Should probably run comparison with GEO or BARY phases.
+    def test_process_and_accuracy(self):
+        # Checks that instantiating an observatory and phasing of
+        # topocentric times works.  Verifies accuracy to the sub-mus
+        # level by comparison with stored Tempo2 "Fermi plugin" results.
+
         modelin = pint.models.get_model(parfile)
         FermiObs(name="Fermi", ft2name=ft2file)
         tl = load_Fermi_TOAs(eventfileraw, weightcolumn="PSRJ0030+0451")
-        ts = toa.TOAs(toalist=tl)
-        ts.filename = eventfileraw
-        ts.compute_TDBs(ephem="DE405")
-        ts.compute_posvels(ephem="DE405", planets=False)
-        modelin.phase(ts)[1]
+        # ts = toa.TOAs(toalist=tl)
+        ts = toa.get_TOAs_list(
+            tl, include_gps=False, include_bipm=False, planets=False, ephem="DE405"
+        )
+        iphss, phss = modelin.phase(ts)
+        ph_pint = phss % 1
+
+        with fits.open(eventfileraw) as f:
+            ph_tempo2 = f[1].data.field("pulse_phase")
+
+        dphi = ph_pint - ph_tempo2
+        dphi[dphi < -0.1] += 1
+        dphi[dphi > 0.1] -= 1
+        resids_mus = dphi / modelin.F0.value * 1e6
+        # if the "2 mus" problem exists, the scatter in these values will
+        # be 4-5 mus, whereas if everything is OK it should be few 100 ns
+        prec = resids_mus.max() - resids_mus.min()
+        self.assertTrue(prec < 0.5)
