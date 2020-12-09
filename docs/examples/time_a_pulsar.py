e6e64c19531c935fc317b3bd38f37fe8d4920585
Anne Archibald <peridot.faceted@gmail.com>
2020-12-09 01:41:54 +0000

    Convert new notebook

--- docs/examples/time_a_pulsar.py
+++ docs/examples/time_a_pulsar.py
@@ -1,23 +1,24 @@
----
-jupyter:
-  jupytext:
-    formats: ipynb,md
-    text_representation:
-      extension: .md
-      format_name: markdown
-      format_version: '1.2'
-      jupytext_version: 1.7.1
-  kernelspec:
-    display_name: Python 3
-    language: python
-    name: python3
----
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.7.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
 
-# Time a pulsar
+# %% [markdown]
+# # Time a pulsar
+#
+# This notebook walks through a simple pulsar timing session, as one might do with TEMPO/TEMPO2: load a `.par` file, load a `.tim` file, do a fit, plot the residuals before and after. This one also displays various additional information you might find useful, and also ignores but then plots TOAs with large uncertainties. Similar code is available as a standalone script at [`fit_NGC6440E.py`](https://github.com/nanograv/PINT/blob/master/docs/examples/fit_NGC6440E.py)
 
-This notebook walks through a simple pulsar timing session, as one might do with TEMPO/TEMPO2: load a `.par` file, load a `.tim` file, do a fit, plot the residuals before and after. This one also displays various additional information you might find useful, and also ignores but then plots TOAs with large uncertainties. Similar code is available as a standalone script at [`fit_NGC6440E.py`](https://github.com/nanograv/PINT/blob/master/docs/examples/fit_NGC6440E.py)
-
-```python
+# %%
 import astropy.units as u
 import matplotlib.pyplot as plt
 
@@ -26,35 +27,31 @@ import pint.models
 from pint.models import get_model
 from pint.residuals import Residuals
 from pint.toa import get_TOAs
-```
 
-```python
+# %%
 parfile = "NGC6440E.par"
 timfile = "NGC6440E.tim"
-```
 
-```python
+# %%
 m = get_model(parfile)
 m
-```
 
-```python
+# %%
 t_all = pint.toa.get_TOAs(timfile, ephem="de436")
 t_all.print_summary()
-```
 
-There are many messages here. As a rule messages marked `INFO` can safely be ignored, they are simply informational; take a look at them if something unexpected happens. Messages marked `WARNING` or `ERROR` are more serious. (These messages are emitted by the python `logger` module and can be suppressed or written to a log file if they are annoying.)
+# %% [markdown]
+# There are many messages here. As a rule messages marked `INFO` can safely be ignored, they are simply informational; take a look at them if something unexpected happens. Messages marked `WARNING` or `ERROR` are more serious. (These messages are emitted by the python `logger` module and can be suppressed or written to a log file if they are annoying.)
 
+# %% [markdown]
+# Let's discard the data points with uncertainties $>30\,\mu\text{s}$ - uncertainty estimation is not always reliable when the signal-to-noise is low.
 
-Let's discard the data points with uncertainties $>30\,\mu\text{s}$ - uncertainty estimation is not always reliable when the signal-to-noise is low.
-
-```python
+# %%
 error_ok = t_all.table["error"] <= 30 * u.us
 t = t_all[error_ok]
 t.print_summary()
-```
 
-```python
+# %%
 # These are pre-fit residuals
 rs = Residuals(t, m).phase_resids
 xt = t.get_mjds()
@@ -64,33 +61,28 @@ plt.title("%s Pre-Fit Timing Residuals" % m.PSR.value)
 plt.xlabel("MJD")
 plt.ylabel("Residual (phase)")
 plt.grid()
-```
 
-```python
+# %%
 f = pint.fitter.WLSFitter(t, m)
 # f = pint.fitter.PowellFitter(t, m)
 f.fit_toas()
 # f = pint.fitter.GLSFitter(t, m)
 # f.fit_toas(full_cov=True)
-```
 
-```python
+# %%
 # Print some basic params
 print("Best fit has reduced chi^2 of", f.resids.chi2_reduced)
 print("RMS in phase is", f.resids.phase_resids.std())
 print("RMS in time is", f.resids.time_resids.std().to(u.us))
-```
 
-```python
+# %%
 # Show the parameter correlation matrix
 corm = f.get_correlation_matrix(pretty_print=True)
-```
 
-```python
+# %%
 f.print_summary()
-```
 
-```python
+# %%
 plt.figure()
 plt.errorbar(
     xt.value,
@@ -102,9 +94,8 @@ plt.title("%s Post-Fit Timing Residuals" % m.PSR.value)
 plt.xlabel("MJD")
 plt.ylabel("Residual (us)")
 plt.grid()
-```
 
-```python
+# %%
 t_bad = t_all[~error_ok]
 r_bad = Residuals(t_bad, f.model)
 plt.figure()
@@ -127,12 +118,8 @@ plt.xlabel("MJD")
 plt.ylabel("Residual (us)")
 plt.grid()
 plt.legend(loc="upper left")
-```
 
-```python
+# %%
 plt.show()
-```
 
-```python
-
-```
+# %%
