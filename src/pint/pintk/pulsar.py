543b997cbd9b17e2c5a003f8a3b0573c044a649e
Matteo Bachetti <matteo@matteobachetti.it>
2020-12-16 09:22:08 +0100

    Resolve post-fit crash

--- src/pint/pintk/pulsar.py
+++ src/pint/pintk/pulsar.py
@@ -186,7 +186,7 @@ class Pulsar(object):
             chi2 = self.postfit_resids.chi2
             wrms = self.postfit_resids.rms_weighted()
             print("Post-Fit Chi2:\t\t%.8g us^2" % chi2)
-            print("Post-Fit Weighted RMS:\t%.8g us" % wrms)
+            print("Post-Fit Weighted RMS:\t%.8g us" % wrms.to(u.us).value)
             print(
                 "%19s  %24s\t%24s\t%16s  %16s  %16s"
                 % (
@@ -504,7 +504,7 @@ class Pulsar(object):
         chi2 = self.prefit_resids.chi2
         wrms = self.prefit_resids.rms_weighted()
         print("Pre-Fit Chi2:\t\t%.8g us^2" % chi2)
-        print("Pre-Fit Weighted RMS:\t%.8g us" % wrms)
+        print("Pre-Fit Weighted RMS:\t%.8g us" % wrms.to(u.us).value)
 
         fitter.fit_toas(maxiter=1)
         self.postfit_model = fitter.model
