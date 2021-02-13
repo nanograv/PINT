5afb40ad9d0e28a0367d733e5f2f9cadbd1015fd
champagne-cmd <chloe.a.champagne@vanderbilt.edu>
2021-02-13 10:19:02 -0600

    added check for PhaseJump in pintempo

--- src/pint/scripts/pintempo.py
+++ src/pint/scripts/pintempo.py
@@ -58,7 +58,8 @@ def main(argv=None):
     # turns pre-existing jump flags in t.table['flags'] into parameters in parfile
     m.jump_flags_to_params(t)
     # adds jump flags to t.table['flags'] for jump parameters already in parfile
-    m.jump_params_to_flags(t)
+    if "PhaseJump" in m.components:
+        m.jump_params_to_flags(t)
 
     if m.TRACK.value == "-2":
         if "pn" in t.table.colnames:
