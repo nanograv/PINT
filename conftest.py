38db4d002140e17d3329df4c209158aefeae55f2
Anne Archibald <peridot.faceted@gmail.com>
2021-03-01 21:47:33 +0000

    Fix hypothesis timeouts

---
+++ conftest.py
@@ -0,0 +1,24 @@
+import os
+import hypothesis
+
+# Tell Hypothesis that we might be running slow tests, to print the seed blob
+# so we can easily reproduce failures from CI, and derive a fuzzing profile
+# to try many more inputs when we detect a scheduled build or when specifically
+# requested using the HYPOTHESIS_PROFILE=fuzz environment variable or
+# `pytest --hypothesis-profile=fuzz ...` argument.
+
+hypothesis.settings.register_profile(
+    "ci", deadline=None, print_blob=True, derandomize=True
+)
+hypothesis.settings.register_profile(
+    "fuzzing", deadline=None, print_blob=True, max_examples=1000
+)
+default = (
+    "fuzzing"
+    if (
+        os.environ.get("IS_CRON") == "true"
+        and os.environ.get("ARCH_ON_CI") not in ("aarch64", "ppc64le")
+    )
+    else "ci"
+)  # noqa: E501
+hypothesis.settings.load_profile(os.environ.get("HYPOTHESIS_PROFILE", default))
