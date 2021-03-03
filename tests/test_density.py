59deaaa60801a318ead83f06d973ac6bf564446a
Patrick O'Neill <p_oneill1@outlook.com>
2021-03-03 10:40:57 +0000

    Implemented actual test
    Added parametrization, testing default argument and arbitrary argument.
    Improved assertion conditions to improve time to test.

--- tests/test_density.py
+++ tests/test_density.py
@@ -7,9 +7,11 @@ from pint.models import get_model
 from pylab import *
 from copy import deepcopy
 import io
+import pytest
 
 
-def test_get_highest_density_range(ndays=7 * u.d):
+@pytest.mark.parametrize("ndays", [7 * u.d, 20 * u.d])
+def test_get_highest_density_range(ndays):
     par_base = """
     PSR J1234+5678
     F0 1 0
@@ -21,17 +23,14 @@ def test_get_highest_density_range(ndays=7 * u.d):
     """
     model = get_model(io.StringIO(par_base))
     toas_1 = toa.make_fake_toas(57000, 58000, 1000, model, obs="@")
-    toas_2 = toa.make_fake_toas(57500, 57507, 100, model, obs="@")
+    toas_2 = toa.make_fake_toas(57500, 57500 + ndays.value, 100, model, obs="@")
     merged = toa.merge_TOAs([toas_1, toas_2])
-    x1, x2 = merged.get_highest_density_range()
-    x3, x4 = merged.get_highest_density_range(ndays)
+    if ndays == 7 * u.d:
+        x1 = merged.get_highest_density_range()
+    x2 = merged.get_highest_density_range(ndays)
 
-    assert x1.value <= 57500 + 1e-5
-    assert x1.value >= 57500 - 1e-5
-    assert x3.value >= x1.value - 1e-5
-    assert x3.value <= x1.value + 1e-5
-
-    assert x2.value <= 57507 + 1e-5
-    assert x2.value >= 57507 - 1e-5
-    assert x4.value >= x2.value - 1e-5
-    assert x4.value <= x2.value + 1e-5
+    assert abs(x2[0].value - 57500) <= 1e-5
+    assert abs(x2[1].value - (57500 + ndays.value)) <= 1e-5
+    if ndays == 7 * u.d:
+        assert abs(x2[0].value - x1[0].value) <= 1e-5
+        assert abs(x2[1].value - x1[1].value) <= 1e-5
