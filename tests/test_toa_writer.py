6b3e9ce85eeda9dc87ff33a7f37293c440bf7520
Scott Ransom <sransom@nrao.edu>
2021-03-27 10:05:00 -0400

    Added test to comment TOAs

--- tests/test_toa_writer.py
+++ tests/test_toa_writer.py
@@ -42,10 +42,14 @@ class TestRoundtripToFiles(unittest.TestCase):
         # Create a barycentric TOA
         t1time = Time(58534.0, 0.0928602471130208, format="mjd", scale="utc")
         t1 = toa.TOA(t1time, obs="gbt", freq=0.0)
-        ts = toa.get_TOAs_list([t1], ephem="DE421")
+        t2 = toa.TOA(t1time, obs="gbt", freq=0.0)
+        ts = toa.get_TOAs_list([t1, t2], ephem="DE421")
+        assert ts.ntoas == 2
+        ts.table[1]["flags"]["ignore"] = True  # comment TOA
         ts.write_TOA_file("testtopo.tim", format="Tempo2")
         ts2 = toa.get_TOAs("testtopo.tim")
         print(ts.table, ts2.table)
+        assert ts2.ntoas == 1  # one should be commented
         assert np.abs(ts.table["mjd"][0] - ts2.table["mjd"][0]) < 1.0e-15 * u.d
         assert np.abs(ts.table["tdb"][0] - ts2.table["tdb"][0]) < 1.0e-15 * u.d
 
