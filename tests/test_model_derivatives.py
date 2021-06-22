f9c72e921389f221799913e889b8b7a846ccd635
Anne Archibald <anne.archibald@ncl.ac.uk>
2021-06-22 17:59:10 +0100

    Fix ECC step size issue

--- tests/test_model_derivatives.py
+++ tests/test_model_derivatives.py
@@ -33,83 +33,6 @@ class Values:
         return f"{os.path.basename(self.parfile)}::{self.param}"
 
 
-models = {}
-bad_models = set()
-
-
-@composite
-def model_and_free_param(draw):
-    known_bad = set()
-    parfiles = sorted(
-        set(
-            glob(os.path.join(os.path.dirname(__file__), "datafile", "*.par"))
-        ).difference(known_bad)
-    )
-    parfile = draw(sampled_from(parfiles))
-    try:
-        m = models[parfile]
-    except KeyError:
-        assume(parfile not in bad_models)
-        with quiet():
-            try:
-                m = get_model(parfile)
-            except (ValueError, AttributeError):
-                bad_models.add(parfile)
-                assume(False)
-            else:
-                models[parfile] = m
-    assume(m.free_params)
-    v = Values()
-    v.parfile = parfile
-    v.model = m
-    v.param = draw(sampled_from(m.free_params))
-    return v
-
-
-# @settings(suppress_health_check=[HealthCheck.too_slow], verbosity=Verbosity.verbose)
-# @given(model_and_free_param())
-# def test_load_ok(model_and_param):
-#    pass
-
-
-# For some reason this is extremely slow
-@settings(suppress_health_check=[HealthCheck.too_slow], verbosity=Verbosity.verbose)
-# @settings(suppress_health_check=[HealthCheck.too_slow])
-@given(model_and_free_param(), floats(50000, 60000))
-def disabled_test_derivative_equals_numerical(model_and_param, start):
-    _, model, param = (
-        model_and_param.parfile,
-        model_and_param.model,
-        model_and_param.param,
-    )
-    with quiet():
-        toas = pint.toa.make_fake_toas(
-            model=model, startMJD=start, endMJD=start + 1, ntoas=2
-        )
-        phase = model.phase(toas)
-    units = getattr(model, param).units
-
-    def f(value):
-        m = copy.deepcopy(model)
-        getattr(m, param).value = value
-        with quiet():
-            warnings.simplefilter("ignore")
-            try:
-                dphase = m.phase(toas) - phase
-            except ValueError:
-                return np.nan * np.zeros_like(phase.frac)
-        return dphase.int + dphase.frac
-
-    df = numdifftools.Derivative(f)
-
-    assert_allclose(
-        model.d_phase_d_param(toas, delay=None, param=param).to_value(1 / units),
-        df(getattr(model, param).value),
-        atol=1e-3,
-        rtol=1e-3,
-    )
-
-
 _cached_models = {}
 
 
@@ -127,9 +50,13 @@ def get_model_and_toas(parfile):
     if parfile not in _cached_toas:
         model = get_model_cached(parfile)
         with quiet():
-            toas = pint.toa.make_fake_toas(
-                model=model, startMJD=57000, endMJD=58000, ntoas=5
+            toas1 = pint.toa.make_fake_toas(
+                model=model, startMJD=57000, endMJD=58000, ntoas=5, freq=1400, obs='gbt'
             )
+            toas2 = pint.toa.make_fake_toas(
+                model=model, startMJD=57100, endMJD=58000, ntoas=5, freq=2000, obs='ao'
+            )
+            toas = pint.toa.merge_TOAs([toas1, toas2])
             phase = model.phase(toas)
         _cached_toas[parfile] = model, toas, phase
     return copy.deepcopy(_cached_toas[parfile])
@@ -174,7 +101,7 @@ def get_model_and_toas(parfile):
     + [("B1855+09_NANOGrav_12yv3.wb.gls.par", param) for param in ["M2", "SINI",]]
     + [
         ("J0023+0923_NANOGrav_11yv0.gls.par", param)
-        for param in ["FB0", "FB1", "FB2", "FB3",]
+        for param in ["FB0", "FB1", "FB2", "FB3", "FD1", "JUMP1"]
     ]
     + [("J0613-0200_NANOGrav_9yv1_ELL1H.gls.par", param) for param in ["H3", "H4",]]
     + [
@@ -186,7 +113,7 @@ def get_model_and_toas(parfile):
         for param in ["PB", "A1", "ECC", "T0", "M2", "KIN", "KOM", "PX",]
     ],
 )
-def test_derivative_equals_numerical_multi(parfile, param):
+def test_derivative_equals_numerical(parfile, param):
     model, toas, phase = get_model_and_toas(
         os.path.join(os.path.dirname(__file__), "datafile", parfile)
     )
@@ -203,7 +130,12 @@ def test_derivative_equals_numerical_multi(parfile, param):
                 return np.nan * np.zeros_like(phase.frac)
         return dphase.int + dphase.frac
 
-    df = numdifftools.Derivative(f)
+    if param == 'ECC':
+        e = model.ECC.value
+        stepgen = numdifftools.MaxStepGenerator(min(e, 1-e))
+    else:
+        stepgen = None
+    df = numdifftools.Derivative(f, step=stepgen)
 
     assert_allclose(
         model.d_phase_d_param(toas, delay=None, param=param).to_value(1 / units),
