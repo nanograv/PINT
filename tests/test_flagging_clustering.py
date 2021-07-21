2bba140cedc1b9dbd3b9c8a78f2569128a385223
David Kaplan <kaplan@uwm.edu>
2021-07-21 12:48:40 -0500

    removed tests for add_flags()

--- tests/test_flagging_clustering.py
+++ tests/test_flagging_clustering.py
@@ -1,4 +1,4 @@
-"""Tests for grouping and flagging"""
+"""Tests for clustering and flagging"""
 import logging
 import os
 import unittest
@@ -32,40 +32,9 @@ def setup_NGC6440E():
     return SimpleSetup("NGC6440E.par", "NGC6440E.tim")
 
 
-def test_add_flags(setup_NGC6440E):
+def test_jump_by_cluster(setup_NGC6440E):
     """
-    test adding a new flag
-    """
-    setup_NGC6440E.t.add_flags(np.arange(len(setup_NGC6440E.t)), "newflag", "test")
-    assert np.array(["newflag" in x for x in setup_NGC6440E.t.get_flags()]).all()
-    flag_values, flag_indices = setup_NGC6440E.t.get_flag_value("newflag")
-    assert (np.array(flag_values) == "test").all()
-
-
-def test_add_flag_twice(setup_NGC6440E):
-    """
-    test adding a new flag twice - should raise exception
-    """
-    setup_NGC6440E.t.add_flags(np.arange(len(setup_NGC6440E.t)), "newflag", "test")
-    with pytest.raises(ValueError):
-        setup_NGC6440E.t.add_flags(np.arange(len(setup_NGC6440E.t)), "newflag", "test")
-
-
-def test_add_flag_twice_overwrite(setup_NGC6440E):
-    """
-    test adding a new flag twice but with overwriting specified - should not raise exception
-    """
-    setup_NGC6440E.t.add_flags(np.arange(len(setup_NGC6440E.t)), "newflag", "test")
-    setup_NGC6440E.t.add_flags(
-        np.arange(len(setup_NGC6440E.t)), "newflag", "test2", overwrite=True
-    )
-    flag_values, flag_indices = setup_NGC6440E.t.get_flag_value("newflag")
-    assert (np.array(flag_values) == "test2").all()
-
-
-def test_jump_by_group(setup_NGC6440E):
-    """
-    Compare selection by MJD with selection by group (which by default selects TOAs by 2h gaps)
+    Compare selection by MJD with selection by cluster (which by default selects TOAs by 2h gaps)
     """
     m_copy = copy.deepcopy(setup_NGC6440E.m)
 
@@ -77,18 +46,17 @@ def test_jump_by_group(setup_NGC6440E):
     # this should be the last group of any TOAs
     cp.add_param(par, setup=True)
 
-    # add groups to the TOAs
-    groups = setup_NGC6440E.t.get_groups(2 * u.hr, add_column=False)
-    # convert group to string
-    groups = [str(x) for x in groups]
-    setup_NGC6440E.t.add_flags(np.arange(len(setup_NGC6440E.t)), "toagroup", groups)
+    # add clusters to the TOAs
+    clusters = setup_NGC6440E.t.get_clusters(2 * u.hr, add_column=False)
+    for i, v in zip(np.arange(len(setup_NGC6440E.t)), clusters):
+        setup_NGC6440E.t.table[i]["flags"]["toacluster"] = str(v)
 
     m_copy.add_component(PhaseJump(), validate=False)
     cp_copy = m_copy.components["PhaseJump"]
     par_copy = p.maskParameter(
-        name="JUMP", key="-toagroup", value=0.2, key_value=41, units=u.s
+        name="JUMP", key="-toacluster", value=0.2, key_value=41, units=u.s
     )
-    # this should be identical to the group above
+    # this should be identical to the cluster above
     cp_copy.add_param(par_copy, setup=True)
     assert (
         np.array(cp.JUMP1.select_toa_mask(setup_NGC6440E.t))
