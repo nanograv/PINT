f490854e3df7428031242a431ce611f72380b151
David Kaplan <kaplan@uwm.edu>
2021-09-01 14:23:28 -0500

    updating gridding notebook

--- docs/examples/grid_parameters.py
+++ docs/examples/grid_parameters.py
@@ -24,6 +24,7 @@
 # %%
 import astropy.units as u
 import numpy as np
+import copy
 
 import pint.config
 import pint.gridutils
@@ -34,8 +35,10 @@ from pint.models.model_builder import get_model, get_model_and_toas
 from pint.toa import get_TOAs
 import matplotlib.pyplot as plt
 from astropy.visualization import quantity_support
+
 quantity_support()
 import scipy.stats
+
 # %matplotlib inline
 
 # %%
@@ -54,15 +57,18 @@ bestfit = f.resids.chi2
 
 # %%
 F0 = np.linspace(
-        f.model.F0.quantity - 3 * f.model.F0.uncertainty,
-        f.model.F0.quantity + 3 * f.model.F0.uncertainty,
-        25,
-    )
+    f.model.F0.quantity - 3 * f.model.F0.uncertainty,
+    f.model.F0.quantity + 3 * f.model.F0.uncertainty,
+    25,
+)
 F1 = np.linspace(
-        f.model.F1.quantity - 3 * f.model.F1.uncertainty,
-        f.model.F1.quantity + 3 * f.model.F1.uncertainty,
-        27,
-    )
+    f.model.F1.quantity - 3 * f.model.F1.uncertainty,
+    f.model.F1.quantity + 3 * f.model.F1.uncertainty,
+    27,
+)
+
+# %% [markdown]
+# # 1D Grid
 
 # %%
 # Do a 1D "grid"  Make sure that the parameters are supplied as tuples with length 1
@@ -70,25 +76,41 @@ chi2_F0 = pint.gridutils.grid_chisq(f, ("F0",), (F0,))
 
 # %%
 # find the uncertainty by looking where Delta chi^2=1
-F0_delta_chi2 = np.interp(1,chi2_F0[F0>=f.model.F0.quantity]-chi2_F0.min(),F0[F0>=f.model.F0.quantity].astype(np.float64))
+F0_delta_chi2 = np.interp(
+    1,
+    chi2_F0[F0 >= f.model.F0.quantity] - chi2_F0.min(),
+    F0[F0 >= f.model.F0.quantity].astype(np.float64),
+)
 
 # %%
-fig, ax = plt.subplots(figsize=(16,9))
+fig, ax = plt.subplots(figsize=(16, 9))
 # just plot the values offset from the best-fit values
-ax.plot(F0 - f.model.F0.quantity, chi2_F0,label='$\chi^2$ curve')
-ax.plot(0, chi2_F0.min(), 'ro',markersize=12,label='Minimum $\chi^2$')
-ax.plot((f.model.F0.uncertainty)*np.ones(2),[59,69],'r:',label='Uncertainty')
-ax.plot((-f.model.F0.uncertainty)*np.ones(2),[59,69],'r:')
-ax.plot([F0_delta_chi2-f.model.F0.quantity,F0_delta_chi2-f.model.F0.quantity,-(F0_delta_chi2-f.model.F0.quantity),-(F0_delta_chi2-f.model.F0.quantity)],
-       [chi2_F0.min(),chi2_F0.min()+1,chi2_F0.min()+1,chi2_F0.min()],'g--',label='$\Delta \chi^2=1$')
-#ax.errorbar(0,0,xerr=f.model.F0.uncertainty,yerr=f.model.F1.uncertainty,fmt='ro')
-ax.set_xlabel('$\Delta F_0$ (Hz)',fontsize=24)
-ax.set_ylabel('$\chi^2$',fontsize=24)
-ax.set_ylim([59,69])
+ax.plot(F0 - f.model.F0.quantity, chi2_F0, label="$\chi^2$ curve")
+ax.plot(0, chi2_F0.min(), "ro", markersize=12, label="Minimum $\chi^2$")
+ax.plot((f.model.F0.uncertainty) * np.ones(2), [59, 69], "r:", label="Uncertainty")
+ax.plot((-f.model.F0.uncertainty) * np.ones(2), [59, 69], "r:")
+ax.plot(
+    [
+        F0_delta_chi2 - f.model.F0.quantity,
+        F0_delta_chi2 - f.model.F0.quantity,
+        -(F0_delta_chi2 - f.model.F0.quantity),
+        -(F0_delta_chi2 - f.model.F0.quantity),
+    ],
+    [chi2_F0.min(), chi2_F0.min() + 1, chi2_F0.min() + 1, chi2_F0.min()],
+    "g--",
+    label="$\Delta \chi^2=1$",
+)
+# ax.errorbar(0,0,xerr=f.model.F0.uncertainty,yerr=f.model.F1.uncertainty,fmt='ro')
+ax.set_xlabel("$\Delta F_0$ (Hz)", fontsize=24)
+ax.set_ylabel("$\chi^2$", fontsize=24)
+ax.set_ylim([59, 69])
 ax.legend()
 
 # %% [markdown]
 # From the above you can see that the calculated uncertainties on $F0$ from the model fit agree with where $\Delta \chi^2=1$, which is what we would expect for a single parameter.
+
+# %% [markdown]
+# # 2D Grid
 
 # %% [markdown]
 # Now, compute a 2D grid of $\chi^2(F0,F1)$
@@ -109,13 +131,13 @@ chi2grid = pint.gridutils.grid_chisq(f, ("F0", "F1"), (F0, F1))
 
 # %% tags=[]
 # 1, 2, and 3 sigma confidence limits
-nsigma = np.arange(1,4)
+nsigma = np.arange(1, 4)
 # these are the CDFs going from -infinity to nsigma.  So subtract away 0.5 and double for the 2-sided values
-CIs = (scipy.stats.norm().cdf(nsigma)-0.5)*2
+CIs = (scipy.stats.norm().cdf(nsigma) - 0.5) * 2
 print(f"Confidence intervals for {nsigma} sigma: {CIs}")
 # chi^2 random variable for 2 parameters
 rv = scipy.stats.chi2(2)
-x = np.linspace(0,20)
+x = np.linspace(0, 20)
 contour_levels = np.interp(CIs, rv.cdf(x), x)
 print(f"Contour levels for {nsigma} sigma and 2 parameters: {contour_levels}")
 
@@ -124,27 +146,47 @@ print(f"Contour levels for {nsigma} sigma and 2 parameters: {contour_levels}")
 
 # %%
 # 1, 2, and 3 sigma confidence limits
-nsigma = np.arange(1,4)
+nsigma = np.arange(1, 4)
 # these are the CDFs going from -infinity to nsigma.  So subtract away 0.5 and double for the 2-sided values
-CIs = (scipy.stats.norm().cdf(nsigma)-0.5)*2
+CIs = (scipy.stats.norm().cdf(nsigma) - 0.5) * 2
 print(f"Confidence intervals for {nsigma} sigma: {CIs}")
 # chi^2 random variable for 2 parameters
 rv = scipy.stats.chi2(1)
-x = np.linspace(0,20)
+x = np.linspace(0, 20)
 contour_levels_1param = np.interp(CIs, rv.cdf(x), x)
 print(f"Contour levels for {nsigma} sigma and 1 parameter: {contour_levels_1param}")
 
 # %%
-fig, ax = plt.subplots(figsize=(16,9))
+fig, ax = plt.subplots(figsize=(16, 9))
 # just plot the values offset from the best-fit values
-ax.contour(F0-f.model.F0.quantity,F1-f.model.F1.quantity,chi2grid-bestfit,levels=contour_levels,colors="b")
-ax.contour(F0-f.model.F0.quantity,F1-f.model.F1.quantity,chi2grid-bestfit,levels=contour_levels_1param,colors="g",linestyles='--')
-ax.errorbar(0,0,xerr=f.model.F0.uncertainty,yerr=f.model.F1.uncertainty,fmt='ro')
-ax.set_xlabel('$\Delta F_0$ (Hz)',fontsize=24)
-ax.set_ylabel('$\Delta F_1$ (Hz/s)',fontsize=24)
+twod = ax.contour(
+    F0 - f.model.F0.quantity,
+    F1 - f.model.F1.quantity,
+    chi2grid - bestfit,
+    levels=contour_levels,
+    colors="b",
+)
+oned = ax.contour(
+    F0 - f.model.F0.quantity,
+    F1 - f.model.F1.quantity,
+    chi2grid - bestfit,
+    levels=contour_levels_1param,
+    colors="g",
+    linestyles="--",
+)
+ax.errorbar(0, 0, xerr=f.model.F0.uncertainty, yerr=f.model.F1.uncertainty, fmt="ro")
+ax.set_xlabel("$\Delta F_0$ (Hz)", fontsize=24)
+ax.set_ylabel("$\Delta F_1$ (Hz/s)", fontsize=24)
+twod_artists, _ = twod.legend_elements()
+oned_artists, _ = oned.legend_elements()
+ax.legend(
+    [twod_artists[0], oned_artists[0]],
+    ["Joint 2D fit", "Single-parameter Fit"],
+    fontsize=18,
+)
 
 # %% [markdown]
-# It's pretty clear that $F0$ and $F1$ are highly (anti)correlated.  This can be improved by chosing **PEPOCH**.  
+# It's pretty clear that $F0$ and $F1$ are highly (anti)correlated.  This can be improved by chosing **PEPOCH**.
 #
 # We can look at the correlation matrix explicitly:
 
@@ -155,4 +197,101 @@ f.parameter_correlation_matrix
 # We can see that the $(F0,F1)$ is $-0.8$, which agrees (qualitatively) with what we see above.
 
 # %% [markdown]
-# It's also clear that the joint confidence region (solid blue contours) are significantly bigger than the single parameter region (green dashed contours).  
+# It's also clear that the joint confidence region (solid blue contours) are significantly bigger than the single parameter region (green dashed contours).
+
+# %% [markdown]
+# # Changing PEPOCH
+
+# %% [markdown]
+# To have minimal covariance between $F0$ and $F1$, we want PEPOCH to be close to the mean time of the TOAs (ideally the weighted mean).  Is it close now?
+
+# %%
+print(f"PEPOCH: {f.model.PEPOCH}")
+print(f"Mean TOA: {f.toas.get_mjds().mean()}")
+
+# %% [markdown]
+# So they differ by 141 days. Let's change that.
+
+# %%
+# keep a copy of our old fit for reference
+old_f = copy.deepcopy(f)
+
+# %%
+f.model.change_pepoch(f.toas.get_mjds().mean())
+f.fit_toas()
+new_bestfit = f.resids.chi2
+print(f"Old chi^2: {bestfit}, new chi^2: {new_bestfit}")
+print(
+    f"Old F0: {old_f.model.F0.quantity}, new F0: {f.model.F0.quantity}, difference {(f.model.F0.quantity - old_f.model.F0.quantity)/f.model.F0.uncertainty} sigma"
+)
+print(
+    f"Old F1: {old_f.model.F1.quantity}, new F1: {f.model.F1.quantity}, difference {(f.model.F1.quantity - old_f.model.F1.quantity)/f.model.F1.uncertainty} sigma"
+)
+
+# %% [markdown]
+# We see that the overall fit quality ($\chi^2$) is the same. That's good.
+#
+# $F1$ doesn't change.  That is expected.  But $F0$ does.  However, that's mostly just from changing epoch to redefine when $F0$ is evaluated.  We can check this by looking at what change we expect for $F0$:
+
+# %%
+print(
+    f"Predicted new F0 - fitted new F0 = {(old_f.model.F0.quantity + (f.model.PEPOCH.quantity - old_f.model.PEPOCH.quantity)*f.model.F1.quantity - f.model.F0.quantity)/f.model.F0.uncertainty} sigma"
+)
+
+# %% [markdown]
+# So the change is pretty close to what we expect.
+
+# %%
+# update the grids
+new_F0 = np.linspace(
+    f.model.F0.quantity - 3 * f.model.F0.uncertainty,
+    f.model.F0.quantity + 3 * f.model.F0.uncertainty,
+    len(F0),
+)
+new_F1 = np.linspace(
+    f.model.F1.quantity - 3 * f.model.F1.uncertainty,
+    f.model.F1.quantity + 3 * f.model.F1.uncertainty,
+    len(F1),
+)
+
+# %%
+new_chi2grid = pint.gridutils.grid_chisq(f, ("F0", "F1"), (new_F0, new_F1))
+
+# %% [markdown]
+# Plot the new and old contours
+
+# %%
+fig, ax = plt.subplots(figsize=(16, 9))
+# just plot the values offset from the best-fit values
+old = ax.contour(
+    F0 - old_f.model.F0.quantity,
+    F1 - old_f.model.F1.quantity,
+    chi2grid - bestfit,
+    levels=contour_levels,
+    colors="b",
+)
+new = ax.contour(
+    new_F0 - f.model.F0.quantity,
+    new_F1 - f.model.F1.quantity,
+    new_chi2grid - bestfit,
+    levels=contour_levels,
+    colors="k",
+    linestyles=":",
+)
+ax.errorbar(0, 0, xerr=f.model.F0.uncertainty, yerr=f.model.F1.uncertainty, fmt="ro")
+ax.set_xlabel("$\Delta F_0$ (Hz)", fontsize=24)
+ax.set_ylabel("$\Delta F_1$ (Hz/s)", fontsize=24)
+old_artists, _ = old.legend_elements()
+new_artists, _ = new.legend_elements()
+ax.legend([old_artists[0], new_artists[0]], ["Old fit", "New Fit"], fontsize=18)
+
+# %% [markdown]
+# The new contours (the black) look a lot more "orthogonal" than the old ones (the blue).  This is because they are less correlated.  We can check this more quantitatively by looking at the matrix:
+
+# %%
+f.parameter_correlation_matrix
+
+# %% [markdown]
+# And we see that now the $(F0,F1)$ element is smaller, $0.29$.
+
+# %%
