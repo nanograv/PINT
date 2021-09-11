457942467d79439448e8c703e93e8ebef5125d99
Deven Bhakta <drb7tg@virginia.edu>
2021-09-10 20:20:08 -0400

    Density Histogram and updated scaling docstring

--- src/pint/plot_utils.py
+++ src/pint/plot_utils.py
@@ -201,8 +201,8 @@ def plot_priors(
         The initial timing model for fitting
     chains : dict
         Post MCMC integration chains that contain the fitter keys and post
-        MCMC samples which are histogrammed. Thinning the samples from the
-        chains is not supported. Can be created using 
+        MCMC samples, which are histogrammed and normalized. Thinning the
+        samples from the chains is not supported. Can be created using 
         :meth:`pint.sampler.EmceeSampler.chains_to_dict`
     maxpost_fitvals : list, optional
         The maximum posterier values returned from MCMC integration for each
@@ -219,8 +219,12 @@ def plot_priors(
     bins : int
         Number of bins used in the histogram
     scale : bool
-        If true, the priors will be scaled to the peak of the histograms.
-        Otherwise, the priors will be plotted independent of the histograms.
+        If True, the priors will be scaled to the peak of the histograms.
+        If False, the priors will be plotted independent of the histograms.
+        In certain cases, such as broad priors, the priors or histograms
+        might not be apparent on the same plot due to one being significantly
+        larger than the other. The scaling is for visual purposes to clearly 
+        plot the priors with the samples
     plotfile : optional
         Name of the plotfile to be saved
     """
@@ -236,7 +240,7 @@ def plot_priors(
         values[i] = values[i][burnin:].flatten()
         x_range.append(np.linspace(values[i].min(), values[i].max(), num=bins))
         priors.append(getattr(model, keys[i]).prior.pdf(x_range[i]))
-        a, x = np.histogram(values[i], bins=bins)
+        a, x = np.histogram(values[i], bins=bins,density = True)
         counts.append(a)
 
     fig, axs = plt.subplots(len(keys), figsize=(8, 11), constrained_layout=True)
@@ -255,33 +259,31 @@ def plot_priors(
                 -2 * values[i].std(), color="b", linestyle="--", label="2 sigma"
             )
             axs[i].axvline(2 * values[i].std(), color="b", linestyle="--")
-            axs[i].hist(
-                values[i] - values[i].mean(), bins=bins, label="Samples",
-            )
+            axs[i].hist(values[i]-values[i].mean(), bins=bins, density=True, label="Samples")
             if scale:
                 axs[i].plot(
-                    x_range[i] - values[i].mean(),
+                    x_range[i]-values[i].mean(),
                     priors[i] * counts[i].max() / priors[i].max(),
                     label="Prior Probability",
                     color="g",
                 )
             else:
                 axs[i].plot(
-                    x_range[i] - values[i].mean(),
+                    x_range[i]-values[i].mean(),
                     priors[i],
                     label="Prior Probability",
                     color="g",
                 )
             if maxpost_fitvals is not None:
                 axs[i].axvline(
-                    maxpost_fitvals[i] - values[i].mean(),
+                    maxpost_fitvals[i]-values[i].mean(),
                     color="c",
                     linestyle="--",
                     label="Maximum Likelihood Value",
                 )
             if fitvals is not None:
                 axs[i].axvline(
-                    fitvals[i] - values[i].mean(),
+                    fitvals[i]-values[i].mean(),
                     color="m",
                     linestyle="--",
                     label="Original Parameter Fit Value",
