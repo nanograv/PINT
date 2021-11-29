7aa0451983c71a58fdb1e181fdddced34eba17f3
Patrick O'Neill <p_oneill1@outlook.com>
2021-11-29 14:47:57 +0000

    Minor changes

--- src/pint/models/stand_alone_psr_binaries/BT_piecewise.py
+++ src/pint/models/stand_alone_psr_binaries/BT_piecewise.py
@@ -36,7 +36,7 @@ class BTpiecewise(BTmodel):
         self.lower_group_edge = []    #initialise arrays to store piecewise group boundaries
         self.upper_group_edge = []
         piecewise_parameter_information = []   #initialise array that will be 5 x n in shape. Where n is the number of pieces required by the model
-        print(f"valDict:{valDict}")
+        #print(f"valDict:{valDict}")
         if valDict is None:   #If there are no updates passed by binary_instance, sets default value (usually overwritten when reading from parfile)
             self.T0X_arr = self.T0   
             self.A1X_arr = self.A1
@@ -70,39 +70,36 @@ class BTpiecewise(BTmodel):
             if len(self.piecewise_parameter_information)>0:
                 #check = hasattr(self,"t")
                 #print(f"Piecewise parameter loader can see t: {check}")
-                if hasattr(self,"t") is True:
-                    self.print_toas_in_group()   #Defines object's index for each toa as an array of length = len(self._t)
-                    self.piecewise_parameter_from_information_array()   #Uses the index for each toa array to create arrays where elements are the A1X/T0X to use with that toa
+                if hasattr(self,"_t") is True:
+                    if (self._t) is not None:
+                        #self.print_toas_in_group()   #Defines object's index for each toa as an array of length = len(self._t)
+                        self.group_index_array = self.toa_belongs_in_group(self._t)
+                        self.T0X_per_toa, self.A1X_per_toa = self.piecewise_parameter_from_information_array(self._t)   #Uses the index for each toa array to create arrays where elements are the A1X/T0X to use with that toa
+                        
                     #print(self.T0X_per_toa)
 
             
             
-    def piecewise_parameter_from_information_array(self):
-        self.A1X_per_toa = []  #arrays to store parameters to use when using toas. i.e. [toa_0,toa_1,toa_2] -> [A1X_0000,A1X_0001,A1X_0002] for one toa per group
-        self.T0X_per_toa = []
-        group_index = self.group_index()
-        for i in group_index: #for each toa
-            if i is not None:
+    def piecewise_parameter_from_information_array(self,t):
+        A1X_per_toa = []  
+        T0X_per_toa = []
+        if hasattr(self,"group_index_array") is False:
+            self.group_index_array = self.toa_belongs_in_group(t) 
+        if len(self.group_index_array) != len(t):
+            self.group_index_array = self.toa_belongs_in_group(t)
+        for i in self.group_index_array:
+            if i != -1:
                 for j in range(len(self.piecewise_parameter_information)):
                     if self.piecewise_parameter_information[j][0] == i: #searches the 5 x n array to find the index matching the toa_index
-                        self.T0X_per_toa.append(self.piecewise_parameter_information[j][1].value)
-                        self.A1X_per_toa.append(self.piecewise_parameter_information[j][2].value)
-                    
-
+                        T0X_per_toa.append(self.piecewise_parameter_information[j][1].value)
+                        A1X_per_toa.append(self.piecewise_parameter_information[j][2].value)
             else: #if a toa lies between 2 groups, use default T0/A1 values (i.e. toa lies after previous upper bound but before next lower bound)
-                #print(f'index mis-match, applying default values for T0/A1: from toa {i}')
-                self.T0X_per_toa.append(self.T0.value)
-                self.A1X_per_toa.append(self.A1.value)
-        #print(f"Unique T0X's used: {np.unique(self.T0X_per_toa,return_counts=True)[0]}")
-        #print(f"Number of toas in each group: {np.unique(self.T0X_per_toa,return_counts=True)[1]}")
-        #print(f"Total toas: {np.sum(np.unique(self.A1X_per_toa,return_counts=True)[1])}")
-        self.T0X_per_toa = self.T0X_per_toa * u.d
-        #print(self.T0X_per_toa)
-        #print(f"Unique A1X's used: {np.unique(self.A1X_per_toa,return_counts=True)[0]}")
-        #print(f"Number of toas in each group: {np.unique(self.A1X_per_toa,return_counts=True)[1]}")
-        #print(f"Total toas: {np.sum(np.unique(self.A1X_per_toa,return_counts=True)[1])}")
-        self.A1X_per_toa = self.A1X_per_toa * ls
-        
+                T0X_per_toa.append(self.T0.value)
+                A1X_per_toa.append(self.A1.value)
+        T0X_per_toa = T0X_per_toa * u.d
+        A1X_per_toa = A1X_per_toa * ls
+        return [T0X_per_toa,A1X_per_toa]
+    
     def group_index(self):
         index = []
         for i in range(len(self._t)):
@@ -111,56 +108,76 @@ class BTpiecewise(BTmodel):
             if (index1==index2):
                 index.append(index1)
             else:
-                index.append(None)
-        #print(index)
-        return index
-                
+                index.append(-1)
+        self.group_index_array = np.array(index)
+        return self.group_index_array
             
-    def print_toas_in_group(self):   #takes array sorted by lower group edge (requires groups to be chronologically ordered). Called from piece_parameter_loader, ordering operation occurs there
-        lower_bound = []   #seperates lower/upper bounds from 5 x n array of piece information
-        upper_bound = []
-        lower_index_temp = []
-        upper_index_temp = []
-        #print("hello from indexers")
-        #print(f"Length of information storage: {len(self.piecewise_parameter_information)}")
-        for i in range(0,len(self.piecewise_parameter_information)):   #loops through the array (len(...) = n)
-            if i == 0:  #for the first group, makes the lower bound slightly earlier than defined such that ambiguity of where first toa is, is accomodated
-                if len(self.piecewise_parameter_information)==1:
-                    lower_bound.append(self.piecewise_parameter_information[i][3].value-1) #modified bounds for singular group
-                    upper_bound.append(self.piecewise_parameter_information[i][4].value+1)  
-                else:
-                    lower_bound.append(self.piecewise_parameter_information[i][3].value-1) #modified sorted lower bound to encompass the first toa
-                    upper_bound.append(self.piecewise_parameter_information[i][4].value) 
-            
-            elif i==len(self.piecewise_parameter_information)-1:  #for the last group, makes the upper bound slightly later than defined such that ambiguity of where last toa is, is accomodated
-                lower_bound.append(self.piecewise_parameter_information[i][3].value)
-                upper_bound.append(self.piecewise_parameter_information[i][4].value+1) #modified sorted upper bound to encompass the last toa
-            else:
-                lower_bound.append(self.piecewise_parameter_information[i][3].value) #append all other lower/upper bounds 
-                upper_bound.append(self.piecewise_parameter_information[i][4].value)
-        if hasattr(self._t, "value") is True:
-            lower_index = np.searchsorted(lower_bound,self._t.value)-1  #Assigns group index to each toa. toa will always be on the right/left of the lower/upper bound, hence the "-1" factor
-            upper_index = np.searchsorted(upper_bound,self._t.value) #For toas between groups i.e lower bound:(55000,55100), upper bound: (55050,55150) lower/upperindex of 55075 should be (0,1)
-        else:
-            lower_index = np.searchsorted(lower_bound,self._t)-1  #Assigns group index to each toa. toa will always be on the right/left of the lower/upper bound, hence the "-1" factor
-            upper_index = np.searchsorted(upper_bound,self._t) #For toas between groups i.e lower bound:(55000,55100), upper bound: (55050,55150) lower/upperindex of 55075 should be (0
-        #print(len(upper_bound))
-        for i in lower_index: #this loop is to accomodate out of order groups
-            #print(i)
-            lower_index_temp.append(self.piecewise_parameter_information[i][0])
-        for i in upper_index:
-            if i > len(upper_bound)-1:
-                upper_index_temp.append(999)
-            else:
-                upper_index_temp.append(self.piecewise_parameter_information[i][0])
-        self.lower_index = lower_index_temp
-        self.upper_index = upper_index_temp
+    #def print_toas_in_group(self):   #takes array sorted by lower group edge (requires groups to be chronologically ordered). Called from piece_parameter_loader, ordering operation occurs there
+    #    lower_bound = []   #seperates lower/upper bounds from 5 x n array of piece information
+    #    upper_bound = []
+    #    lower_index_temp = []
+    #    upper_index_temp = []
+    #    for i in range(0,len(self.piecewise_parameter_information)):   #loops through the array (len(...) = n)
+    #        if i == 0:  #for the first group, makes the lower bound slightly earlier than defined such that ambiguity of where first toa is, is accomodated
+    #            if len(self.piecewise_parameter_information)==1:
+    #                lower_bound.append(self.piecewise_parameter_information[i][3].value-1) #modified bounds for singular group
+    #                upper_bound.append(self.piecewise_parameter_information[i][4].value+1)  
+    #            else:
+    #                lower_bound.append(self.piecewise_parameter_information[i][3].value-1) #modified sorted lower bound to encompass the first toa
+    #                upper_bound.append(self.piecewise_parameter_information[i][4].value) 
+    #        elif i==len(self.piecewise_parameter_information)-1:  #for the last group, makes the upper bound slightly later than defined such that ambiguity of where last toa is, is accomodated
+    #            lower_bound.append(self.piecewise_parameter_information[i][3].value)
+    #            upper_bound.append(self.piecewise_parameter_information[i][4].value+1) #modified sorted upper bound to encompass the last toa
+    #        else:
+    #            lower_bound.append(self.piecewise_parameter_information[i][3].value) #append all other lower/upper bounds 
+    #            upper_bound.append(self.piecewise_parameter_information[i][4].value)
+    #    if hasattr(self._t, "value") is True:
+    #        lower_index = np.searchsorted(lower_bound,self._t.value)-1  #Assigns group index to each toa. toa will always be on the right/left of the lower/upper bound, hence the "-1" factor
+    #        upper_index = np.searchsorted(upper_bound,self._t.value) #For toas between groups i.e lower bound:(55000,55100), upper bound: (55050,55150) lower/upperindex of 55075 should be (0,1)
+    #    else:
+    #        lower_index = np.searchsorted(lower_bound,self._t)-1  #Assigns group index to each toa. toa will always be on the right/left of the lower/upper bound, hence the "-1" factor
+    #        upper_index = np.searchsorted(upper_bound,self._t) #For toas between groups i.e lower bound:(55000,55100), upper bound: (55050,55150) lower/upperindex of 55075 should be: 0
+    #    for i in lower_index: #this loop is to accomodate out of order groups
+    #        lower_index_temp.append(self.piecewise_parameter_information[i][0])
+    #    for i in upper_index:
+    #        if i > len(upper_bound)-1:
+    #            upper_index_temp.append(999)
+    #        else:
+    #            upper_index_temp.append(self.piecewise_parameter_information[i][0])
+    #    self.lower_index = np.array(lower_index_temp)
+    #    self.upper_index = np.array(upper_index_temp)
     
+    
+    def toa_belongs_in_group(self,t):
+        group_no = []
+        lower_edge, upper_edge = self.get_group_boundaries()
+        for i in t:
+            lower_bound = np.searchsorted(lower_edge,i)-1
+            upper_bound = np.searchsorted(upper_edge,i)
+            if lower_bound == upper_bound:
+                index_no = (lower_bound)
+            else:
+                index_no = (-1)
+                
+            if index_no !=-1:
+                group_no.append(self.piecewise_parameter_information[index_no][0])
+            else:
+                group_no.append(index_no)
+        return group_no
+        
+    def get_group_boundaries(self):
+        lower_group_edge = []
+        upper_group_edge = []
+        #print("from get_group_boundaries")
+        if hasattr(self,"piecewise_parameter_information"):
+            for i in range(0, len(self.piecewise_parameter_information)):
+                lower_group_edge.append(self.piecewise_parameter_information[i][3])
+                upper_group_edge.append(self.piecewise_parameter_information[i][4])
+            return [lower_group_edge, upper_group_edge]
     
     def a1(self):
         self.A1_val = self.A1X_arr*ls
         if hasattr(self, "A1X_per_toa") is True:
-            #print(np.unique(self.A1X_per_toa))
             ret = self.A1X_per_toa + self.tt0 * self.A1DOT
         else:
             ret = self.A1 + self.tt0 * self.A1DOT
@@ -174,19 +191,23 @@ class BTpiecewise(BTmodel):
             return tt0
         if len(barycentricTOA)>1:
             if len(self.piecewise_parameter_information)>0:
-                self.print_toas_in_group()   #Defines object's index for each toa as an array of length = len(self._t)
-                self.piecewise_parameter_from_information_array()   #Uses the index for each toa array to create arrays where elements are the A1X/T0X to use with that toa
+                self.toa_belongs_in_group(barycentricTOA)   #Defines object's index for each toa as an array of length = len(self._t)
+                self.T0X_per_toa,self.A1X_per_toa = self.piecewise_parameter_from_information_array(self._t)   #Uses the index for each toa array to create arrays where elements are the A1X/T0X to use with that toa
         if len(barycentricTOA)>1:
             check = hasattr(self, "T0X_per_toa")
             if hasattr(self, "T0X_per_toa") is True:
                 if len(self.T0X_per_toa)==1:
                     T0 = self.T0X_per_toa
+                    #print("hello from 1")
                 else:
                     T0 = self.T0X_per_toa
+                    #print("hello from 2")
             else:
                 T0 = self.T0
+                #print("hello from 3")
         else:
             T0 = self.T0
+            #print("hello from 4")
         if not hasattr(barycentricTOA, "unit") or barycentricTOA.unit == None:
             barycentricTOA = barycentricTOA * u.day
         #print(f"Unique T0s being used in tt0 calculation: {np.unique(T0)}\n")
@@ -250,21 +271,25 @@ class BTpiecewise(BTmodel):
                     return np.zeros(len(self.t)) * u.second / par_obj.unit            
     
     def in_piece(self,par):
-        #print(par)
-        a = getattr(self,par)
-        text = par.split("_")
-        param = text[0]
-        toa_index = int(text[1])
-        group_index = np.array(self.group_index())
-        if param == "T0X":
-            ret = (group_index == toa_index)
-            return [ret,"T0X"]
-        elif param == "A1X":
-            ret = (group_index == toa_index)
-            return [ret,"A1X"]
+        if "_" in par:
+            text = par.split("_")
+            param = text[0]
+            toa_index = int(text[1])
         else:
-            return [np.zeros(len(self.tt0))+1,None]
-    
+            param = par
+        if hasattr(self, "group_index_array"):
+            group_indexes = np.array(self.group_index_array)
+            if param == "T0X":
+                ret = (group_indexes == toa_index)
+                return [ret,"T0X"]
+            elif param == "A1X":
+                ret = (group_indexes == toa_index)
+                return [ret,"A1X"]
+            else:
+                ret = (group_indexes == -1)
+                return [ret,None]
+        else:
+            return [np.zeros(len(self._t))+1,None]
     
     
     def d_BTdelay_d_par(self, par):
@@ -375,13 +400,7 @@ class BTpiecewise(BTmodel):
             errorMesg = par + " is not in binary parameter list."
             raise ValueError(errorMesg)
         par_obj = getattr(self, par)
-        
         result = self.orbits_cls.d_orbits_d_par(par)
         with u.set_enabled_equivalencies(u.dimensionless_angles()):
             result = result.to(u.Unit("") / par_obj.unit)
-        return result
-        
-#____________________________________________________________________________________________________________________________________
-        
-   
-    
+        return result   
