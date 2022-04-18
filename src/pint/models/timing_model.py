16198e2bf0166991a1b0e2229191d798f26b4140
tcromartie <thankful.cromartie@gmail.com>
2022-04-18 16:23:10 -0400

    blacken timing_model.py

--- src/pint/models/timing_model.py
+++ src/pint/models/timing_model.py
@@ -3045,9 +3045,13 @@ class AllComponents:
                 for start_idx in [0, 1]:
                     # Ensure compatibility with tempo-style "DM001"
                     if "DM0" in alias:
-                        first_init_par_alias = prefix + "{1:0{0}}".format(fmt-2, start_idx)
+                        first_init_par_alias = prefix + "{1:0{0}}".format(
+                            fmt - 2, start_idx
+                        )
                     else:
-                        first_init_par_alias = prefix + "{1:0{0}}".format(fmt, start_idx)
+                        first_init_par_alias = prefix + "{1:0{0}}".format(
+                            fmt, start_idx
+                        )
                     first_init_par = self._param_alias_map.get(
                         first_init_par_alias, None
                     )
