ac43ee2ed7ef096eb68aabd94977e5291bc85963
David Kaplan <kaplan@uwm.edu>
2022-05-09 11:18:14 -0500

    changed test to use add_flag

--- tests/test_flagging_clustering.py
+++ tests/test_flagging_clustering.py
@@ -48,14 +48,12 @@ def test_jump_by_cluster(setup_NGC6440E):
     cp.add_param(par, setup=True)
 
     # add clusters to the TOAs
-    clusters = setup_NGC6440E.t.get_clusters(2 * u.hr, add_column=False)
-    for i, v in zip(np.arange(len(setup_NGC6440E.t)), clusters):
-        setup_NGC6440E.t.table[i]["flags"]["toacluster"] = str(v)
+    clusters = setup_NGC6440E.t.get_clusters(2 * u.hr, add_column=False, add_flag=True)
 
     m_copy.add_component(PhaseJump(), validate=False)
     cp_copy = m_copy.components["PhaseJump"]
     par_copy = p.maskParameter(
-        name="JUMP", key="-toacluster", value=0.2, key_value=41, units=u.s
+        name="JUMP", key="-cluster", value=0.2, key_value=41, units=u.s
     )
     # this should be identical to the cluster above
     cp_copy.add_param(par_copy, setup=True)
