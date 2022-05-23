6d62cb8f92448b260ac2724d239bc0ee519adaf7
David Kaplan <kaplan@uwm.edu>
2022-05-23 16:54:36 -0500

    removed reimplementation of warning filters

--- src/pint/logging.py
+++ src/pint/logging.py
@@ -87,8 +87,7 @@ script_level = "WARNING"
 # Other formatting:
 # https://loguru.readthedocs.io/en/stable/api/logger.html#color
 
-warn_ = warnings.showwarning
-warning_onceregistry = {}
+showwarning_ = warnings.showwarning
 # add "once" filter for this warning
 warnings.filterwarnings("once", message="Using A1DOT with a DDK model is not advised.")
 
@@ -97,42 +96,23 @@ def showwarning(message, category, filename, lineno, file=None, line=None):
     """
     Function to allow ``loguru`` to capture warnings emitted by :func:`warnings.warn`.
 
-    Also look at the existing :data:`warnings.filters` to see if warnings should be ignored or only seen once.
+    These should have already passed any warning filters so we don't have to replicate that.
 
     See https://loguru.readthedocs.io/en/stable/resources/recipes.html#capturing-standard-stdout-stderr-and-warnings
     """
     # check to see if a standard warning filter has already been inserted that would catch whatever this is
     # this isn't the exact same implementation as the standard filter because we don't get all of the relevant pieces
     # but it works for ignoring
+    print(f"{message} {category}")
     if isinstance(message, Warning):
         message_text = str(message)
         category = message.__class__
     else:
         message_text = message
-    # take action according to standard warning filters
-    for filter in warnings.filters:
-        action, msg, cat, mod, ln = filter
-        if (msg is not None) and (msg.match(message_text)) and action == "ignore":
-            # if it matches an "ignore" message
-            return
-        # matching based on category
-        if (
-            (cat is not None)
-            and ((msg is None or msg.match(message_text)) and issubclass(category, cat))
-            and action == "ignore"
-        ):
-            return
-        # only issue once, just based on text matching.  construct a key for future matching
-        # mostly follows python warnings module
-        if action == "once":
-            oncekey = (message_text, category)
-            if warning_onceregistry.get(oncekey):
-                return
-            warning_onceregistry[oncekey] = 1
     # turn the warning into text.
     arg_string = f"{filename}:{lineno} {category.__name__}"
     log.warning(f"{arg_string}: {message_text}")
-    warn_(message, category, filename, lineno, file=file, line=line)
+    showwarning_(message, category, filename, lineno, file=file, line=line)
 
 
 class LogFilter:
