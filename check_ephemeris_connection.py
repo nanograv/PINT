2837f317cd9b8c085ffe6aa6454920632933ed08
Anne Archibald <anne.archibald@ncl.ac.uk>
2022-05-25 15:23:10 +0100

    Handle astropy failing to download ephemeris

--- check_ephemeris_connection.py
+++ check_ephemeris_connection.py
@@ -2,6 +2,7 @@ import urllib.request
 import pint.solar_system_ephemerides
 
 
-urllib.request.urlopen("https://data.nanograv.org/static/data/ephem/de440.bsp").read()
+urllib.request.urlopen("https://data.nanograv.org/static/data/ephem/de405.bsp").read()
 
-pint.solar_system_ephemerides.load_kernel("de440")
+for e in ["de440", "de405", "de421", "de430", "de430t", "de434", "de436t", "de436", "de436t"]:
+    pint.solar_system_ephemerides.load_kernel(e)
