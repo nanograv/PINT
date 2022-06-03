83154ded4131cd8063d0586d5b192bf74b4476e4
tcromartie <thankful.cromartie@gmail.com>
2022-06-03 13:11:36 -0400

    Fixed test for use in test_parameters

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -590,12 +590,12 @@ def test_parameter_can_be_pickled(p):
 
 
 def test_add_remove_param():
-    m = tm.get_model(os.path.join(datadir, "B1855+09_NANOGrav_9yv1.gls.par"))
-    t = toa.get_TOAs(os.path.join(datadir, "B1855+09_NANOGrav_9yv1.tim"))
-    FD4 = p.prefixParameter(parameter_type="float", name="FD4", value=0.0, units=u.s)
+    m = get_model(os.path.join(datadir, "B1855+09_NANOGrav_9yv1.gls.par"))
+    t = get_TOAs(os.path.join(datadir, "B1855+09_NANOGrav_9yv1.tim"))
+    FD4 = prefixParameter(parameter_type="float", name="FD4", value=0.0, units=u.s)
     m.add_param_from_top(FD4, "FD")
     assert len(m.components["FD"].params) == 4
-    f = fitter.GLSFitter(toas=t, model=m)
+    f = pint.fitter.GLSFitter(toas=t, model=m)
     m.remove_param("FD4")
     assert len(m.components["FD"].params) == 3
-    f = fitter.GLSFitter(toas=t, model=m)
+    f = pint.fitter.GLSFitter(toas=t, model=m)
