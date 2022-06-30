b8df738e3812804b1b446460df77a9d7dc7d6348
Anne Archibald <anne.archibald@ncl.ac.uk>
2022-06-30 15:46:31 +0100

    Reenable xfail that fails on oldestdeps

--- tests/test_erfautils.py
+++ tests/test_erfautils.py
@@ -118,7 +118,9 @@ def test_IERS_B_agree_with_IERS_Auto_dX():
         )
 
 
-# @pytest.mark.xfail(reason="IERS changes old values in new versions of the B table")
+@pytest.mark.xfail(
+    reason="IERS changes old values in new versions of the B table in astropy 4"
+)
 def test_IERS_B_agree_with_IERS_Auto():
     A = IERS_Auto.open()
     B = IERS_B.open(download_file(IERS_B_URL, cache=True))
