03e655bede32a09c812f7c0e738f6b41b89c9700
Abhimanyu S <abhisrkckl@gmail.com>
2022-07-12 10:18:21 -0500

    Removed unnecessary parantheses.

--- tests/test_plrednoise.py
+++ tests/test_plrednoise.py
@@ -44,10 +44,10 @@ def modelJ0023p0923():
     return mb.get_model(io.StringIO(parfile_contents))
 
 def test_read_PLRedNoise_component(modelJ0023p0923):
-    assert ('PLRedNoise' in modelJ0023p0923.components)
+    assert 'PLRedNoise' in modelJ0023p0923.components
 
 def test_read_PLRedNoise_component_type(modelJ0023p0923):
-    assert (modelJ0023p0923.components['PLRedNoise'] in modelJ0023p0923.NoiseComponent_list)
+    assert modelJ0023p0923.components['PLRedNoise'] in modelJ0023p0923.NoiseComponent_list
 
 def test_read_PLRedNoise_params(modelJ0023p0923):
     params = ['TNREDAMP', 'TNREDGAM', 'TNREDC']
