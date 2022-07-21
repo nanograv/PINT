f7af5262f20ee1b03f91e20be89384354b900980
Abhimanyu S <abhisrkckl@gmail.com>
2022-07-21 14:18:43 -0500

    par files via StringIO

--- tests/test_compare_model_J1713p0747.py
+++ tests/test_compare_model_J1713p0747.py
@@ -1,18 +1,54 @@
+import io
 import pint.models.model_builder as mb
 import pytest
 
 
 @pytest.fixture()
-def model_J1713p0747_ECL():
-    return mb.get_model("datafile/J1713+0747_NANOGrav_11yv0_short.gls.par")
+def model_ECL():
+    parfile = """
+        PSR              J1713+0747
+        LAMBDA   256.6686952405695  1     0.0000000023351
+        BETA      30.7003604937060  1     0.0000000041466
+        PMLAMBDA            5.2671  1              0.0021
+        PMBETA             -3.4428  1              0.0043
+        PX                  0.8211  1              0.0258
+        POSEPOCH        56553.0000
+        F0    218.8118437960826270  1  0.0000000000000988
+        F1     -4.083888637248D-16  1  1.433249826455D-21
+        PEPOCH        55391.000000
+        DM               15.917131
+        EPHEM               DE421
+        ECL                 IERS2003
+        CLK                 TT(BIPM2015)
+        UNITS               TDB
+        TIMEEPH             FB90
+    """
+    return mb.get_model(io.StringIO(parfile))
 
 
 @pytest.fixture()
-def model_J1713p0747_ICRS():
-    return mb.get_model("datafile/J1713+0747_NANOGrav_11yv0_short.gls.ICRS.par")
+def model_ICRS():
+    parfile = """
+        PSR                            J1713+0747
+        MODE                                    1
+        EPHEM                               DE421
+        CLK                          TT(BIPM2015)
+        UNITS                                 TDB
+        TIMEEPH                              FB90
+        RAJ                     17:13:49.53355031 1 0.00000057727246830927
+        DECJ                     7:47:37.48847685 1 0.00001419450415241618
+        PMRA                    4.925763856775273 1 0.002489055022551827
+        PMDEC                 -3.9156180453775624 1 0.004087126752953659
+        PX                                 0.8211 1 0.0258
+        POSEPOCH           55391.0000000000000000
+        F0                    218.811843796082627 1 9.88e-14
+        F1                    -4.083888637248e-16 1 1.433249826455e-21
+        PEPOCH             55391.0000000000000000
+    """
+    return mb.get_model(io.StringIO(parfile))
 
 
-def test_model_compare(model_J1713p0747_ECL, model_J1713p0747_ICRS):
-    comparison1 = model_J1713p0747_ECL.compare(model_J1713p0747_ICRS)
-    comparison2 = model_J1713p0747_ICRS.compare(model_J1713p0747_ECL)
+def test_model_compare(model_ECL, model_ICRS):
+    comparison1 = model_ECL.compare(model_ICRS)
+    comparison2 = model_ICRS.compare(model_ECL)
     assert isinstance(comparison1, str) and isinstance(comparison2, str)
