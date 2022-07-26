a449536fc7cb3bb8a30e2a9122d87b146d348dab
David Kaplan <kaplan@uwm.edu>
2022-07-26 08:15:09 -0500

    removed check for astropy <4 in __init__.py

--- src/pint/__init__.py
+++ src/pint/__init__.py
@@ -103,12 +103,3 @@ pint_units = {
     "pulse phase": u.dimensionless_unscaled,
     "hourangle_second": hourangle_second,
 }
-
-import astropy.version
-
-if astropy.version.major < 4:
-    log.warning(
-        "Using astropy version {}. To get most recent IERS data, upgrade to astropy >= 4.0".format(
-            astropy.__version__
-        )
-    )
