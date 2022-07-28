0d56a75dcf6d098d7cabd4c60d10cf91943dfc59
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2022-07-28 13:55:54 -0500

    Fixed test_designmatrix_noisepars.py

--- tests/test_designmatrix_noisepars.py
+++ tests/test_designmatrix_noisepars.py
@@ -12,35 +12,51 @@ def model_and_toas():
     return mb.get_model_and_toas(parfile, timfile)
 
 
-def _test_designmatrix(model, toas):
-    try:
+def test_designmatrix_noise_exception_no_free_noise_param(model_and_toas):
+    model, toas = model_and_toas
+
+    # Both of these should work.
+    M = model.designmatrix(toas)
+    M = model.designmatrix(toas, incnoise=False)
+
+
+def test_designmatrix_noise_exception_free_efac(model_and_toas):
+    model, toas = model_and_toas
+    model.EFAC1.frozen = False
+
+    with pytest.raises(NotImplementedError):
         # This should raise a NotImplementedError rather than an AttributeError.
         M = model.designmatrix(toas)
-    except NotImplementedError:
-        pass
 
     # This should work.
     M = model.designmatrix(toas, incnoise=False)
 
-
-def test_designmatrix_noise_exception_falsepositive(model_and_toas):
-    model, toas = model_and_toas
-    _test_designmatrix(model, toas)
+    model.EFAC1.frozen = True
 
 
-def test_designmatrix_noise_exception_efac(model_and_toas):
-    model, toas = model_and_toas
-    model.EFAC1.frozen = False
-    _test_designmatrix(model, toas)
-
-
-def test_designmatrix_noise_exception_equad(model_and_toas):
+def test_designmatrix_noise_exception_free_equad(model_and_toas):
     model, toas = model_and_toas
     model.EQUAD1.frozen = False
-    _test_designmatrix(model, toas)
+
+    with pytest.raises(NotImplementedError):
+        # This should raise a NotImplementedError rather than an AttributeError.
+        M = model.designmatrix(toas)
+
+    # This should work.
+    M = model.designmatrix(toas, incnoise=False)
+
+    model.EQUAD1.frozen = True
 
 
 def test_designmatrix_noise_exception_ecorr(model_and_toas):
     model, toas = model_and_toas
     model.ECORR1.frozen = False
-    _test_designmatrix(model, toas)
+
+    with pytest.raises(NotImplementedError):
+        # This should raise a NotImplementedError rather than an AttributeError.
+        M = model.designmatrix(toas)
+
+    # This should work.
+    M = model.designmatrix(toas, incnoise=False)
+
+    model.ECORR1.frozen = True
