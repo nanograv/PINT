3e8981d28ab833c25194a7476d3d1ef326f76f29
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2022-09-12 11:15:14 -0500

    Docstring for test

--- tests/test_combine_design_matrices_by_param.py
+++ tests/test_combine_design_matrices_by_param.py
@@ -15,14 +15,20 @@ def model_and_toas():
     return model, toas
 
 
-def test_combine_design_matrices_by_param(model_and_toas):
+def test_combine_design_matrices_by_param_repeated_params(model_and_toas):
+    """The combine_design_matrices_by_param function should throw
+    a ValueError if there is a repeated parameter and work otherwise.
+    """
+
     model, toas = model_and_toas
     dmm = DesignMatrixMaker("phase", u.Quantity(1, ""))
     dm1 = dmm(toas, model, ["F0"], offset=True)
     dm2 = dmm(toas, model, ["F1"], offset=True)
 
     with pytest.raises(ValueError):
-        dm12 = combine_design_matrices_by_param(dm1, dm2)
+        dm12_repeated_param = combine_design_matrices_by_param(dm1, dm2)
 
     dm1_no_offset = dmm(toas, model, ["F0"], offset=False)
-    dm12 = combine_design_matrices_by_param(dm1_no_offset, dm2)  # This should work.
+    dm12_no_repeated_param = combine_design_matrices_by_param(
+        dm1_no_offset, dm2
+    )  # This should work.
