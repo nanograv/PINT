75e3a0991920f448b36d463ebf5ba35c0aa9b03b
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2022-10-05 11:31:49 -0500

    cleanup test_stigma.py test_solar_wind.py test_solar_system_body.py

--- tests/test_stigma.py
+++ tests/test_stigma.py
@@ -1,18 +1,8 @@
-import logging
-import os
-import io
-
-import astropy.units as u
-import numpy as np
 import pytest
 import tempfile
 
 from pint.models import get_model
-import pint.toa as toa
 import pint.simulation as simulation
-import test_derivative_utils as tdu
-from pint.residuals import Residuals
-from pinttestdata import datadir
 
 
 stigma_template = """
