3ab7067a39aa8f8160be3f073f7ce1394d1a99ef
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2022-10-31 10:23:22 -0500

    zima wb test

--- tests/test_zima.py
+++ tests/test_zima.py
@@ -13,7 +13,19 @@ def test_result(tmp_path):
     timfile = tmp_path / "fake_testzima.tim"
     saved_stdout, sys.stdout = sys.stdout, StringIO("_")
     try:
-        cmd = "{0} {1}".format(parfile, timfile)
+        cmd = f"{parfile} {timfile}"
+        zima.main(cmd.split())
+        lines = sys.stdout.getvalue()
+    finally:
+        sys.stdout = saved_stdout
+
+
+def test_wb_result(tmp_path):
+    parfile = os.path.join(datadir, "NGC6440E.par")
+    timfile = tmp_path / "fake_testzima_wb.tim"
+    saved_stdout, sys.stdout = sys.stdout, StringIO("_")
+    try:
+        cmd = f"{parfile} {timfile} --addnoise --simulatedm --dmerror 1e-5"
         zima.main(cmd.split())
         lines = sys.stdout.getvalue()
     finally:
