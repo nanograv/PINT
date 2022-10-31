Fix numerical issue in gradient evaluation.

src/pint/templates/lcnorm.py
@@ -230,15 +230,17 @@ class NormAngles:
         else:
             m = np.zeros((self.dim, self.dim,p.shape[1]), dtype=float)
         s2p = np.sin(2 * p)
-        # TODO -- sometimes this produces a divide by zero (particularly
-        # the /sin(p) part).  Is it better to catch this?  It still
-        # blows up formally in the limit
+        # NB -- the gradient is always well defined, and numerical issues
+        # here stem from the way the gradient is evaluated by first
+        # calculating the normal. I *think* that any matrix element that
+        # involves cot(p) where p-->0 will always be 0.  Thus, simply set
+        # the places where p is 0 to 0.
+        mask = p%(0.5*np.pi) != 0
+        sp = np.zeros(p.shape)
         cp = -s2p / np.cos(p) ** 2 # = -2*tan(p)
-        sp = s2p / np.sin(p) ** 2  # =  2*cot(p)
-        #mask = cp != 0
-        #sp = cp.copy()
-        #sp[sp==0] = -1e-20
-        #sp = -1./sp
+        cp[~mask] = 0
+        sp = np.zeros_like(cp)
+        sp[mask] = s2p[mask] / np.sin(p[mask])**2 # = 2*cot(p)
         # loop over normalizations
         for i in range(self.dim):
             for j in range(self.dim):