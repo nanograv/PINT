ccc94d26ebec0103279b2d61ff85a2d1be00f0c4
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2022-11-12 10:56:26 -0600

    remove redundant cmd line args

--- src/pint/scripts/compare_parfiles.py
+++ src/pint/scripts/compare_parfiles.py
@@ -20,24 +20,10 @@ def main(argv=None):
         "--dmx", default=False, action="store_true", help="Print DMX parameters"
     )
     parser.add_argument(
-        "--no-dmx",
-        dest="dmx",
-        default=False,
-        action="store_false",
-        help="Do not print DMX parameters",
-    )
-    parser.add_argument(
         "--convertcoordinates",
         default=True,
         action="store_true",
         help="Convert coordinates to make models consistent",
-    )
-    parser.add_argument(
-        "--no-convertcoordinates",
-        dest="convertcoordinates",
-        default=False,
-        action="store_false",
-        help="Do not convert coordinates to make models consistent",
     )
     parser.add_argument(
         "--sigma",
