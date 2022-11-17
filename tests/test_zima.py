139d192da8355f5e3da5a4b77f2b05b2c4b53819
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2022-11-17 15:01:24 -0600

    test_zima_inputtim

--- tests/test_zima.py
+++ tests/test_zima.py
@@ -8,12 +8,26 @@ import pint.scripts.zima as zima
 from pinttestdata import datadir
 
 
-def test_result(tmp_path):
+def test_zima(tmp_path):
     parfile = os.path.join(datadir, "NGC6440E.par")
-    timfile = tmp_path / "fake_testzima.tim"
+    output_timfile = tmp_path / "fake_testzima.tim"
     saved_stdout, sys.stdout = sys.stdout, StringIO("_")
     try:
-        cmd = "{0} {1}".format(parfile, timfile)
+        cmd = "{0} {1}".format(parfile, output_timfile)
+        zima.main(cmd.split())
+        lines = sys.stdout.getvalue()
+    finally:
+        sys.stdout = saved_stdout
+
+
+def test_zima_inputtim(tmp_path):
+    parfile = os.path.join(datadir, "NGC6440E.par")
+    input_timfile = os.path.join(datadir, "NGC6440E.tim")
+    output_timfile = tmp_path / "fake_testzima.tim"
+
+    saved_stdout, sys.stdout = sys.stdout, StringIO("_")
+    try:
+        cmd = f"--inputtim {input_timfile} {parfile} {output_timfile}"
         zima.main(cmd.split())
         lines = sys.stdout.getvalue()
     finally:
