5ddd527c69fade4c9c3261fd3d8bfdda01b42af7
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2022-11-18 07:15:52 -0600

    put the precision back

--- tests/test_reduced_precision.py
+++ tests/test_reduced_precision.py
@@ -1,14 +1,16 @@
 import numpy as np
-
-np.longdouble = np.float128 = np.float64
-
 from pint.utils import require_longdouble_precision, PINTPrecisionError
 import pytest
 
 
 def test_require_longdouble_precision():
+    ld = np.longdouble
+    np.longdouble = np.float128 = np.float64
+
     # There are some limited precision warnings here.
     import pint.pulsar_mjd
 
     with pytest.raises(PINTPrecisionError):
         require_longdouble_precision()
+
+    np.longdouble = np.float128 = ld
