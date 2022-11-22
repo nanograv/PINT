77f45a5bc68596ee316066d0e72570db75a73ac5
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2022-11-22 15:20:31 -0600

    monkeypatch in test_reduced_precision.py

--- tests/test_reduced_precision.py
+++ tests/test_reduced_precision.py
@@ -3,14 +3,12 @@ from pint.utils import require_longdouble_precision, PINTPrecisionError
 import pytest
 
 
-def test_require_longdouble_precision():
-    ld = np.longdouble
-    np.longdouble = np.float128 = np.float64
+def test_require_longdouble_precision(monkeypatch):
+    monkeypatch.setattr(np, "longdouble", np.float64)
+    monkeypatch.setattr(np, "float128", np.float64)
 
     # There are some limited precision warnings here.
     import pint.pulsar_mjd
 
     with pytest.raises(PINTPrecisionError):
         require_longdouble_precision()
-
-    np.longdouble = np.float128 = ld
