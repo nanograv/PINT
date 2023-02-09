dd74b13dbd5253934015276eaa0474871eed9489
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2023-02-09 17:49:06 -0600

    __init__

--- src/pint/models/parameter/masked_prefix_parameter.py
+++ src/pint/models/parameter/masked_prefix_parameter.py
@@ -6,7 +6,9 @@ from pint.utils import split_masked_prefixed_name
 class MaskedPrefixParameter(prefixParameter):
     def __init__(
         self,
-        name=None,
+        prefix=None,
+        prefix_index=1,
+        mask_index=1,
         value=None,
         units=None,
         unit_template=None,
@@ -15,60 +17,59 @@ class MaskedPrefixParameter(prefixParameter):
         uncertainty=None,
         frozen=True,
         continuous=True,
-        prefix_aliases=None,
         long_double=False,
         unit_scale=False,
-        scale_factor=None,
-        scale_threshold=None,
-        time_scale="utc",
         **kwargs,
     ):
         # Split prefixed name, if the name is not in the prefixed format, error
         # will be raised
-        self.name = name
-        (
-            self.prefix,
-            (self.prefix_idxfmt, self.mask_idxfmt),
-            (self.prefix_index, self.mask_index),
-        ) = split_masked_prefixed_name(name)
+        self.prefix = prefix
+        self.prefix_index = prefix_index
+        self.mask_index = mask_index
+        self.name = f"{prefix}{prefix_index}_{mask_index}"
+        self.mask_name = f"{prefix}{prefix_index}"
+
         self.parameter_type = "mask"
         self.param_class = maskParameter
 
         # Set up other attributes in the wrapper class
         self.unit_template = unit_template
         self.description_template = description_template
+
         input_units = units
         input_description = description
-        self.prefix_aliases = [] if prefix_aliases is None else prefix_aliases
+
+        self.prefix_aliases = []
         # set templates, the templates should be a named function and input is
         # the index of prefix parameter.
 
         # Set the description and units for the parameter composition.
         if self.unit_template is not None:
-            real_units = self.unit_template(self.index)
+            real_units = self.unit_template(self.prefix_index)
         else:
             real_units = input_units
+
         if self.description_template is not None:
             real_description = self.description_template(self.index)
         else:
             real_description = input_description
-        aliases = [pa + self.idxfmt for pa in self.prefix_aliases]
+
         self.long_double = long_double
+
         # initiate parameter class
         self.param_comp = self.param_class(
-            name=self.name,
+            name=self.mask_name,
             value=value,
             units=real_units,
             description=real_description,
             uncertainty=uncertainty,
             frozen=frozen,
             continuous=continuous,
-            aliases=aliases,
             long_double=long_double,
-            time_scale=time_scale,
             unit_scale=unit_scale,
-            scale_factor=scale_factor,
-            scale_threshold=scale_threshold,
         )
+        self.param_comp.name = self.name
+        self.param_comp.origin_name = self.mask_name
+
         self.is_prefix = True
-        self.time_scale = time_scale
+        self.is_mask = True
