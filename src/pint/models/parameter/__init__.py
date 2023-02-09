275cb3b02cc7a0e4a909e2be1c4042332cf03fcd
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2023-02-09 15:18:27 -0600

    prefix_parameter.py

--- src/pint/models/parameter/__init__.py
+++ src/pint/models/parameter/__init__.py
@@ -1,3 +1,4 @@
 from pint.models.parameter.parameter import *
 from pint.models.parameter.pair_parameter import pairParameter
 from pint.models.parameter.mask_parameter import maskParameter
+from pint.models.parameter.prefix_parameter import prefixParameter
