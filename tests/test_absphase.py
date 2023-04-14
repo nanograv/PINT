cd504ffe68347d38ab06e747badc9a292cd69e69
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2023-04-14 09:07:41 -0500

    test_offset

--- tests/test_absphase.py
+++ tests/test_absphase.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 import os
 import unittest
+import io
 
 import pint.models
 import pint.toa
+from astropy import units as u
 from pinttestdata import datadir
 
 parfile = os.path.join(datadir, "NGC6440E.par")
@@ -20,3 +22,12 @@ class TestAbsPhase(unittest.TestCase):
         # Check that integer and fractional phase values are very close to 0.0
         self.assertAlmostEqual(ph.int.value, 0.0)
         self.assertAlmostEqual(ph.frac.value, 0.0)
+
+
+def test_offset():
+    par = """
+    F0 100 1
+    OFFSET 0.5 1
+    """
+    m = pint.models.get_model(io.StringIO(par))
+    assert hasattr(m, "OFFSET") and m.OFFSET.quantity == u.Quantity(0.5, "rad")
