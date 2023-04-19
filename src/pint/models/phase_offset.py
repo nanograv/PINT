964efe06207ffb69bc5bcddd15571ec0d567a883
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2023-04-19 10:16:32 -0500

    phase offset component

---
+++ src/pint/models/phase_offset.py
@@ -0,0 +1,48 @@
+"""Explicit phase offset"""
+
+from pint.models.timing_model import PhaseComponent
+from pint.models.parameter import floatParameter
+from astropy import units as u
+import numpy as np
+
+
+class PhaseOffset(PhaseComponent):
+    """Explicit pulse phase offset.
+
+    Parameters supported:
+
+    .. paramtable::
+        :class: pint.models.jump.PhaseOffset
+    """
+
+    register = True
+    category = "phase_offset"
+
+    def __init__(self):
+        super().__init__()
+        self.add_param(
+            floatParameter(
+                name="PHOFF",
+                value=0.0,
+                units="",
+                description="Overall phase offset.",
+            )
+        )
+        self.phase_funcs_component += [self.offset_phase]
+        self.register_deriv_funcs(self.d_offset_phase_d_PHOFF, "PHOFF")
+
+    def offset_phase(self, toas, delay):
+        """An overall phase offset between physical TOAs and the TZR TOA."""
+
+        return (
+            (np.zeros(len(toas)) * self.PHOFF.quantity).to(u.dimensionless_unscaled)
+            if toas.tzr
+            else (np.ones(len(toas)) * self.PHOFF.quantity).to(u.dimensionless_unscaled)
+        )
+
+    def d_offset_phase_d_PHOFF(self, toas, param, delay):
+        return (
+            np.zeros(len(toas)) * u.Unit("")
+            if toas.tzr
+            else np.ones(len(toas)) * u.Unit("")
+        )
