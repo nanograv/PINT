65444f949aa1659c4340c7c8e22d4323681b77ae
David Kaplan <kaplan@uwm.edu>
2023-05-19 09:00:59 -0500

    changed test

--- tests/test_parunits.py
+++ tests/test_parunits.py
@@ -21,7 +21,6 @@ from pint.utils import get_unit
         "PB",
         "RA",
         "A1",
-        "PLANET_SHAPIRO",
         "M2",
         "EDOT",
         "ECC",
@@ -37,7 +36,20 @@ from pint.utils import get_unit
 def test_par_units(p):
     unit = get_unit(p)
     print(f"{p}: {unit}")
-    assert isinstance(unit, u.UnitBase) or unit is None
+    assert isinstance(unit, u.UnitBase)
+
+
+# strings should have units of None
+@pytest.mark.parametrize(
+    "p",
+    [
+        "PLANET_SHAPIRO",
+    ],
+)
+def test_par_units_none(p):
+    unit = get_unit(p)
+    print(f"{p}: {unit}")
+    assert unit is None
 
 
 def test_par_units_fails():
