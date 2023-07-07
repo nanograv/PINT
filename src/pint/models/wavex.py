3a2424d15461cebd885c8b577b59a08c948b623b
LuluAgazie <gabriella.agazie@gmail.com>
2023-07-06 22:08:15 -0500

    added comments

--- src/pint/models/wavex.py
+++ src/pint/models/wavex.py
@@ -5,69 +5,79 @@ import numpy as np
 from pint.models.parameter import MJDParameter, floatParameter, prefixParameter
 from pint.models.timing_model import DelayComponent, MissingParameter
 
+
 class WaveX(DelayComponent):
-# These lines are in wave.py and not sure what they are meant to do
+    """Implementation of the wave model as a delay correction
+    Delays are expressed as a sum of sinusoids.
+    Used for decomposition of timing noise into a series of sine/cosine components
+    with the amplitudes as fitted parameters.
+
+
+    Parameters supported:
+
+    .. paramtable::
+        :class: pint.models.wave.Wave
+
+    This is an extension of the L13 method described in Lentati et al., 2013
+    """
+
     register = True
-#  category = "wave"
+
+    #  category = "wave"
     def __init__(self):
         super().__init__()
         self.add_param(
             MJDParameter(
-                name= "WXEPOCH",
+                name="WXEPOCH",
                 description="Reference epoch for wave delay solution",
                 time_scale="tdb",
             )
         )
         self.add_param(
             prefixParameter(
-            name="WXFREQ_",
-            description="Base frequency of wave delay solution",
-            units="1/d",
+                name="WXFREQ_",
+                description="Base frequency of wave delay solution",
+                units="1/d",
             )
         )
         self.add_param(
             prefixParameter(
-            name="WXSIN_",
-            description="Sine amplitudes for wave delay function",
-            units="s",
+                name="WXSIN_",
+                description="Sine amplitudes for wave delay function",
+                units="s",
             )
         )
         self.add_param(
             prefixParameter(
-            name="WXCOS_",
-            description="Cosine amplitudes for wave delay function",
-            units="s",
+                name="WXCOS_",
+                description="Cosine amplitudes for wave delay function",
+                units="s",
             )
         )
-        #self.delay_funcs_component += [self.wavex_delay]
+        # self.delay_funcs_component += [self.wavex_delay]
 
-    #Initialize setup 
-    # def setup(self):
-    #     super().setup()
+    # Initialize setup
+    def setup(self):
+        super().setup()
+
     #     self.wave_freqs = list(self.get_prefix_mapping_component("WXFREQ_").keys())
     #     self.num_wave_freqs = len(self.wave_freqs)
-    
-    #Placeholder for validation tests
+
+    # Placeholder for validation tests
     # def validate(self)
 
-    def wavex_delay(self,toas,delays):
+    def wavex_delay(self, toas, delays):
         total_delay = 0
-       # wave_freq_params = self.get_prefix_mapping_component("WXFREQ_")
+        # wave_freq_params = self.get_prefix_mapping_component("WXFREQ_")
         wave_freqs = self.get_prefix_mapping_component("WXFREQ_").values()
         wave_sins = self.get_prefix_mapping_component("WXSIN_").values()
         wave_cos = self.get_prefix_mapping_component("WXSIN_").values()
         base_phase = (
-            toas.table["tbdld"] * u.day
-            - self.WXEPOCH.value * u.day
-            - delays.to(u.day)
+            toas.table["tbdld"] * u.day - self.WXEPOCH.value * u.day - delays.to(u.day)
         ).value
-        for f,freq in enumerate(wave_freqs):
-            arg = 2. * np.pi * freq.value * base_phase
-            total_delay += (wave_sins[f] * np.sin(arg) + wave_cos[f] * np.cos(arg))
+        for f, freq in enumerate(wave_freqs):
+            arg = 2.0 * np.pi * freq.value * base_phase
+            total_delay += wave_sins[f] * np.sin(arg) + wave_cos[f] * np.cos(arg)
 
-    #Placeholder for calculations of derivatives
+    # Placeholder for calculations of derivatives
     # def d_wavex_delay
-
-
-
-
