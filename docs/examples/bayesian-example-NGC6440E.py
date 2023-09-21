474876c0cce37243adf55602c69597b9f568d1a4
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2023-09-21 11:30:10 -0500

    --

--- docs/examples/bayesian-example-NGC6440E.py
+++ docs/examples/bayesian-example-NGC6440E.py
@@ -1,3 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.7
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
 # %% [markdown]
 # # PINT Bayesian Interface Examples
 
--- docs/examples/bayesian-wideband-example.py
+++ docs/examples/bayesian-wideband-example.py
@@ -1,3 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # PINT Bayesian Interface Example (Wideband)
+
 # %%
 import corner
 import emcee
