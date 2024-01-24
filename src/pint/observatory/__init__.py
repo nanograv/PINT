0e29ef8965cd20d214515c92a2a260154e759859
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-01-24 14:34:23 -0600

    don't overwrite include_gps & include_bipm for ssb

--- src/pint/observatory/__init__.py
+++ src/pint/observatory/__init__.py
@@ -476,8 +476,12 @@ def get_observatory(
         file switches/options are added at a public API level.
 
     """
-    if include_bipm is not None or include_gps is not None:
-        site = deepcopy(Observatory.get(name))
+    site0 = Observatory.get(name)
+
+    if (
+        include_bipm is not None or include_gps is not None
+    ) and site0.name != "barycenter":
+        site = deepcopy(site0)
 
         if include_gps is not None:
             site.include_gps = include_gps
@@ -488,7 +492,7 @@ def get_observatory(
 
         return site
 
-    return Observatory.get(name)
+    return site0
 
 
 def earth_location_distance(loc1, loc2):
