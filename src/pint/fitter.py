1befd58046e27ba3591022bf5b3d4816efe560da
Anne Archibald <peridot.faceted@gmail.com>
2024-02-28 19:33:23 +0000

    Get fitter working

--- src/pint/fitter.py
+++ src/pint/fitter.py
@@ -60,6 +60,7 @@ To automatically select a fitter based on the properties of the data and model::
 
 import contextlib
 import copy
+from functools import cached_property
 from typing import List, Optional
 from warnings import warn
 
@@ -72,8 +73,9 @@ from numdifftools import Hessian
 
 import pint
 import pint.derived_quantities
-import pint.utils
+import pint.models
 import pint.models.timing_model
+import pint.utils
 from pint.models.parameter import (
     AngleParameter,
     boolParameter,
@@ -107,64 +109,6 @@ __all__ = [
     "StepProblem",
     "MaxiterReached",
 ]
-
-try:
-    from functools import cached_property
-except ImportError:
-    # not supported in python 3.7
-    # This is just the code from python 3.8
-    from _thread import RLock
-
-    _NOT_FOUND = object()
-
-    class cached_property:
-        def __init__(self, func):
-            self.func = func
-            self.attrname = None
-            self.__doc__ = func.__doc__
-            self.lock = RLock()
-
-        def __set_name__(self, owner, name):
-            if self.attrname is None:
-                self.attrname = name
-            elif name != self.attrname:
-                raise TypeError(
-                    "Cannot assign the same cached_property to two different names "
-                    f"({self.attrname!r} and {name!r})."
-                )
-
-        def __get__(self, instance, owner=None):
-            if instance is None:
-                return self
-            if self.attrname is None:
-                raise TypeError(
-                    "Cannot use cached_property instance without calling __set_name__ on it."
-                )
-            try:
-                cache = instance.__dict__
-            except AttributeError:
-                # not all objects have __dict__ (e.g. class defines slots)
-                msg = (
-                    f"No '__dict__' attribute on {type(instance).__name__!r} "
-                    f"instance to cache {self.attrname!r} property."
-                )
-                raise TypeError(msg) from None
-            val = cache.get(self.attrname, _NOT_FOUND)
-            if val is _NOT_FOUND:
-                with self.lock:
-                    # check if another thread filled cache while we awaited lock
-                    val = cache.get(self.attrname, _NOT_FOUND)
-                    if val is _NOT_FOUND:
-                        val = self.func(instance)
-                        try:
-                            cache[self.attrname] = val
-                        except TypeError:
-                            msg = (
-                                f"The '__dict__' attribute on {type(instance).__name__!r} instance "
-                                f"does not support item assignment for caching {self.attrname!r} property."
-                            )
-                            raise TypeError(msg) from None
-            return val
 
 
 class DegeneracyWarning(UserWarning):
@@ -1119,17 +1063,19 @@ class DownhillFitter(Fitter):
             self.parameter_covariance_matrix.to_correlation_matrix()
         )
 
-        for p, e in zip(self.current_state.params, self.errors):
+        for p, error in zip(self.current_state.params, self.errors):
             try:
                 # I don't know why this fails with multiprocessing, but bypass if it does
                 with contextlib.suppress(ValueError):
-                    log.trace(f"Setting {getattr(self.model, p)} uncertainty to {e}")
+                    log.trace(
+                        f"Setting {getattr(self.model, p)} uncertainty to {error}"
+                    )
                 pm = getattr(self.model, p)
             except AttributeError:
                 if p != "Offset":
                     log.warning(f"Unexpected parameter {p}")
             else:
-                pm.uncertainty = e * pm.units
+                pm.uncertainty = error * pm.units
         self.update_model(self.current_state.chi2)
         if exception is not None:
             raise StepProblem(
