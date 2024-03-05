fixing units in binary delay derivatives when deriving with respect to parameters with unit "deg" now returns correct units

src/pint/models/stand_alone_psr_binaries/BT_model.py
@@ -143,17 +143,17 @@ class BTmodel(PSR_BINARY):
         """Full BT model delay"""
         return (self.delayL1() + self.delayL2()) * self.delayR()
 
-    # NOTE: Below, OMEGA is supposed to be in RADIANS!
-    # TODO: Fix UNITS!!!
     def d_delayL1_d_E(self):
         a1 = self.a1() / c.c
-        return -a1 * np.sin(self.omega()) * np.sin(self.E())
+        return -a1 * np.sin(self.omega()) * np.sin(self.E()) / self.E().unit
 
     def d_delayL2_d_E(self):
         a1 = self.a1() / c.c
         return (
-            a1 * np.cos(self.omega()) * np.sqrt(1 - self.ecc() ** 2) + self.GAMMA
-        ) * np.cos(self.E())
+            (a1 * np.cos(self.omega()) * np.sqrt(1 - self.ecc() ** 2) + self.GAMMA)
+            * np.cos(self.E())
+            / self.E().unit
+        )
 
     def d_delayL1_d_A1(self):
         return np.sin(self.omega()) * (np.cos(self.E()) - self.ecc()) / c.c
@@ -171,7 +171,12 @@ class BTmodel(PSR_BINARY):
 
     def d_delayL1_d_OM(self):
         a1 = self.a1() / c.c
-        return a1 * np.cos(self.omega()) * (np.cos(self.E()) - self.ecc())
+        return (
+            a1
+            * np.cos(self.omega())
+            * (np.cos(self.E()) - self.ecc())
+            / self.omega().unit
+        )
 
     def d_delayL1_d_OMDOT(self):
         return self.tt0 * self.d_delayL1_d_OM()
@@ -179,7 +184,11 @@ class BTmodel(PSR_BINARY):
     def d_delayL2_d_OM(self):
         a1 = self.a1() / c.c
         return (
-            -a1 * np.sin(self.omega()) * np.sqrt(1 - self.ecc() ** 2) * np.sin(self.E())
+            -a1
+            * np.sin(self.omega())
+            * np.sqrt(1 - self.ecc() ** 2)
+            * np.sin(self.E())
+            / self.omega().unit
         )
 
     def d_delayL2_d_OMDOT(self):