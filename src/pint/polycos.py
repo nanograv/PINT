c0f100998ced2a3771fe19b79edb84bd9941268e
Paul Ray <paul.ray@nrl.navy.mil>
2024-04-26 14:03:48 -0400

    Update CHANGELOG for v1.0

--- src/pint/polycos.py
+++ src/pint/polycos.py
@@ -32,6 +32,7 @@ References
 ----------
 http://tempo.sourceforge.net/ref_man_sections/tz-polyco.txt
 """
+
 import astropy.table as table
 import astropy.units as u
 import numpy as np
@@ -84,7 +85,7 @@ class PolycoEntry:
     """One Polyco entry.
 
     Referenced from polyco.py authored by
-        - Paul S. Ray <paul.ray@nrl.navy.mil>
+        - Paul S. Ray <paul.s.ray3.civ@us.navy.mil>
         - Matthew Kerr <matthew.kerr@gmail.com>
 
     Parameters
