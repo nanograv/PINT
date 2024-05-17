7294cc9845d0b09c91b4d324f090dddc8ce6c687
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-05-17 09:48:42 +0200

    test_solarsystemshapiro

--- tests/test_solarsystemshapiro.py
+++ tests/test_solarsystemshapiro.py
@@ -5,7 +5,7 @@ import pytest
 
 def test_solarsystemshapiro_exception():
     m, t = get_model_and_toas(datadir / "NGC6440E.par", datadir / "NGC6440E.tim")
-    m.PLANET.SHAPIRO.value = True
+    m.PLANET_SHAPIRO.value = True
 
     with pytest.raises(KeyError):
         m.components["SolarSystemShapiro"].solar_system_shapiro_delay(t)
