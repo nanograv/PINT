557ab55c4b19c821a22afef44862232707a1578f
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-07-11 12:38:33 +0200

    test_dmwavex

--- tests/test_dmwavex.py
+++ tests/test_dmwavex.py
@@ -11,23 +11,23 @@ from pint import dmu
 import pytest
 import astropy.units as u
 
-par = """
-    PSR              B1937+21
-    LAMBDA   301.9732445337270
-    BETA      42.2967523367957
-    PMLAMBDA           -0.0175
-    PMBETA             -0.3971
-    PX                  0.1515
-    POSEPOCH        55321.0000
-    F0    641.9282333345536244  1  0.0000000000000132
-    F1     -4.330899370129D-14  1  2.149749089617D-22
-    PEPOCH        55321.000000
-    DM               71.016633
-    UNITS                  TDB
-"""
-
 
 def test_dmwavex():
+    par = """
+        PSR              B1937+21
+        LAMBDA   301.9732445337270
+        BETA      42.2967523367957
+        PMLAMBDA           -0.0175
+        PMBETA             -0.3971
+        PX                  0.1515
+        POSEPOCH        55321.0000
+        F0    641.9282333345536244  1  0.0000000000000132
+        F1     -4.330899370129D-14  1  2.149749089617D-22
+        PEPOCH        55321.000000
+        DM               71.016633
+        UNITS                  TDB
+    """
+
     m = get_model(StringIO(par))
 
     with pytest.raises(ValueError):
@@ -111,6 +111,21 @@ def test_dmwavex_badpar():
 
 
 def test_add_dmwavex():
+    par = """
+        PSR              B1937+21
+        LAMBDA   301.9732445337270
+        BETA      42.2967523367957
+        PMLAMBDA           -0.0175
+        PMBETA             -0.3971
+        PX                  0.1515
+        POSEPOCH        55321.0000
+        F0    641.9282333345536244  1  0.0000000000000132
+        F1     -4.330899370129D-14  1  2.149749089617D-22
+        PEPOCH        55321.000000
+        DM               71.016633
+        UNITS                  TDB
+    """
+
     m = get_model(StringIO(par))
     idxs = dmwavex_setup(m, 3600, n_freqs=5)
 
