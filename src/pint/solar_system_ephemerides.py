92016db5d3507c3926753cd7c4dabb30d4773482
David Kaplan <kaplan@uwm.edu>
2024-08-21 11:05:57 -0500

    ability to use local ephemeris, with tests and docs

--- src/pint/solar_system_ephemerides.py
+++ src/pint/solar_system_ephemerides.py
@@ -1,9 +1,12 @@
 """Solar system ephemeris downloading and setting support."""
 
 import os
+import pathlib
+from typing import Optional, Union
 
 import astropy.coordinates
 import astropy.units as u
+import astropy.utils.state
 import contextlib
 import numpy as np
 from astropy.utils.data import download_file
@@ -41,21 +44,67 @@ jpl_obj_code = {
     "pluto": 9,
 }
 
+loaded_ephems = {}
 
-def _load_kernel_link(ephem, link=None):
+
+def clear_loaded_ephem() -> None:
+    """Clear the dictionary of pre-loaded ephemeris files, to allow fresh loading"""
+    loaded_ephems = {}
+
+
+def _load_kernel_link(
+    ephem: str, link: Optional[str] = None
+) -> astropy.utils.state._ScienceStateContext:
+    """Load an ephemeris file from a URL
+
+    Parameters
+    ----------
+    ephem : str
+        Name of ephemeris (without ``bsp`` extension)
+    link : str, Optional
+        URL
+
+    Returns
+    -------
+    loaded_ephemeris : astropy.utils.state._ScienceStateContext
+
+    Notes
+    -----
+    If ``link`` is None, will still search default mirror sites
+    """
     if link == "":
         raise ValueError("Empty string is not a valid URL")
 
     mirrors = [f"{m}{ephem}.bsp" for m in ephemeris_mirrors]
     if link is not None:
         mirrors = [link] + mirrors
-    astropy.coordinates.solar_system_ephemeris.set(
+    loaded = astropy.coordinates.solar_system_ephemeris.set(
         download_file(mirrors[0], cache=True, sources=mirrors)
     )
     log.info(f"Set solar system ephemeris to {ephem} from download")
+    return loaded
 
 
-def _load_kernel_local(ephem, path):
+def _load_kernel_local(
+    ephem: str, path: Union[str, pathlib.Path]
+) -> Union[str, pathlib.Path]:
+    """Load an ephemeris file from a URL
+
+    Parameters
+    ----------
+    ephem : str
+        Name of ephemeris (without ``bsp`` extension)
+    path : str or pathlib.Path
+        Path to file
+
+    Returns
+    -------
+    loaded_ephemeris : str or pathlib.Path
+
+    Notes
+    -----
+    Will also search default PINT runtime data location in ``pint.config``
+    """
     ephem_bsp = f"{ephem}.bsp"
     custom_path = os.path.join(path, ephem_bsp) if os.path.isdir(path) else path
     search_list = [custom_path]
@@ -64,13 +113,17 @@ def _load_kernel_local(ephem, path):
     for p in search_list:
         if os.path.exists(p):
             # .set() can accept a path to an ephemeris
-            astropy.coordinates.solar_system_ephemeris.set(ephem)
-            log.info(f"Set solar system ephemeris to local file:\n\t{ephem}")
-            return
+            astropy.coordinates.solar_system_ephemeris.set(p)
+            log.info(f"Set solar system ephemeris to local file:\n\t{p}")
+            return p
     raise FileNotFoundError(f"ephemeris file {ephem} not found in any of {search_list}")
 
 
-def load_kernel(ephem, path=None, link=None):
+def load_kernel(
+    ephem: str,
+    path: Optional[Union[str, pathlib.Path]] = None,
+    link: Optional[str] = None,
+) -> Union[str, pathlib.Path, astropy.utils.state._ScienceStateContext]:
     """Load the solar system ephemeris
 
     Ephemeris files may be obtained through astropy's internal
@@ -92,7 +145,7 @@ def load_kernel(ephem, path=None, link=None):
     ----------
     ephem : str
         Short name of the ephemeris, for example ``de421``. Case-insensitive.
-    path : str, optional
+    path : str or pathlib.Path, optional
         Load the ephemeris from the file specified in path, rather than
         requesting it from the network or astropy's collection of
         ephemerides. The file is searched for by treating path as relative
@@ -102,17 +155,30 @@ def load_kernel(ephem, path=None, link=None):
         Suggest the URL as a possible location astropy should search
         for the ephemeris.
 
+    Returns
+    -------
+    loaded_ephemeris : str or pathlib.Path or astropy.utils.state._ScienceStateContext
+        Can be str or pathlib.Path if loaded from a local file, or :class:`astropy.utils.state._ScienceStateContext`
+        if loaded from URL
+
 
     Note
     ----
-    If both path and link are provided. Path will be first to try.
+    If both ``path`` and ``link`` are provided, local path will be tried first.
+
+    If ``path`` is not provided, will still search default mirror sites.
+
+    Any loaded ephemeris will be stored so it will not be re-requested.
     """
     ephem = ephem.lower()
+    if ephem in loaded_ephems:
+        log.debug(f"Using pre-loaded kernel for {ephem}: {loaded_ephems[ephem]}")
+        return loaded_ephems[ephem]
     # If a local path is provided, the local search will be considered first.
     if path is not None:
         try:
-            _load_kernel_local(ephem, path=path)
-            return
+            loaded_ephems[ephem] = _load_kernel_local(ephem, path=path)
+            return loaded_ephems[ephem]
         except OSError:
             log.info(
                 f"Failed to load local solar system ephemeris kernel {path}, falling back on astropy"
@@ -123,14 +189,21 @@ def load_kernel(ephem, path=None, link=None):
     # or astropy can't access it (because astropy doesn't know about
     # the nanograv mirrors)
     with contextlib.suppress(ValueError, OSError):
-        astropy.coordinates.solar_system_ephemeris.set(ephem)
+        loaded_ephems[ephem] = astropy.coordinates.solar_system_ephemeris.set(ephem)
         log.info(f"Set solar system ephemeris to {ephem} through astropy")
-        return
+        return loaded_ephems[ephem]
     # If this raises an exception our last hope is gone so let it propagate
-    _load_kernel_link(ephem, link=link)
+    loaded_ephems[ephem] = _load_kernel_link(ephem, link=link)
+    return loaded_ephems[ephem]
 
 
-def objPosVel_wrt_SSB(objname, t, ephem, path=None, link=None):
+def objPosVel_wrt_SSB(
+    objname: str,
+    t: astropy.time.Time,
+    ephem: str,
+    path: Optional[Union[str, pathlib.Path]] = None,
+    link: Optional[str] = None,
+) -> PosVel:
     """This function computes a solar system object position and velocity respect
     to solar system barycenter using astropy coordinates get_body_barycentric()
     method.
@@ -146,8 +219,8 @@ def objPosVel_wrt_SSB(objname, t, ephem, path=None, link=None):
     t: Astropy.time.Time object
         Observation time in Astropy.time.Time object format.
     ephem: str
-        The ephem to for computing solar system object position and velocity
-    path : str, optional
+        The ephem to for computing solar system object position and velocity (without bsp extension)
+    path : str or pathlib.Path, optional
         Local path to the ephemeris file.
     link : str, optional
         Location of path on the internet.
@@ -163,7 +236,14 @@ def objPosVel_wrt_SSB(objname, t, ephem, path=None, link=None):
     return PosVel(pos.xyz, vel.xyz.to(u.km / u.second), origin="ssb", obj=objname)
 
 
-def objPosVel(obj1, obj2, t, ephem, path=None, link=None):
+def objPosVel(
+    obj1: str,
+    obj2: str,
+    t: astropy.time.Time,
+    ephem: str,
+    path: Optional[Union[str, pathlib.Path]] = None,
+    link: Optional[str] = None,
+) -> PosVel:
     """Compute the position and velocity for solar system obj2 referenced at obj1.
 
     This function uses astropy solar system Ephemerides module.
@@ -177,8 +257,8 @@ def objPosVel(obj1, obj2, t, ephem, path=None, link=None):
     tdb: Astropy.time.Time object
         TDB time in Astropy.time.Time object format
     ephem: str
-        The ephem to for computing solar system object position and velocity
-    path : str, optional
+        The ephem to for computing solar system object position and velocity (without bsp extension)
+    path : str or pathlib.Path, optional
         Local path to the ephemeris file.
     link : str, optional
         Location of path on the internet.
@@ -205,7 +285,12 @@ def objPosVel(obj1, obj2, t, ephem, path=None, link=None):
         )
 
 
-def get_tdb_tt_ephem_geocenter(tt, ephem, path=None, link=None):
+def get_tdb_tt_ephem_geocenter(
+    tt: astropy.time.Time,
+    ephem: str,
+    path: Optional[Union[str, pathlib.Path]] = None,
+    link: Optional[str] = None,
+) -> u.Quantity:
     """The is a function to read the TDB_TT correction from the JPL DExxxt.bsp
     ephemeris file.
 
@@ -214,11 +299,15 @@ def get_tdb_tt_ephem_geocenter(tt, ephem, path=None, link=None):
     t: Astropy.time.Time object
         Observation time in Astropy.time.Time object format.
     ephem: str
-        Ephemeris name.
-    path : str, optional
+        The ephem to for computing solar system object position and velocity (without bsp extension)
+    path : str or pathlib.Path, optional
         Local path to the ephemeris file.
     link : str, optional
         Location of path on the internet.
+
+    Returns
+    -------
+    tdb_tt_correction : u.Quantity
 
     Note
     ----
