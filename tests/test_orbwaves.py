ff87d668df8d77513407eeab44d9fc3165ae71ca
Colin Clark <colin.clark@aei.mpg.de>
2024-09-13 16:50:54 +0200

    Adding a test for ORBWAVEs with the DD model

--- tests/test_orbwaves.py
+++ tests/test_orbwaves.py
@@ -29,6 +29,23 @@ def test_orbwaves_fit():
     assert rms > 21.1 and rms < 21.2
 
 
+def test_orbwaves_DD_fit():
+    m, t = get_model_and_toas(
+        datadir / "J1048+2339_orbwaves_DD.par", datadir / "J1048+2339_3PC_fake.tim"
+    )
+
+    f = fitter.WLSFitter(toas=t, model=m)
+
+    rms = f.resids.rms_weighted().to_value("us")
+
+    assert rms > 29.2 and rms < 29.3
+
+    f.fit_toas()
+    rms = f.resids.rms_weighted().to_value("us")
+
+    assert rms > 21.1 and rms < 21.2
+
+
 def test_invalid_parfiles():
     parlines = open(datadir / "J1048+2339_orbwaves.par").readlines()
 
