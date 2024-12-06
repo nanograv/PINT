a5d1a174084f256ac112e53689b9e6b6c30e1a05
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-12-06 11:24:26 +0100

    wideband residuals and design matrix

--- src/pint/residuals.py
+++ src/pint/residuals.py
@@ -17,6 +17,7 @@ import numpy as np
 from scipy.linalg import LinAlgError
 from loguru import logger as log
 
+from pint import dmu
 from pint.models.dispersion_model import Dispersion
 from pint.models.parameter import maskParameter
 from pint.models.timing_model import TimingModel
@@ -1270,3 +1271,10 @@ class WidebandTOAResiduals(CombinedResiduals):
     def reduced_chi2(self) -> float:
         """Return the weighted reduced chi-squared."""
         return self.chi2 / self.dof
+
+    def calc_combined_resids(self) -> np.ndarray:
+        """Returns the combined TOA and DM residuals as a numpy array.
+        The TOA residuals are in s and the DM residuals are in dmu."""
+        tres = self.toa.calc_time_resids().to_value(u.s)
+        dres = self.dm.calc_resids().to_value(dmu)
+        return np.hstack((tres, dres)).astype(float)
