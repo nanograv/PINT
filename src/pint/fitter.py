ad0cb606a0eeb753b711099c4dddcb7b0a2dfb30
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2025-02-06 13:54:12 +0100

    noise_ampls

--- src/pint/fitter.py
+++ src/pint/fitter.py
@@ -1553,19 +1553,16 @@ class DownhillGLSFitter(DownhillFitter):
         # Compute the noise realizations if possible
         if not self.full_cov:
             noise_dims = self.model.noise_model_dimensions(self.toas)
-            noise_resids = {}
+            noise_ampls = {}
             ntmpar = len(self.model.free_params)
             for comp in noise_dims:
                 # The first column of designmatrix is "offset", add 1 to match
                 # the indices of noise designmatrix
                 p0 = noise_dims[comp][0] + ntmpar + 1
                 p1 = p0 + noise_dims[comp][1]
-                noise_resids[comp] = (
-                    np.dot(
-                        self.current_state.M[:, p0:p1], self.current_state.xhat[p0:p1]
-                    )
-                    * u.s
-                )
+                noise_ampls[comp] = (self.current_state.xhat / self.current_state.norm)[
+                    p0:p1
+                ] * u.s
                 if debug:
                     setattr(
                         self.resids,
@@ -1576,7 +1573,7 @@ class DownhillGLSFitter(DownhillFitter):
                         ),
                     )
                     setattr(self.resids, f"{comp}_M_index", (p0, p1))
-            self.resids.noise_resids = noise_resids
+            self.resids.noise_ampls = noise_ampls
             if debug:
                 setattr(self.resids, "norm", self.current_state.norm)
 
