b5aeab8fae4bb36812d5e23a140a888ba69bfd53
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2025-02-13 17:10:06 +0100

    full_wideband_designmatrix

--- src/pint/fitter.py
+++ src/pint/fitter.py
@@ -1472,11 +1472,14 @@ class WidebandState(ModelState):
             Mn = DesignMatrixMaker("toa_noise", u.s)(self.fitter.toas, self.model)
             phiinv = 1 / self.model.full_basis_weight(self.fitter.toas)
             if Mn is not None:
-                new_d_matrix = combine_design_matrices_by_param(d_matrix, Mn)
-                M, params, units = (
-                    new_d_matrix.matrix,
-                    new_d_matrix.derivative_params,
-                    new_d_matrix.param_units,
+                # new_d_matrix = combine_design_matrices_by_param(d_matrix, Mn)
+                # M, params, units = (
+                #     new_d_matrix.matrix,
+                #     new_d_matrix.derivative_params,
+                #     new_d_matrix.param_units,
+                # )
+                M, params, units, _ = self.model.full_wideband_designmatrix(
+                    self.fitter.toas
                 )
 
         M, norm = normalize_designmatrix(M, params)
