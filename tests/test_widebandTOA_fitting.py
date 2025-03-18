eec785145ff66c84f1451fcd8162e65671d36283
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2025-03-18 15:51:11 +0100

    test_wideband_fitting_with_dmgp

--- tests/test_widebandTOA_fitting.py
+++ tests/test_widebandTOA_fitting.py
@@ -1,12 +1,16 @@
 """ Various of tests for the general data fitter using wideband TOAs."""
 
+import contextlib
+from io import StringIO
 import os
 
 import numpy as np
 import astropy.units as u
+from pint.exceptions import StepProblem
 from pint.models import get_model
+from pint.simulation import make_fake_toas_uniform
 from pint.toa import get_TOAs
-from pint.fitter import WidebandTOAFitter
+from pint.fitter import WidebandDownhillFitter, WidebandTOAFitter
 from pinttestdata import datadir
 
 os.chdir(datadir)
@@ -96,3 +100,65 @@ class TestWidebandTOAFitter:
             fitter.resids.pl_red_noise_M[0] * fitter.resids.norm[p0:p1][np.newaxis, :]
         )
         assert np.all(np.isclose(pl_rd, pl_rd_backwards[0:313, :]))
+
+
+def test_wideband_fitting_with_dmgp():
+    par = """
+        RAJ     05:00:00    1
+        DECJ    15:00:00    1
+        F0      100         1
+        F1      -1e-15      1
+        PEPOCH  50000
+        PHOFF   0           1
+        DM      15          1
+        DM1     0.1         1
+        DM2     1e-4        1
+        DMEPOCH 50000
+        TNDMAMP -13         
+        TNDMGAM 3
+        TNDMC   15
+        TZRMJD  50000
+        TZRSITE ssb
+        TZRFRQ  1400
+    """
+    m = get_model(StringIO(par))
+    ntoas = 200
+    t = make_fake_toas_uniform(
+        startMJD=54000,
+        endMJD=55000,
+        ntoas=ntoas,
+        model=m,
+        add_noise=True,
+        add_correlated_noise=True,
+        wideband=True,
+        include_bipm=True,
+        subtract_mean=False,
+    )
+
+    ntmpar = len(m.free_params)
+    nharms = int(m.TNDMC.value)
+
+    assert m.designmatrix(t)[0].shape == (ntoas, ntmpar)
+    assert m.noise_model_designmatrix(t).shape == (ntoas, 2 * nharms)
+    assert m.full_designmatrix(t)[0].shape == (ntoas, ntmpar + 2 * nharms)
+
+    assert m.wideband_designmatrix(t)[0].shape == (2 * ntoas, ntmpar)
+    assert m.noise_model_wideband_designmatrix(t).shape == (
+        2 * ntoas,
+        2 * nharms,
+    )
+    assert m.full_wideband_designmatrix(t)[0].shape == (2 * ntoas, ntmpar + 2 * nharms)
+
+    ftr = WidebandDownhillFitter(t, m)
+    with contextlib.suppress(StepProblem):
+        # The fitting works, but the downhill fitter complains too often
+        ftr.fit_toas()
+
+    assert np.all(
+        (
+            np.abs(ftr.model[par].value - m[par].value)
+            / ftr.model[par].uncertainty_value
+            < 3
+        )
+        for par in m.free_params
+    )
