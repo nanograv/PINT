c8ff5210458f5e2cf7d3f2d0b3694bfe416e1de5
David Kaplan <kaplan@uwm.edu>
2026-05-27 12:39:58 -0500

    added explicit casting

--- src/pint/simulation.py
+++ src/pint/simulation.py
@@ -66,7 +66,7 @@ def zero_residuals(
         maxresid = np.abs(resids).max()
         if abs(resids).max() < tolerance:
             break
-        ts.adjust_TOAs(-time.TimeDelta(resids))
+        ts.adjust_TOAs(-time.TimeDelta(np.float64(resids)))
     else:
         raise ValueError(
             f"Unable to make fake residuals - left over errors are {abs(resids).max()}"
@@ -314,7 +314,7 @@ def make_fake_toas_uniform(
 
     clk_version = get_fake_toa_clock_versions(model, include_bipm=include_bipm)
     ts = pint.toa.get_TOAs_array(
-        times,
+        np.float64(times),
         obs=obs,
         scale=get_observatory(obs).timescale,
         freqs=freq_array,
